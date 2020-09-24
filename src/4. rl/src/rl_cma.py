b28d1303d499eb82b302ee9cd30c76e65e4c541f
bader <baderalattar22@gmail.com>
2020-09-24 15:29:58 -0400

    fixed bugs

--- src/4. rl/src/rl_cma.py
+++ src/4. rl/src/rl_cma.py
@@ -5,29 +5,32 @@ from math import floor,log,sqrt,ceil
 import time
 class CMA(ES):
     # adapted from https://arxiv.org/pdf/1604.00772.pdf
-    def __init__(self,n,gamma = 0.95):
+    def __init__(self,n,gamma = 0.8):
         self.gamma = gamma 
+
+        self.alpha_mu = np.array([[0.0],[0.0] ])#[2.0]] )#,[0.1]])
+        self.alpha_sigma = np.array([[0.0],[0.0] ])#, [1.0]])#,[0.05]])
         self.N = n # dimensions to learn
 
         # initialize mean and learning rate
-        self.xmean = np.random.rand(self.N,1)
-        self.xmean = np.array([[3.0],[5.0]])
-        self.sigma = 0.5
+        self.mu = np.random.rand(self.N,1)
+        self.mu = np.array([[1.0],[5.0]])
+        self.sigma_lr = 0.5
         self.counteval = 0
 
 
-        self.lamda =  4.0 + 4.0 + floor(3.0*log(self.N))  # number of population members
-        self.n_rollout = int(self.lamda)  # same as lamda
-        self.mu = floor(self.lamda/2) # number of sample members from each populatoin //added floor /2
+        self.lamda =   4.0 + 4.0 + floor(3.0*log(self.N))  # number of population members
+        self.n_rollout = int(self.lamda/2.0)  # same as lamda
+        self.mu_s = floor(self.lamda/4) # number of sample members from each populatoin //added floor /2
         
         # determine weights for top mu members of population for mean+sig update
-        self.weights = log(self.mu + 0.5) - np.transpose(np.log(np.linspace(1,self.mu,self.mu))) # transpose???
+        self.weights = log(self.mu_s + 0.5) - np.transpose(np.log(np.linspace(1,self.mu_s,self.mu_s))) # transpose???
         self.weights = self.weights/np.sum(self.weights) # normalize
 
         # calculate effective members of population given weights
-        self.mueff= (np.sum(np.square(self.weights)))/np.sum(np.square(self.weights))
+        self.mueff= ((np.sum(self.weights))**2)/np.sum(np.square(self.weights))
 
-        print(self.N,self.lamda,self.mu,self.mueff)
+        print(self.N,self.lamda,self.mu_s,self.mueff)
         print(self.weights)
 
         # try different learning parameters from text
@@ -38,11 +41,19 @@ class CMA(ES):
         self.damps = 1.0 + 2.0*max(0, sqrt((self.mueff - 1.0)/(self.N + 1.0)) - 1.0) + self.cs # damping term for sigma learning rate
   
         self.pc = np.zeros((self.N,1))
-        self.ps = np.zeros((self.N,1))   # evolution paths for C and sigma   (basically how they changed)
+        self.ps = np.zeros((self.N,1))   # evolution paths for C and sigma   (basically how they mueffd)
         #print(self.ps)
         self.B = np.identity(self.N)    # initizlize eigenvector
         self.D = np.identity(self.N)   # initizlize eigenvalue  
         self.C = (self.B.dot(self.D)).dot(np.transpose(self.B.dot(self.D)))   # construct covariance matrix from eigenvec/val  
+        
+        self.sigma = np.array([self.C[0,0],self.C[1,1],self.C[1,0] ])
+        # try defining B and D from C instead
+        #self.C = 2*np.identity(self.N)
+        #self.D,self.B = np.linalg.eig(self.C)    
+        #self.D = np.diag(self.D)
+
+        
         self.eigeneval = 0      # counter  
         self.chiN = (self.N**0.5)*(1.0 - 1.0/(4.0*self.N) + 1.0/(21.0*(self.N**2)))  # expectation estimation for normal dist?
 
@@ -58,8 +69,8 @@ class CMA(ES):
             self.arz[1,k] = mm [1]  # base mean for gain rate
             #print(self.arz)
 
-            #arx = self.xmean + self.sigma*(np.dot(self.B*self.D,mm) )  # add mutation    % Eq.40       
-            arx = self.xmean + self.sigma*(self.B.dot(self.D)).dot(mm) # mutate child using covariance matrix and base mean
+            #arx = self.mu + self.sigma_lr*(np.dot(self.B*self.D,mm) )  # add mutation    % Eq.40       
+            arx = self.mu + self.sigma_lr*(self.B.dot(self.D)).dot(mm) # mutate child using covariance matrix and base mean
             #print(arx)
             #print(arx2)
             self.arx[0,k] = arx[0]*(arx[0] > 0)  # enforce stricly postive values (gain rate is negated in start_training.py)
@@ -67,21 +78,21 @@ class CMA(ES):
             self.counteval +=1 # increase species number counter
 
         theta = copy.copy(self.arx)
-        return theta
+        return theta,0
 
-    def train(self,theta,reward):
+    def train(self,theta,reward, epsilon=0):
         # combine theta and reward and sort by highest reward
         summary = np.concatenate((np.transpose(theta),reward,np.transpose(self.arz)),axis=1)
         summary = np.transpose(summary[summary[:,2].argsort()[::-1]])
 
         # weighted recombination of mmu best members
-        xmean = np.dot(summary[0:2,0:self.mu],np.transpose(self.weights))
-        zmean = np.dot(summary[3:5,0:self.mu],np.transpose(self.weights))
-        #print(summary)
-        #print(xmean)
-        #print(zmean)
+        mu = np.dot(summary[0:2,0:self.mu_s],np.transpose(self.weights))
+        zmean = np.dot(summary[3:5,0:self.mu_s],np.transpose(self.weights))
+        print(summary)
+        print(mu)
+        print(zmean)
         # reshape to column vector
-        self.xmean = xmean.reshape(2,1)
+        self.mu = mu.reshape(2,1)
         self.zmean = zmean.reshape(2,1)
 
 
@@ -101,17 +112,16 @@ class CMA(ES):
         #print(C)
         C = C + self.c1*(self.pc*np.transpose(self.pc)+ (1-self.hsig)*self.cc*(2.0 - self.cc)*self.C)
         #print(C)
-        temp = (self.B.dot(self.D)).dot(summary[3:5,0:self.mu])
+        temp = (self.B.dot(self.D)).dot(summary[3:5,0:self.mu_s])
         #print(temp)
         C = C + self.cmu*( (temp.dot(np.diag(self.weights))).dot(np.transpose(temp))  )
         #print(self.cmu*( (temp.dot(np.diag(self.weights))).dot(np.transpose(temp))  ))
         #print(self.cmu)
         #print(C)
         self.C = C
-
         # adjust lerning rate
-        self.sigma = self.sigma*np.exp((self.cs/self.damps)*(np.linalg.norm(self.ps)/self.chiN - 1.0))
-        #print(self.sigma)
+        self.sigma_lr = self.sigma_lr*np.exp((self.cs/self.damps)*(np.linalg.norm(self.ps)/self.chiN - 1.0))
+        #print(self.sigma_lr)
 
         # force covariance matrix to be symmetric
         self.eigeneval = self.counteval      
@@ -119,11 +129,15 @@ class CMA(ES):
         self.D,self.B = np.linalg.eig(self.C)    
         # if theres an error, its probably here!!!!!!! 
         print(self.D)
-        print(np.diag(self.D))
+
+        self.sigma = np.array([self.C[0,0],self.C[1,1],self.C[1,0] ])
+        self.D = np.diag(self.D)
+        print(self.D)
         print(np.sqrt(np.diag(self.D)))
-        self.D = np.sqrt(np.diag(self.D))
+        #self.D = np.sqrt(np.diag(self.D))
         #self.D = np.diag(np.sqrt(np.diag(self.D))) # <------- !!!!
         print(self.D)
+        print(self.C)
         #print(C)print
         #print(self.D)
         #print(self.B)
@@ -132,7 +146,7 @@ class CMA(ES):
 if __name__ == "__main__":
     es = CMA(2)
     #print(es.N)
-    #print(es.xmean)
+    #print(es.mu)
     theta = es.get_theta()
     #print(theta)
     reward = np.array([[10],[5],[3],[2],[12],[20]])
@@ -142,40 +156,43 @@ class CMA_basic(ES):
     # expand on update
     # https://arxiv.org/pdf/1604.00772.pdf
     def __init__(self,mu ,sigma,N_best = 0.25,n_rollout = 10,gamma=0.95):
+        # not needed alpha (but doenst work without)
+        self.alpha_mu = np.array([[0.0],[0.0] ])#[2.0]] )#,[0.1]])
+        self.alpha_sigma = np.array([[0.0],[0.0] ])#, [1.0]])#,[0.05]])
         self.gamma = gamma
         self.mu = mu
         self.sigma = sigma
         self.N_best = N_best
-        self.n_rollout = n_rollout
+        self.n_rollout = int(n_rollout/2)  # to use with pepg framework
 
-        self.mu_history = copy.copy(self.mu)  # Creates another array of self.mu and attaches it to self.mu_history
+        self.mu_history = copy.copy(self.mu)  # Creates another array of self.mu_s and attaches it to self.mu_s_history
         self.sigma_history = copy.copy(self.sigma)
         self.reward_history = np.array([0])
 
     def get_theta(self):
-        # change format for np multivariate normal function
+        # mueff format for np multivariate normal function
         mu = np.array([self.mu[0,0],self.mu[1,0]])
 
         sigma = np.array([[self.sigma[0,0],self.sigma[2,0]],[self.sigma[2,0],self.sigma[1,0]]])
-        theta = np.random.multivariate_normal(mu,sigma,[1, self.n_rollout])
+        theta = np.random.multivariate_normal(mu,sigma,[1, int(self.n_rollout*2)])
         theta = theta[0,:,:]
         theta = np.transpose(theta)
 
         
         ## Caps values of RREV to be greater than zero and Gain to be neg so
         # the system doesnt fight itself learning which direction to rotate
-        for k_n in range(self.n_rollout):
+        for k_n in range(int(self.n_rollout*2)):
             if theta[0,k_n] < 0:
                 theta[0,k_n] = 0.001
             if theta[1,k_n] > 0:
                 theta[1,k_n] = -0.001
 
-        return theta
+        return theta,0
 
-    def train(self,theta,reward):
+    def train(self,theta,reward,epsilon=0):
         summary = np.append(np.transpose(theta),reward,axis=1)
         summary = summary[summary[:,2].argsort()[::-1]]
-        N = ceil(self.N_best*self.n_rollout)
+        N = ceil(self.N_best*self.n_rollout*2.0)
 
         x = summary[0:N,0]
         y = summary[0:N,1]
