e5a2d0bfd6657492ad9b720587f0aa98ce01acbb
bader <baderalattar22@gmail.com>
2020-10-10 23:39:12 -0400

    try equation for pitch rate

--- src/4. rl/src/CF_training_env.py
+++ src/4. rl/src/CF_training_env.py
@@ -9,10 +9,11 @@ from scipy.spatial.transform import Rotation
 from numpy.core.fromnumeric import repeat
 
 from crazyflie_env import CrazyflieEnv
-from rl_syspepg import rlsysPEPGAgent_reactive ,rlsysPEPGAgent_cov
-from rl_EM import rlEM_PEPGAgent, rlEM_PEPG_CovAgent, rlEM_OutlierAgent , rlEMsys_PEPGAgent
+from rl_syspepg import rlsysPEPGAgent_reactive ,rlsysPEPGAgent_cov, rlsysPEPGAgent_adaptive
+from rl_EM import rlEM_PEPGAgent, rlEM_PEPG_CovAgent, rlEM_OutlierAgent , rlEMsys_PEPGAgent,rlEM_AdaptiveCovAgent, rlEM_AdaptiveCovAgent3D, rlEM_AdaptiveAgent
 from rl_cma import CMA_basic,CMA,CMA_sym
 
+np.random.seed(0)
 os.system("clear")
 
 # ============================
@@ -59,24 +60,28 @@ extra_time = 0.0
 # ============================
 
 ## Learning rate
-alpha_mu = np.array([[0.2],[0.2]])#[2.0]] )#,[0.1]])
-alpha_sigma = np.array([[0.1],[0.1]  ])#, [1.0]])#,[0.05]])
+alpha_mu = np.array([[0.2]])#[2.0]] )#,[0.1]])
+alpha_sigma = np.array([[0.1] ])#, [1.0]])#,[0.05]])
 
 ## Initial parameters for gaussian function
-mu = np.array([[5.0],[-5.0] , [1.5] ])# ,[1.5]])#,[1.5]])   # Initial estimates of mu: 
-sigma = np.array([[1.5],[1.5] ,[1.0] ])# ,[0.75]])      # Initial estimates of sigma: 
+mu = np.array([[4.0]])# ,[1.5]])#,[1.5]])   # Initial estimates of mu: 
+sigma = np.array([[1.0]])#,[0.0]])# ,[0.75]])      # Initial estimates of sigma: 
 
 # noise tests all started at:
 #mu = np.array([[5.0],[-5.0] ])
 #sigma = np.array([[3.0],[3.0] ])
 #  
-#agent = rlsysPEPGAgent_reactive(alpha_mu, alpha_sigma, mu,sigma, gamma=0.95,n_rollout=2)
+agent = rlsysPEPGAgent_reactive(alpha_mu, alpha_sigma, mu,sigma, gamma=0.95,n_rollout=5)
 #agent = rlsysPEPGAgent_cov(alpha_mu, alpha_sigma, mu,sigma, gamma=0.95,n_rollout=4)
 #agent = rlEM_PEPGAgent(mu,sigma,n_rollout=5)
+#agent = rlsysPEPGAgent_adaptive(alpha_mu,alpha_sigma,mu,sigma,n_rollout=10)
 #agent = rlEM_OutlierAgent(mu,sigma,n_rollout=5) # diverges?
 #agent = rlEM_PEPG_CovAgent(mu,sigma,n_rollout=5)
-agent = rlEMsys_PEPGAgent(mu,sigma,n_rollout=5)
+#agent = rlEMsys_PEPGAgent(mu,sigma,n_rollout=5)
+#agent = rlEM_AdaptiveCovAgent(mu,sigma,gamma=0.95,n_rollout=5)
+#agent = rlEM_AdaptiveCovAgent3D(mu,sigma,gamma=0.95,n_rollout=5)
 
+#agent = rlEM_AdaptiveAgent(mu,sigma,n_rollout=5)
 # ============================
 ##           CMA 
 # ============================
@@ -137,8 +142,8 @@ for k_ep in range(ep_start,1000):
 
     print( time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) )
 
-    print("RREV=%.3f, \t theta1=%.3f, \t theta2=%.3f, \t theta3=%.3f" %(mu[0], mu[1], mu[2], mu[1]))
-    print("sig1=%.3f, \t sig2=%.3f, \t sig12=%.3f, \t sig2=%.3f," %(sigma[0], sigma[1], sigma[2], sigma[1]))
+    print("RREV=%.3f, \t theta1=%.3f, \t theta2=%.3f, \t theta3=%.3f" %(mu[0], mu[0], mu[0], mu[0]))
+    print("sig1=%.3f, \t sig2=%.3f, \t sig12=%.3f, \t sig2=%.3f," %(sigma[0], sigma[0], sigma[0], sigma[0]))
     print('\n')
 
 
@@ -152,8 +157,8 @@ for k_ep in range(ep_start,1000):
 
     np.set_printoptions(precision=2, suppress=True)
     print(theta_rl[0,:], "--> RREV")
-    print(theta_rl[1,:], "--> Gain")
-    print(theta_rl[2,:], "--> v_x Gain")
+    #print(theta_rl[1,:], "--> Gain")
+    #print(theta_rl[2,:], "--> v_x Gain")
     #print(theta_rl[3,:], "--> omega_y Gain")
 
 
@@ -169,9 +174,9 @@ for k_ep in range(ep_start,1000):
         #image_prev = env.cv_image.flatten()
         
 
-        vz_ini = np.random.uniform(low=2.5, high=4.0)
-        vx_ini = np.random.uniform(low=-2.5, high=2.5)
-        #vx_ini = 0.0#np.random.normal(0.0,1.0)
+        vz_ini = np.random.uniform(low=2.0, high=3.0)
+        #vx_ini = np.random.uniform(low=-2.5, high=2.5)
+        vx_ini = 0.0#np.random.normal(0.0,1.0)
         vy_ini = 0.0#np.random.uniform(low=-1.5, high=1.5)
         v_ini = [vz_ini,vx_ini,vy_ini]
         # try adding policy parameter for roll pitch rate for vy ( roll_rate = gain3*omega_x)
@@ -183,7 +188,7 @@ for k_ep in range(ep_start,1000):
         #vy_ini = v_mag*sin(angle)
 
         print("\n!-------------------Episode # %d run # %d-----------------!" %(k_ep,k_run))
-        print("RREV: %.3f \t gain1: %.3f \t gain2: %.3f \t gain3: %.3f" %(theta_rl[0,k_run], theta_rl[1,k_run],theta_rl[2,k_run],theta_rl[1,k_run]))
+        print("RREV: %.3f \t gain1: %.3f \t gain2: %.3f \t gain3: %.3f" %(theta_rl[0,k_run], theta_rl[0,k_run],theta_rl[0,k_run],theta_rl[0,k_run]))
         print("Vz_ini: %.3f \t Vx_ini: %.3f \t Vy_ini: %.3f" %(vz_ini, vx_ini, vy_ini))
 
 
@@ -318,16 +323,20 @@ for k_ep in range(ep_start,1000):
                 ## bader work
                 # wn = sqrt(omega_x**2 + omega_y**2)
 
-                # angle_omega = pi + atan2(omega_y,omega_x)
+                #angle_omega = pi + atan2(OF_x,OF_y)
+                #print(angle_omega)
 
                 # add term to adjust for tilt 
-                qRREV = theta_rl[1,k_run]*RREV + theta_rl[2,k_run]*abs(OF_y)
-                #qomega = theta_rl[2,k_run]*wn
-                #q_roll = qRREV*sin(angle_omega)
-                #q_pitch = -qRREV*cos(angle_omega)
-                q_pitch = qRREV*np.sign(OF_y)# + qomega #omega_x#*(1-b3y)#sin(r[1]*3.14159/180)
+                #qRREV = theta_rl[1,k_run]*RREV + theta_rl[2,k_run]*OF_y
+                #qomega = theta_rl[2,k_run]*
+                
+                #angle_omega = pi/2 #sqrt(2)/2.0
+                #q_roll = -qRREV*sin(angle_omega)
+                #q_pitch = qRREV*cos(angle_omega)
+                #q_pitch = qRREV*np.sign(OF_y)# + qomega #omega_x#*(1-b3y)#sin(r[1]*3.14159/180)
 
-
+                qRREV = -(RREV + 1/3)
+                q_pitch = RREV*qRREV*np.sign(OF_y)
 
 
 
@@ -337,14 +346,14 @@ for k_ep in range(ep_start,1000):
                 print('----- pitch starts -----')
                 print('vz=%.3f, vx=%.3f, vy=%.3f' %(vz,vx,vy))
                 print('r[0]=%.3f, r[1]=%.3f, r[2]=%.3f, b3y=%.3f' %(r[0],r[1],r[2],b3y))
-                print('RREV=%.3f, OF_y=%.3f, OF_x=%.3f, q_pitch=%.3f' %( RREV, OF_y, OF_x,q_pitch) )   
+                print('RREV=%.3f, OF_y=%.3f, OF_x=%.3f, q_pitch=%.3f, q_RREV=%.3f' %( RREV, OF_y, OF_x,q_pitch,qRREV) )   
                 print("Pitch Time: %.3f" %start_time_pitch)
                 #print('d_est = %.3f, d_act = %.3f' %(d,d_acutal))
                 #print('wy = %.3f , qomega = %.3f , qRREV = %.3f' %(omega[1],qomega,qRREV))
 
                 # randomly sample noise delay with mean = 30ms and sigma = 5
-                #t_delay = np.random.normal(30.0,5.0)
-                #print("t_delay = %.3f" %(t_delay))
+                t_delay = 0 #np.random.normal(30.0,5.0)
+                print("t_delay = %.3f" %(t_delay))
                 #env.delay_env_time(t_start=start_time_pitch,t_delay=t_delay) # Artificial delay to mimic communication lag [ms]
                 
 
@@ -449,7 +458,6 @@ for k_ep in range(ep_start,1000):
         else:
             k_run += 1 # Move on to next run
 
-    print(reward)
     if not any( np.isnan(reward) ):
         print("Episode # %d training, average reward %.3f" %(k_ep, np.mean(reward)))
         agent.train(theta_rl,reward,epsilon_rl)
