ec3fbb80014f62312241829c5f59e6ac1746bd08
bhabas <bhabas@psu.edu>
2020-10-30 10:27:12 -0400

    Progress save before things go wrong

--- src/4. rl/src/dashboard4_test.py
+++ src/4. rl/src/dashboard4_test.py
@@ -5,25 +5,41 @@ import time,os,getpass
 import matplotlib.pyplot as plt
 import os
 
+from multiprocessing import Process
+
 
 from crazyflie_env import CrazyflieEnv
 
-
-env = CrazyflieEnv()
-print("Environment done")
-
-
-np.set_printoptions(precision=2, suppress=True)
-env.step('home',ctrl_flag=1) # Reset control vals and functionality
-state = env.reset_pos() # Reset Gazebo pos
+def print_val():
+    while True:
+        time.sleep(0.01)
+        # print(total.value)
 
 
-while True:
-        
+def Main():
+    env = CrazyflieEnv()
+    print("Environment done")
 
-    ## DEFINE CURRENT STATE [Can we thread this to get states even when above]
-    state = env.state_current
-    print(state)
+    np.set_printoptions(precision=2, suppress=True)
+    env.step('home',ctrl_flag=1) # Reset control vals and functionality
+    state = env.reset_pos() # Reset Gazebo pos
+
+
+    while True:
+
+        time.sleep(5e-4) # Time step size
+
+        ## DEFINE CURRENT STATE [Can we thread this to get states even when above]
+        state = env.state_current
+        print(state[0])
+
+
+if __name__ == '__main__':
+
+    p1 = Process(target=print_val)
+    p1.start()
+    Main()
+    
 
 
 
