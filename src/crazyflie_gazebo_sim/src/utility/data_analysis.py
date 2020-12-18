0c858c6844c9550463479516c0bc327650863a3a
bhabas <bhabas@psu.edu>
2020-12-18 00:30:39 -0500

    Formatting and documentation

--- src/crazyflie_gazebo_sim/src/utility/data_analysis.py
+++ src/crazyflie_gazebo_sim/src/utility/data_analysis.py
@@ -17,65 +17,6 @@ class DataFile:
         run_df = self.trial_df[(self.trial_df['k_ep']==k_ep) & (self.trial_df['k_run']==k_run)]
         return run_df
 
-    def grab_V_ini(self):
-
-        """Return IC values
-
-        Returns:
-            float: Initial Velocity Conditions (vx,vy,vz)
-        """        
-        vx_IC = self.trial_df.iloc[-1]['vx']
-        vy_IC = self.trial_df.iloc[-1]['vy']
-        vz_IC = self.trial_df.iloc[-1]['vz']
-        
-        return vx_IC,vy_IC,vz_IC
-
-    def grab_finalPolicy(self):
-        """Returns the final policy for a trial
-
-        Returns:
-           mu [np.array]: Final policy average
-           sigma [np.array]: Final policy standard deviation
-
-        """        
-        
-        ## FIND FINAL RUN DF
-        k_ep = self.trial_df.iloc[-1]['k_ep']
-        k_run = self.trial_df.iloc[-1]['k_run']
-        run_df = self.select_run(k_ep,k_run)
-
-        ## SELECT MU & SIGMA
-        mu = run_df.iloc[-1]['mu']
-        mu = np.fromstring(mu[2:-2], dtype=float, sep=' ')  # Convert str to np array
-        
-
-        sigma = run_df.iloc[-1]['sigma']
-        sigma = np.fromstring(sigma[2:-2], dtype=float, sep=' ')
-        
-
-        return mu,sigma
-
-    def grab_stateData(self,k_ep,k_run,stateName):
-        """Returns np.array of specified state
-
-        Args:
-            k_ep (int): Episode number
-            k_run (int): Run number
-            stateName (int): State name
-
-        Returns:
-            state [np.array]: Returned state values
-        """        
-        run_df = self.select_run(k_ep,k_run)
-        run_df = run_df.iloc[:-1] # Drop last row of DF
-
-        ## GRAB STATE DATA AND CONVERT TO NUMPY ARRAY
-        state = run_df.iloc[:][stateName]
-        state = state.to_numpy()
-        state = np.expand_dims(state, axis=0).T
-
-        return state
-    
     def plot_rewardFunc(self,figNum=0):
         """Plot rewards for trial
 
@@ -112,6 +53,160 @@ class DataFile:
         ax.grid()
 
         plt.show()
+
+    def landing_rate(self): 
+        """Determines successful landing rate from final two episodes
+
+        Returns:
+            [float]: [Landing rate]
+        """    
+        ## GRAB REWARD DF ALONG W/ INIT N_ROLLOUTS AND LANDING CUTTOFF CRITERIA
+        reward_df = self.trial_df.iloc[:][['k_ep','reward']].dropna()
+        num_rollouts = self.trial_df.iloc[-1]['n_rollouts']
+        landing_cutoff = 18.5
+
+        ## GRAB FINAL N_ROLLOUT REWARDS AND CALC SUCCESSFUL LANDINGS
+        temp = reward_df.iloc[-int(num_rollouts*4):]['reward'] # Store last [20] rows
+        landings= temp[temp>landing_cutoff].count()
+        sim_bug = temp[temp<3.00].count()
+
+        attempts = num_rollouts*4-sim_bug
+        landingRate = landings/attempts
+
+        return landingRate
+
+
+## POLICY FUNCTIONS
+    def grab_policy(self,k_ep,k_run):
+        """Returns policy from specific run
+
+        Args:
+            k_ep (int): Episode number
+            k_run (int): Run number
+
+        Returns:
+            np.array: [RREV,G1,G2,...]
+        """        
+        run_df = self.select_run(k_ep,k_run)
+
+        ## SELECT POLICY
+        policy = run_df.iloc[-1]['policy']
+        policy = np.fromstring(policy[2:-2], dtype=float, sep=' ')  # Convert str to np array
+
+        return policy
+
+    def plot_policy(self,trialNum=np.nan):
+        """Creates subplots to show convergence for policy gains
+
+        Args:
+            trialNum ([int], optional): Display trial number. Defaults to np.nan.
+        """        
+
+        ## CLEAN AND GRAB DATA FOR MU & SIGMA
+        policy_df = self.trial_df.iloc[:][['k_ep','mu','sigma']]
+        policy_df = policy_df.dropna().drop_duplicates()
+        k_ep_arr = policy_df.iloc[:]['k_ep'].to_numpy()
+
+        
+
+        ## CREATE NP ARRAYS FOR MU & SIGMA OVER TRIAL
+        mu_arr = []
+        for mu in policy_df.iloc[:]['mu']:
+            mu = np.fromstring(mu[2:-2], dtype=float, sep=' ')
+            mu_arr.append(mu)
+        mu_arr = np.array(mu_arr)
+
+        sigma_arr = []
+        for sigma in policy_df.iloc[:]['sigma']:
+            sigma = np.fromstring(sigma[2:-2], dtype=float, sep=' ')
+            sigma_arr.append(sigma)
+        sigma_arr = np.array(sigma_arr)
+
+        
+
+        num_col = mu_arr.shape[1] # Number of policy gains in mu [Currently 3]
+        G_Labels = ['RREV_trigger','G1','G2','G3','G4','G5'] # List of policy gain names
+        vx,vy,vz = self.grab_vel_d()
+
+
+        ## CREATE SUBPLOT FOR MU 
+        fig = plt.figure()
+        ax = fig.add_subplot(211)
+        for jj in range(num_col): # Iterate through gains and plot each
+            ax.plot(k_ep_arr,mu_arr[:,jj],label=G_Labels[jj])
+
+
+        ax.set_ylabel('Policy Values')
+        ax.set_xlabel('K_ep')
+        ax.set_ylim(0) # Set lower ylim
+        ax.set_title(f'Policy Value vs Episode (Trial:{trialNum}) | Vx = {vx} Vz = {vz}')
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25),ncol=num_col)
+        ax.grid()
+        fig.tight_layout()
+
+
+        ## CREATE SUBPLOT FOR SIGMA
+        ax = fig.add_subplot(212)
+        for jj in range(num_col): # Iterate through gains and plot each
+            ax.plot(k_ep_arr,sigma_arr[:,jj],label=G_Labels[jj])
+
+        ax.set_ylabel('Standard Deviation')
+        ax.set_xlabel('K_ep')
+        ax.set_title(f'Policy S.D. vs Episode (Trial:{trialNum}) | Vx = {vx} Vz = {vz}')
+        ax.legend(ncol=3,loc='upper right')
+        ax.grid()
+
+        fig.tight_layout()
+        plt.show()
+
+    def grab_finalPolicy(self):
+        """Returns the final policy for a trial
+
+        Returns:
+           mu [np.array]: Final policy average
+           sigma [np.array]: Final policy standard deviation
+
+        """        
+        
+        ## FIND FINAL RUN DF
+        k_ep = self.trial_df.iloc[-1]['k_ep']
+        k_run = self.trial_df.iloc[-1]['k_run']
+        run_df = self.select_run(k_ep,k_run)
+
+        ## SELECT MU & SIGMA
+        mu = run_df.iloc[-1]['mu']
+        mu = np.fromstring(mu[2:-2], dtype=float, sep=' ')  # Convert str to np array
+        
+
+        sigma = run_df.iloc[-1]['sigma']
+        sigma = np.fromstring(sigma[2:-2], dtype=float, sep=' ')
+        
+
+        return mu,sigma
+
+
+
+## STATE FUNCTIONS
+    def grab_stateData(self,k_ep,k_run,stateName):
+        """Returns np.array of specified state
+
+        Args:
+            k_ep (int): Episode number
+            k_run (int): Run number
+            stateName (int): State name
+
+        Returns:
+            state [np.array]: Returned state values
+        """        
+        run_df = self.select_run(k_ep,k_run)
+        run_df = run_df.iloc[:-1] # Drop last row of DF
+
+        ## GRAB STATE DATA AND CONVERT TO NUMPY ARRAY
+        state = run_df.iloc[:][stateName]
+        state = state.to_numpy()
+        state = np.expand_dims(state, axis=0).T
+
+        return state
 
     def plot_state(self,k_ep,k_run,stateName,figNum=0):
         """Plot state data from run vs time
@@ -151,108 +246,21 @@ class DataFile:
 
         plt.show()
 
-    def plot_policy(self,trialNum=np.nan):
-        """Creates subplots to show convergence for policy gains
-
-        Args:
-            trialNum ([int], optional): Display trial number. Defaults to np.nan.
-        """        
-
-        ## CLEAN AND GRAB DATA FOR MU & SIGMA
-        policy_df = self.trial_df.iloc[:][['k_ep','mu','sigma']]
-        policy_df = policy_df.dropna().drop_duplicates()
-        k_ep_arr = policy_df.iloc[:]['k_ep'].to_numpy()
-
-        
-
-        ## CREATE NP ARRAYS FOR MU & SIGMA OVER TRIAL
-        mu_arr = []
-        for mu in policy_df.iloc[:]['mu']:
-            mu = np.fromstring(mu[2:-2], dtype=float, sep=' ')
-            mu_arr.append(mu)
-        mu_arr = np.array(mu_arr)
-
-        sigma_arr = []
-        for sigma in policy_df.iloc[:]['sigma']:
-            sigma = np.fromstring(sigma[2:-2], dtype=float, sep=' ')
-            sigma_arr.append(sigma)
-        sigma_arr = np.array(sigma_arr)
-
-        
-
-        num_col = mu_arr.shape[1] # Number of policy gains in mu [Currently 3]
-        G_Labels = ['RREV_trigger','G1','G2','G3','G4','G5'] # List of policy gain names
-        vx,vy,vz = self.grab_V_ini()
 
 
-        ## CREATE SUBPLOT FOR MU 
-        fig = plt.figure()
-        ax = fig.add_subplot(211)
-        for jj in range(num_col): # Iterate through gains and plot each
-            ax.plot(k_ep_arr,mu_arr[:,jj],label=G_Labels[jj])
-
-
-        ax.set_ylabel('Policy Values')
-        ax.set_xlabel('K_ep')
-        ax.set_ylim(0) # Set lower ylim
-        ax.set_title(f'Policy Value vs Episode (Trial:{trialNum}) | Vx = {vx} Vz = {vz}')
-        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25),ncol=num_col)
-        ax.grid()
-        fig.tight_layout()
-
-
-        ## CREATE SUBPLOT FOR SIGMA
-        ax = fig.add_subplot(212)
-        for jj in range(num_col): # Iterate through gains and plot each
-            ax.plot(k_ep_arr,sigma_arr[:,jj],label=G_Labels[jj])
-
-        ax.set_ylabel('Standard Deviation')
-        ax.set_xlabel('K_ep')
-        ax.set_title(f'Policy S.D. vs Episode (Trial:{trialNum}) | Vx = {vx} Vz = {vz}')
-        ax.legend(ncol=3,loc='upper right')
-        ax.grid()
-
-        fig.tight_layout()
-        plt.show()
-
-    def landing_rate(self): 
-        """Determines successful landing rate from final two episodes
+## DESIRED IC FUNCTIONS
+    def grab_vel_d(self):
+    
+        """Return IC values
 
         Returns:
-            [float]: [Landing rate]
-        """    
-        ## GRAB REWARD DF ALONG W/ INIT N_ROLLOUTS AND LANDING CUTTOFF CRITERIA
-        reward_df = self.trial_df.iloc[:][['k_ep','reward']].dropna()
-        num_rollouts = self.trial_df.iloc[-1]['n_rollouts']
-        landing_cutoff = 18.5
-
-        ## GRAB FINAL N_ROLLOUT REWARDS AND CALC SUCCESSFUL LANDINGS
-        temp = reward_df.iloc[-int(num_rollouts*4):]['reward'] # Store last [20] rows
-        landings= temp[temp>landing_cutoff].count()
-        sim_bug = temp[temp<3.00].count()
-
-        attempts = num_rollouts*4-sim_bug
-        landingRate = landings/attempts
-
-        return landingRate
-
-    def grab_policy(self,k_ep,k_run):
-        """Returns policy from specific run
-
-        Args:
-            k_ep (int): Episode number
-            k_run (int): Run number
-
-        Returns:
-            np.array: [RREV,G1,G2,...]
+            float: Initial Velocity Conditions (vx,vy,vz)
         """        
-        run_df = self.select_run(k_ep,k_run)
-
-        ## SELECT POLICY
-        policy = run_df.iloc[-1]['policy']
-        policy = np.fromstring(policy[2:-2], dtype=float, sep=' ')  # Convert str to np array
-
-        return policy
+        vx_IC = self.trial_df.iloc[-1]['vx']
+        vy_IC = self.trial_df.iloc[-1]['vy']
+        vz_IC = self.trial_df.iloc[-1]['vz']
+        
+        return vx_IC,vy_IC,vz_IC
 
     def grab_omega_d(self,k_ep,k_run):
         """Returns desired omega_d as list
@@ -270,16 +278,6 @@ class DataFile:
         wz_d = run_df.iloc[-1]['wz']
 
         return [wx_d,wy_d,wz_d]
-    
-    def grab_V_flip(self,k_ep,k_run):
-        run_df = self.select_run(k_ep,k_run)
-        flip_df = run_df.loc[run_df['flip_trigger']==True]
-
-        vx = flip_df.iloc[0]['vx']
-        vy = flip_df.iloc[0]['vy']
-        vz = flip_df.iloc[0]['vz']
-
-        return [vx,vy,vz]
 
     def grab_omega_d_trial(self):
         """Returns average omega_d over final two episodes
@@ -299,6 +297,10 @@ class DataFile:
 
         return [wx_d,wy_d,wz_d]
 
+
+
+
+## FLIP FUNCTIONS
     def grab_omega_flip(self,k_ep,k_run):
         """Returns omega at flip for specific ep/run
 
@@ -341,6 +343,7 @@ class DataFile:
         w_flip = np.mean(arr,axis=0)
         
         return w_flip
+
 
     def grab_vel_flip(self,k_ep,k_run):
         """Returns vel at flip for specific ep/run
@@ -386,6 +389,10 @@ class DataFile:
         return v_flip
 
 
+
+
+
+
 ## IMPACT FUNCTIONS
     def grab_impact_time(self,k_ep,k_run):
         _,t_impact,t_impact_norm = self.grab_impact_angle(k_ep,k_run)
@@ -394,6 +401,15 @@ class DataFile:
 
 
     def grab_impact_angle(self,k_ep,k_run):
+        """Returns pitch angle at time of impact
+
+        Args:
+            k_ep (int): Episode number
+            k_run (int): Run number
+
+        Returns:
+            List: eul_impact,t_impact,t_impact_normalized
+        """        
         ## NOTE: This is only verified for 2-D case with Pitch maneuver
 
         run_df = self.select_run(k_ep,k_run)
@@ -417,6 +433,11 @@ class DataFile:
         return eul_impact,t_impact,t_impact_norm
 
     def grab_impact_angle_trial(self):
+        """Returns avg angle at impact for final two episodes
+
+        Returns:
+            Float: eul_impact
+        """
 
         num_rollouts = int(self.trial_df.iloc[-1]['n_rollouts'])
 
@@ -434,6 +455,16 @@ class DataFile:
 
 
     def grab_impact_vel(self,k_ep,k_run):
+        """Returns vel at time of impact
+
+        Args:
+            k_ep (int): Episode number
+            k_run (int): Run number
+
+        Returns:
+            list: [vx_impact,vy_impact,vz_impact]
+        """ 
+
         run_df = self.select_run(k_ep,k_run)
         t_impact,_ = self.grab_impact_time(k_ep,k_run)
 
@@ -444,6 +475,11 @@ class DataFile:
         return [vx_impact,vy_impact,vz_impact]
 
     def grab_impact_vel_trial(self):
+        """Returns avg vel at impact for final two episodes
+
+        Returns:
+            [np.array]: [vx_impact,vy_impact,vz_impact]
+        """
     
         num_rollouts = int(self.trial_df.iloc[-1]['n_rollouts'])
 
@@ -461,7 +497,15 @@ class DataFile:
 
 
     def grab_impact_omega(self,k_ep,k_run):
+        """Returns omega at time of impact
 
+        Args:
+            k_ep (int): Episode number
+            k_run (int): Run number
+
+        Returns:
+            list: [wx_impact,wy_impact,wz_impact]
+        """        
         run_df = self.select_run(k_ep,k_run)
         t_impact,_ = self.grab_impact_time(k_ep,k_run)
 
@@ -473,10 +517,10 @@ class DataFile:
 
     def grab_impact_omega_trial(self):
 
-        """Returns avg omega at flip for final two episodes
+        """Returns avg omega at impact for final two episodes
 
         Returns:
-            [np.array]: [wx,wy,wz]
+            [np.array]: [wx_impact,wy_impact,wz_impact]
         """        
         num_rollouts = int(self.trial_df.iloc[-1]['n_rollouts'])
 
