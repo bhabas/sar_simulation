fb9483846744321bb74b8cb9e6fd6fdf046712b9
bhabas <bhabas@psu.edu>
2020-12-29 11:13:14 -0500

    Launching and reward msg fix

--- src/crazyflie_rl/src/crazyflie_env.py
+++ src/crazyflie_rl/src/crazyflie_env.py
@@ -212,25 +212,25 @@ class CrazyflieEnv:
 
             print("[STARTING] Starting Gazebo Process...")
             self.gazebo_p = subprocess.Popen( # Gazebo Process
-                "gnome-terminal --disable-factory  -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_gazebo_sim/src/utility/launch_gazebo.bash", 
+                "gnome-terminal --disable-factory  -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/utility/launch_gazebo.bash", 
                 close_fds=True, preexec_fn=os.setsid, shell=True)
             time.sleep(5)
 
             print("[STARTING] Starting Controller Process...")
             self.controller_p = subprocess.Popen( # Controller Process
-                "gnome-terminal --disable-factory --geometry 81x33 -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_gazebo_sim/src/utility/launch_controller.bash", 
+                "gnome-terminal --disable-factory --geometry 81x33 -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/utility/launch_controller.bash", 
                 close_fds=True, preexec_fn=os.setsid, shell=True)
 
         else:
             print("[STARTING] Starting Gazebo Process...")
             self.gazebo_p = subprocess.Popen( # Gazebo Process
-                "gnome-terminal --disable-factory -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_gazebo_sim/src/utility/launch_gazebo.bash", 
+                "gnome-terminal --disable-factory -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/utility/launch_gazebo.bash", 
                 close_fds=True, preexec_fn=os.setsid, shell=True)
             time.sleep(5)
 
             print("[STARTING] Starting Controller Process...")
             self.controller_p = subprocess.Popen( # Controller Process
-                "gnome-terminal --disable-factory --geometry 81x33 -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_gazebo_sim/src/utility/launch_controller.bash", 
+                "gnome-terminal --disable-factory --geometry 81x33 -- ~/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/utility/launch_controller.bash", 
                 close_fds=True, preexec_fn=os.setsid, shell=True)
 
     def launch_dashboard(self):
