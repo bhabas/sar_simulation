11b67fe90e5ed1b44d18cd5031bc80ba265a4ec0
bhabas <bhabas@psu.edu>
2021-01-08 19:11:13 -0500

    Working examples for c++ pub and sub

--- src/ros_nodes/abc_qa/src/abc_code.cpp
+++ src/ros_nodes/abc_qa/src/abc_code.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 
+// working example for c++ publisher
+// https://www.youtube.com/watch?v=s7kluSveBuE&list=PLK0b4e05LnzbrLrLhOhSvLdaQZOsJl59N&index=68&t=598s
 int main(int argc, char** argv)
 {
     //Initialize and start ros node
