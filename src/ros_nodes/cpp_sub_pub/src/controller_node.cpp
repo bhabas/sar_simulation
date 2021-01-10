fb11832c5d4bb19119b461beaf8746b630a2ccdc
bhabas <bhabas@psu.edu>
2021-01-10 15:05:34 -0500

    Working mock controller

--- src/ros_nodes/cpp_sub_pub/src/controller_node.cpp
+++ src/ros_nodes/cpp_sub_pub/src/controller_node.cpp
@@ -4,45 +4,52 @@
 #include <std_msgs/Int64.h>
 #include <gazebo_communication_pkg/GlobalState.h>
 
+#include "controller_node.h"
 
+#include "cpp_sub_pub/my_msg.h"
 
 // Reference: https://roboticsbackend.com/oop-with-ros-in-cpp/
 
-class NumberCounter {
-
-    private:
-    int counter;
-    ros::Publisher pub;
-    ros::Subscriber number_subscriber;
 
 
-    public:
-    void callback_number(const gazebo_communication_pkg::GlobalState::ConstPtr &msg);
-    NumberCounter(ros::NodeHandle *nh) {
-        counter = 0;
-        
-        pub = nh->advertise<std_msgs::Int64>("/number_count",10);
-        number_subscriber = nh->subscribe("/global_state",1000,
-            &NumberCounter::callback_number,this);
+void Controller::callback_number(const gazebo_communication_pkg::GlobalState::ConstPtr &msg){
+    const ros::Time a = msg->header.stamp;
+    const geometry_msgs::Point b = msg->global_pose.position;
+    alpha = b.z;
+    // std::cout << b.z << std::endl;
 
-    }
+}
+
+void Controller::controlThread(){
+
+  float motorspeed[4];
+  float i=0;
+  ros::Rate rate(200);
+  while(ros::ok()){
+    i+=1;
+    std::cout << alpha << std::endl;
+    cpp_sub_pub::my_msg msg;
+    msg.motorspeeds = {1+i,2+i,3+i,4+i};
+    pub.publish(msg);
+    rate.sleep();
 
 
-};
-
-void NumberCounter::callback_number(const gazebo_communication_pkg::GlobalState::ConstPtr &msg){
-
-    std::cout << "test" << std::endl;
+  }
 }
 
 
 
 int main(int argc, char **argv)
 {
-    std::cout << "test" << std::endl;
-    ros::init(argc, argv,"number_counter");
+    
+    ros::init(argc, argv,"controller_node");
     ros::NodeHandle nh;
-    NumberCounter nc = NumberCounter(&nh);
-
+    Controller controller = Controller(&nh);
     ros::spin();
+
+    // while(ros::ok()){
+
+    //   // std::cout << controller.alpha << std::endl;
+    //   int c = 0;
+    // }
 }
