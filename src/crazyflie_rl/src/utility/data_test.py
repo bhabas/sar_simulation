51f121d1c4ff7842b35b6583345df2b306695e67
bhabas <bhabas@psu.edu>
2021-01-26 11:56:12 -0500

    Updated landing rate function and landing plot

--- src/crazyflie_rl/src/utility/data_test.py
+++ src/crazyflie_rl/src/utility/data_test.py
@@ -7,21 +7,26 @@ import os
 from data_analysis import DataFile
 os.system("clear")
 
-vz = 3.5
-vx = 1.5
-trialNum = 0
+vz = 4.00
+vx = 0.00
+trialNum = 2
+agent = "EM_PEPG"
 
-filepath = f"/home/bhabas/catkin_ws/src/crazyflie_simulation/local_files/data/Vz_{vz:.2f}--Vx_{vx:.2f}--trial_{trialNum}.csv"
+# filepath = f"/home/bhabas/catkin_ws/src/crazyflie_simulation/local_files/data/Vz_{vz:.2f}--Vx_{vx:.2f}--trial_{trialNum}.csv"
 # filepath = f"/home/bhabas/catkin_ws/src/crazyflie_simulation/src/crazyflie_gazebo_sim/src/log/Vz_{vz}--Vx_{vx}--trial_{trialNum}.csv"
-filepath = "/home/bhabas/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/log/EM_PEPG--Vz_4.00--Vx_1.00--trial_1.csv"
+# filepath = "/home/bhabas/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/log/EM_PEPG--Vz_4.00--Vx_1.00--trial_1.csv"
+filepath = f"/home/bhabas/catkin_ws/src/crazyflie_simulation/src/crazyflie_rl/src/log/{agent}--Vz_{vz:.2f}--Vx_{vx:.2f}--trial_{trialNum}.csv"
 
 
 
 trial = DataFile(filepath)
 
-k_ep = 19
-k_run = 9
+k_ep = 16
+k_run = 5
 
-# print(trial.grab_eulerData(k_ep,k_run)[0])
-print(trial.grab_impact_state(k_ep,k_run,'vx'))
-trial.plot_state(k_ep,k_run,'x')
+
+# trial.plot_rewardData()
+# trial.plot_state(k_ep,k_run,'z')
+
+print(trial.landing_rate())
+trial.landing_plot()
