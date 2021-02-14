1fb87ef2506c4c11787c237ee2cbbca359454b96
Bader AlAttar <baderalattar22@gmail.com>
2021-02-14 15:15:30 -0800

    add new EPHE class with adaptive/manual modes

--- src/crazyflie_rl/src/rl_EM.py
+++ src/crazyflie_rl/src/rl_EM.py
@@ -93,3 +93,95 @@ class rlEM_AdaptiveAgent(rlEM_PEPGAgent):
 
         self.mu_history = np.append(self.mu_history, self.mu, axis=1)
         self.sigma_history = np.append(self.sigma_history, self.sigma, axis=1)
+
+
+
+
+class EPHE_Agent(ES):  # EM Policy Hyper Paramter Exploration
+     def __init__(self, mu, sigma, gamma = 0.95, n_rollout = 5, adaptive = True, lamda = 1):
+         self.gamma, self.n_rollout = gamma, n_rollout
+
+         self.mu = mu
+         self.sigma = sigma
+
+         self.adaptive = adaptive
+         self.n = 2*n_rollout  # total runs per episode
+         self.d = len(self.mu) # problem dimension
+         self.alpha_mu, self.alpha_sigma  = np.array([[0],[0]]),np.array([[0],[0]])  # placeholder for compatibility with pepg agent
+
+
+         if lamda < 1 or lamda > self.n:
+             raise ValueError("lamda must be between 1 and 2*n_rollout")
+         else:
+             self.lamda = lamda
+
+         self.mu_history = copy.copy(self.mu)  # Creates another array of self.mu and attaches it to self.mu_history
+         self.sigma_history = copy.copy(self.sigma)
+         self.reward_history = None
+
+     def get_theta(self):
+         theta = np.zeros((self.d,self.n))
+
+         for dim in range(0,self.d):
+             theta[dim,:] = np.random.normal(self.mu[dim,0],self.sigma[dim,0],[1,self.n])
+
+         for k_n in range(self.n):
+             for dim in range(self.d):
+                 if theta[dim,k_n] < 0: 
+                     theta[dim,k_n] = 0.001 
+
+         return theta , 0
+
+
+     def train(self,theta,reward,epsilon):
+         reward_mean = np.mean(reward)
+
+         if self.reward_history is None:  # initizlize np array for first rollout, o.w. append 
+             self.reward_history = np.array(reward_mean)
+         else:
+             self.reward_history = np.append(self.reward_history, reward_mean)
+
+         reward_b = np.zeros_like(reward)
+         b = self.get_baseline(span=3)
+
+         for count,r in enumerate(reward):  # reward used for adaptive case
+             reward_b[count] = max(0,r - b)
+
+
+         summary = np.concatenate((theta.T,reward,reward_b),axis=1)   # sort rollouts from highest to lowest reward
+         summary = (summary[summary[:,self.d].argsort()[::-1]]).T
+
+         if not self.adaptive:   # consider base reward for top lamda members if not adaptive
+             summary[self.d+1,:self.lamda] = summary[self.d,:self.lamda]
+             summary[self.d+1,self.lamda:] = np.zeros((1,self.n-self.lamda))
+
+         print(summary)
+
+
+         S_theta = summary[0:self.d,:]@summary[self.d+1,:].reshape(self.n,1)   # sigma and mu update terms
+         S_reward = np.sum(summary[self.d+1,:])
+         S_diff = np.square(summary[0:self.d,:] - self.mu)@(summary[self.d+1,:].reshape(self.n,1))
+
+
+         self.mu =  S_theta/(S_reward +0.001)
+         self.sigma = np.sqrt(S_diff/(S_reward + 0.001))
+
+         self.mu_history = np.append(self.mu_history, self.mu, axis=1)
+         self.sigma_history = np.append(self.sigma_history, self.sigma, axis=1)
+
+
+
+
+
+ if __name__ == "__main__":
+     np.set_printoptions(precision=2, suppress=True)
+     mu = np.array([[1.0],[-4.0]])
+     sigma = np.array([[0.1],[1.5]])
+
+     agent = EPHE_Agent(mu,sigma)
+
+     theta,epsilon = agent.get_theta()
+     reward = np.array([[1.0],[2.0],[0.3],[4.0],[2.5],[1.2] ])
+
+     reward = np.array([[1.0],[2.0],[0.3],[4.0],[2.5],[1.2],[0.3],[4.0],[2.5],[1.2]  ])
+     agent.train(theta,reward,0)
