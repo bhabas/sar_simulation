5eecead9b32cdc9d794c9f0255a863796393757b
bhabas <bhabas@psu.edu>
2021-08-02 10:28:24 -0400

    List creater update

--- crazyflie_data/data_collection/test_list_creator.py
+++ crazyflie_data/data_collection/test_list_creator.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-List = []
+test_list = []
 for V_d in np.arange(1.0,4.25,0.5):    # [m/s]
     for phi in np.arange(90,15,-7.5):      # [deg]
         for trial_num in np.arange(3,8,1):
-            List.append([V_d,phi,trial_num])
+            test_list.append([V_d,phi,trial_num])
+
+test_list = np.array(test_list)
+test_list = np.flip(test_list,axis=0)
 
 
-print(np.asarray(List))
 np.set_printoptions(suppress=True)
-np.savetxt("crazyflie_data/data_collection/MasterTestList.csv", np.asarray(List), delimiter=",",fmt="%.1f",header='vz_d,vx_d,trial_num')
+np.savetxt("crazyflie_data/data_collection/MasterTestList.csv", np.asarray(test_list), delimiter=",",fmt="%.1f",header='vz_d,vx_d,trial_num')
         
 
 
