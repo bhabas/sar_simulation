a3f865063899eb0956df5e53142cd27d524a3640
bhabas <bhabas@psu.edu>
2021-08-09 22:22:25 -0400

    Test list update

--- crazyflie_data/data_collection/test_list_creator.py
+++ crazyflie_data/data_collection/test_list_creator.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 test_list = []
-for V_d in np.arange(1.0,4.25,0.5):    # [m/s]
-    for phi in np.arange(90,15,-2.5):      # [deg]
+for V_d in np.arange(2.5,4.25,0.5):    # [m/s]
+    for phi in np.concatenate((np.arange(90,40,-10),np.arange(45,15,-2.5))):      # [deg]
         for trial_num in np.arange(0,5,1):
             test_list.append([V_d,phi,trial_num])
 
