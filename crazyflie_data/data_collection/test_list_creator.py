e783c6a04ae233badcfa8e91335a01c943176201
bhabas <bhabas@psu.edu>
2021-08-31 22:11:58 -0400

    Test list updates

--- crazyflie_data/data_collection/test_list_creator.py
+++ crazyflie_data/data_collection/test_list_creator.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 test_list = []
-RREV_list = [1.0,2.0,3.0,4.0,5.0]
+RREV_list = [1.0,3.0,5.0]
 
-for V_d in np.concatenate((np.arange(0.25,2.0,0.25),np.arange(2.0,4.0,0.5))):    # [m/s]
+for V_d in np.concatenate((np.arange(0.25,2.0,0.25),np.arange(2.0,4.25,0.25))):    # [m/s]
     for phi in np.arange(90,15,-5):      # [deg]
-        for ii,trial_num in enumerate(np.arange(20,25,1)):
+        for ii,trial_num in enumerate(np.arange(20,23,1)):
             test_list.append([V_d,phi,trial_num,RREV_list[ii]])
 
 test_list = np.array(test_list)
@@ -13,7 +13,7 @@ test_list = np.array(test_list)
 
 
 np.set_printoptions(suppress=True)
-np.savetxt("crazyflie_data/data_collection/MasterTestList.csv", np.asarray(test_list), delimiter=",",fmt="%.2f",header='vz_d,vx_d,trial_num')
+np.savetxt("crazyflie_data/data_collection/MasterTestList.csv", np.asarray(test_list), delimiter=",",fmt="%.2f",header='vz_d,vx_d,trial_num,RREV')
         
 
 
