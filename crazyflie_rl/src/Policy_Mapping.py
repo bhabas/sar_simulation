535c35255c8cc5139d3f2583e71b052ed26fb1d2
bhabas <bhabas@psu.edu>
2021-10-11 20:20:32 -0400

    Inserted more code

--- crazyflie_rl/src/Policy_Mapping.py
+++ crazyflie_rl/src/Policy_Mapping.py
@@ -16,7 +16,7 @@ from rospy.exceptions import ROSException
 os.system("clear")
 np.set_printoptions(precision=2, suppress=True)
 
-def runfunction(vel,phi,d_ceiling,My):
+def runfunction(env,vel,phi,d_ceiling,My):
 
     ## INIT LAUNCH/FLIGHT CONDITIONS
     phi_rad = np.radians(phi)
@@ -37,58 +37,139 @@ def runfunction(vel,phi,d_ceiling,My):
     ##          Run 
     # ============================
 
-    try: # Use try block to catch raised exceptions and attempt rollout again
-
-        ## IF CONTROLLER FAILS, RELAUNCH IT
-        try:
-            rospy.wait_for_message("/ctrl_data",CtrlData,timeout=5.0)
-        except rospy.exceptions.ROSException:
-            env.launch_controller()
-            time.sleep(2)
-            env.reset_pos()
-
-
-        ## RESET TO INITIAL STATE
+    ## IF CONTROLLER FAILS, RELAUNCH IT
+    try:
+        rospy.wait_for_message("/ctrl_data",CtrlData,timeout=5.0)
+    except rospy.exceptions.ROSException:
+        env.launch_controller()
+        time.sleep(2)
         env.reset_pos()
-        env.step('home') # Reset control vals and functionality to default vals
-        time.sleep(0.65) # Time for CF to settle [Real-Time seconds]
+
+
+    ## RESET TO INITIAL STATE
+    env.reset_pos()
+    env.step('home') # Reset control vals and functionality to default vals
+    time.sleep(0.65) # Time for CF to settle [Real-Time seconds]
+    
+
+
+    ## RESET/UPDATE RUN CONDITIONS
+    repeat_run= False
+
+    start_time_rollout = env.getTime()
+    start_time_pitch = np.nan
+    start_time_impact = np.nan
+
+    ## RESET LOGGING CONDITIONS 
+    
+    
+    t_step = 0
+    t_prev = 0.0
+
+
+    
+
+    
+    flag = False # Ensures flip data recorded only once (Needs a better name)
+    flag2 = False # Ensures impact data recorded only once (Needs a better name)
+
+
+    pos_z = env.h_ceiling - d_ceiling
+
+    env.step('pos',ctrl_flag=0)                     # Turn off pos control
+    env.step('vel',env.vel_d,ctrl_flag=1)           # Set desired vel
+    env.step('sticky',ctrl_flag=1)                  # Enable sticky pads
+    env.launch_IC(pos_z,env.vel_d[0]+0.03,env.vel_d[2])   # Use Gazebo to impart desired vel with extra vx to ensure -OF_y when around zero
+    env.step('moment',[0,-My*1e-3,0],ctrl_flag=1)
+
+    while True:
+
+
+
+        # ============================
+        ##      Pitch Recording 
+        # ============================
+        if (env.flip_flag == True and flag == False):
+            start_time_pitch = env.getTime() # Starts countdown for when to reset run
+
+            # Recieve flip moments from controller and then update class var to be sent out of /rl_data topic
+            Mx_d = env.FM_flip[1] 
+            My_d = env.FM_flip[2]
+            Mz_d = env.FM_flip[3]
+            env.M_d = [Mx_d,My_d,Mz_d] # [N*mm]
+
         
-
-
-        ## RESET/UPDATE RUN CONDITIONS
-        repeat_run= False
-
-        start_time_rollout = env.getTime()
-        start_time_pitch = np.nan
-        start_time_impact = np.nan
-
-        ## RESET LOGGING CONDITIONS 
-        
-        
-        t_step = 0
-        t_prev = 0.0
-
+            print("----- pitch starts -----")
+            print(f"vx={vx:.3f}, vy={vy:.3f}, vz={vz:.3f}")
+            print(f"RREV_tr={env.RREV_tr:.3f}, OF_y_tr={env.OF_y_tr:.3f}, My_d={My_d:.3f} [N*mm]")  
+            print(f"Pitch Time: {env.state_flip[0]:.3f} [s]")
+            
+            flag = True # Turns on to make sure this only runs once per rollout
 
         
+        if ((env.impact_flag or env.body_contact) and flag2 == False):
+            start_time_impact = env.getTime()
+            flag2 = True
 
-        
-        flag = False # Ensures flip data recorded only once (Needs a better name)
-        flag2 = False # Ensures impact data recorded only once (Needs a better name)
+        # ============================
+        ##    Termination Criteria 
+        # ============================
+        if (env.impact_flag or env.body_contact) and ((env.getTime()-start_time_impact) > 1.0):
+            env.error_str = "Rollout Completed: Impact Timeout"
+            print(env.error_str)
+
+            env.runComplete_flag = True
+
+        # IF TIME SINCE TRIGGERED PITCH EXCEEDS [1.5s]  
+        elif env.flip_flag and ((env.getTime()-start_time_pitch) > (2.25)):
+            env.error_str = "Rollout Completed: Pitch Timeout"
+            print(env.error_str)
+
+            env.runComplete_flag = True
+
+        # IF POSITION FALLS BELOW FLOOR HEIGHT
+        elif env.position[2] <= -8.0: # Note: there is a lag with this at high RTF
+            env.error_str = "Rollout Completed: Falling Drone"
+            print(env.error_str)
+
+            env.runComplete_flag = True
+
+        # IF TIME SINCE RUN START EXCEEDS [6.0s]
+        elif (env.getTime() - start_time_rollout) > (5.0):
+            env.error_str = "Rollout Completed: Time Exceeded"
+            print(env.error_str)
+
+            env.runComplete_flag = True
 
 
-        pos_z = env.h_ceiling - d_ceiling
+        # ============================
+        ##          Errors  
+        # ============================
 
-        env.step('pos',ctrl_flag=0)                     # Turn off pos control
-        env.step('vel',env.vel_d,ctrl_flag=1)           # Set desired vel
-        env.step('sticky',ctrl_flag=1)                  # Enable sticky pads
-        env.launch_IC(pos_z,env.vel_d[0]+0.03,env.vel_d[2])   # Use Gazebo to impart desired vel with extra vx to ensure -OF_y when around zero
-        env.step('moment',[0,-My*1e-3,0],ctrl_flag=1)
+        ## IF NAN IS FOUND IN STATE VECTOR REPEAT RUN (Model collision Error)
+        if any(np.isnan(env.state_current)): 
+            env.error_str = "Error: NAN found in state vector"
+            print(env.error_str)
+            repeat_run = True
+            break
 
-        env.step('sticky',ctrl_flag=0)
+        if np.abs(env.position[1]) >= 1.0: # If CF goes crazy it'll usually shoot out in y-direction
+            env.error_str = "Error: Y-Position Exceeded"
+            print(env.error_str)
+            repeat_run = True
+            break
 
 
-    except:
-        pass
+        # ============================
+        ##       Run Completion  
+        # ============================
+        if env.runComplete_flag==True:
+            pass
+
+        ## =======  RUN COMPLETED  ======= ##
+        if repeat_run == True: # Runs when error detected
+            env.relaunch_sim()
+    
 
 
 
@@ -110,5 +191,5 @@ if __name__ == '__main__':
             print("Trials are over")
             break
 
-        runfunction(vel,phi,d_ceiling,My)
+        runfunction(env,vel,phi,d_ceiling,My)
 
