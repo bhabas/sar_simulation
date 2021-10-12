79d1689a800ca870263dd26883dd1330e41a8876
bhabas <bhabas@psu.edu>
2021-10-11 22:07:27 -0400

    Working directory import thing I dont understand

---
+++ Projects/Policy_Mapping/TestFile.py
@@ -0,0 +1,26 @@
+import numpy as np
+import time,os
+import rospy
+from crazyflie_msgs.msg import ImpactData,CtrlData
+import math
+import pandas as pd
+
+
+import sys
+cwd = os.getcwd()
+sys.path.insert(0, f'{cwd}/')
+print(sys.path)
+# from crazyflie_rl.src.Crazyflie_env import CrazyflieEnv
+from crazyflie_rl.src.Crazyflie_env import CrazyflieEnv
+from rospy.exceptions import ROSException
+
+os.system("clear")
+np.set_printoptions(precision=2, suppress=True)
+
+
+
+if __name__ == '__main__':
+
+    ## INIT GAZEBO ENVIRONMENT
+    env = CrazyflieEnv(gazeboTimeout=False)
+    # env.launch_dashboard()
