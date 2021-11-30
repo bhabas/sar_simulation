801f4cf899704d57da853b4cae22978df4c8dbb3
bhabas <bhabas@psu.edu>
2021-11-30 10:37:54 -0500

    Combined NN but terrible performance

--- crazyflie_projects/Policy_Mapping/NeuralNetwork/func_approx_1D_2D.py
+++ crazyflie_projects/Policy_Mapping/NeuralNetwork/func_approx_1D_2D.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 
 
 def myfun(x,range_min,range_max):
-    y = np.sin(x)
+    y = x**2
     y_valid = [1 if range_min < i < range_max else 0 for i in x]
     y_valid = np.reshape(y_valid,(-1,1))
 
@@ -23,7 +23,7 @@ def myfun(x,range_min,range_max):
 
 ## DEFINE NN MODEL
 class Model(nn.Module):
-    def __init__(self,in_features=1,h1=8,h2=8,out_features=1):
+    def __init__(self,in_features=1,h1=16,h2=16,out_features=1):
         super().__init__()
 
         # Input Layer (4 features) --> h1 (N) --> h2 (N) --> output (3 classes)
@@ -36,32 +36,38 @@ class Model(nn.Module):
         # PASS DATA THROUGH NETWORK
         x = F.tanh(self.fc1(x))
         x = F.tanh(self.fc2(x))
-        x = F.tanh(self.out(x))
+        x1 = self.out(x)
+        x2 = F.tanh(self.out(x))
         ## Try NN to predict valid region and then combine both
 
-        return x
+        return x1,x2
 
 
-def train_model(X_train,y_train):
+def train_model(X_train,y_train1,y_train2):
     ## INITIALIZE NEURAL NETWORK MODEL
-    torch.manual_seed(32)
+    torch.manual_seed(3)
     model = Model()
 
     ## DEFINE OPTIMIZER AND LOSS FUNCTION (CRITERION)
-    criterion = nn.MSELoss(reduction='mean')
+    criterion1 = nn.MSELoss(reduction='mean')
+    criterion2 = nn.MSELoss(reduction='mean')
+
     optimizer = torch.optim.Adam(model.parameters(),lr=0.01) # Model parameters are the layers of model
 
 
-    epochs = 10000
+    epochs = 5000
     losses = []
 
     for i in range(epochs):
 
         ## MODEL PREDICTION
-        y_pred = model.forward(X_train)
+        y_pred1,y_pred2 = model.forward(X_train)
 
         ## CALCULATE LOSS/ERROR
-        loss = criterion(y_pred,y_train)
+        loss1 = criterion1(y_pred1,y_train1)
+        loss2 = criterion2(y_pred2,y_train2)
+        loss = loss1 + loss2
+
         losses.append(loss)
 
         if i%10 == 1:
@@ -154,16 +160,15 @@ if __name__ == '__main__':
     X_train = torch.FloatTensor(train[['X']].to_numpy())
     X_test = torch.FloatTensor(test[['X']].to_numpy())
 
-    y_train = torch.FloatTensor(train[['y_valid']].to_numpy())
-    y_test = torch.FloatTensor(test[['y_valid']].to_numpy())
+    y_train1 = torch.FloatTensor(train[['y']].to_numpy())
+    y_test1 = torch.FloatTensor(test[['y']].to_numpy())
 
-    # train_model(X_train,y_train)
+    y_train2 = torch.FloatTensor(train[['y_valid']].to_numpy())
+    y_test2 = torch.FloatTensor(test[['y_valid']].to_numpy())
+
+    train_model(X_train,y_train1,y_train2)
     model = torch.load('Func_approx_1D_2D.pt')
     # test_model(model)
-
-
-
-
 
 
 
@@ -171,15 +176,35 @@ if __name__ == '__main__':
     X_eval = np.linspace(-3,10,50).reshape(-1,1)
     X_eval = torch.FloatTensor(X_eval)
     with torch.no_grad():
-        y_eval = model.forward(X_eval)
+        y_eval1,y_eval2 = model.forward(X_eval)
 
 
 
     ## PLOT NETWORK OUTPUTS
     fig = plt.figure(1, figsize=(12,6))
 
+    ax1 = fig.add_subplot(2,2,1)
+    ax1.plot(X_eval,y_eval1,color='blue')
+    ax1.plot(X_train,myfun(X_train,range_min,range_max)[0],'.')
+
+    ax1.grid()
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('f(x)')
+    ax1.set_title('%d neurons in hidden layer with %d epochs of training' % (8,10000))
+    ax1.legend(['Function: f(x)', 'Neural Network: g(x)', 'Training Set'])
+
+    ax2 = fig.add_subplot(2,2,2)
+    ax2.plot(X_eval,np.abs(y_eval1 - myfun(X_eval,range_min,range_max)[0]))
+
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('Error')
+    ax2.set_title('Absolute difference between prediction and actual function')
+    ax2.legend(['Error |f(x)-g(x)|'])
+
+
+
     ax3 = fig.add_subplot(2,2,3)
-    ax3.plot(X_eval,y_eval,color='red')
+    ax3.plot(X_eval,y_eval2,color='red')
     ax3.axvspan(range_min, range_max, alpha=0.15, color='limegreen')
 
     ax3.grid()
@@ -187,8 +212,8 @@ if __name__ == '__main__':
     ax3.set_ylabel('f(x)')
 
     ax4 = fig.add_subplot(2,2,4)
-    ax4.plot(X_eval,np.abs(y_eval-myfun(X_eval,range_min,range_max)[1]))
+    ax4.plot(X_eval,np.abs(y_eval2-myfun(X_eval,range_min,range_max)[1]))
     ax4.axvspan(range_min,range_max, alpha=0.15, color='limegreen')
 
-
+    fig.tight_layout()
     plt.show()
