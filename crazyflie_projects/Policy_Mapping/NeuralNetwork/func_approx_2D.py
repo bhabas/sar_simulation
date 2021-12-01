795a31a398ca301e498cf9dcbd44c877bff25bc8
bhabas <bhabas@psu.edu>
2021-12-01 11:20:29 -0500

    Working 2D function approximator network

--- crazyflie_projects/Policy_Mapping/NeuralNetwork/func_approx_2D.py
+++ crazyflie_projects/Policy_Mapping/NeuralNetwork/func_approx_2D.py
@@ -1,29 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-import os
-
+import pandas as pd
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 
+from sklearn.model_selection import train_test_split
 
-## DESIGNATE FUNCTION USED
-def my2dfun(x1, x2,function_select):
-    functions = {
-        1: np.power(x1,2)-np.power(x2,2), # quadratic function
-        2: np.sin(np.sqrt(x1**2 + x2**2)) # sinus
-    }
-    return functions.get(function_select)
 
+def myfun(x1,x2):
+    y1 = np.power(x1,2)-np.power(x2,2)
+    y2 = np.sin(np.sqrt(x1**2 + x2**2)) # sinus
+
+    return y1,y2
 
 ## DEFINE NN MODEL
 class Model(nn.Module):
-    def __init__(self,in_features=2,h1=8,h2=8,out_features=1):
+    def __init__(self,in_features=2,h1=10,h2=10,out_features=2):
         super().__init__()
 
         # Input Layer (4 features) --> h1 (N) --> h2 (N) --> output (3 classes)
@@ -34,110 +29,176 @@ class Model(nn.Module):
     def forward(self,x):
 
         # PASS DATA THROUGH NETWORK
-        x = F.tanh(self.fc1(x))
-        x = F.tanh(self.fc2(x))
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
         x = self.out(x)
 
         return x
 
+def train_model(epochs,X_train,y_train):
+
+    ## INITIALIZE NEURAL NETWORK MODEL
+    torch.manual_seed(22)
+    model = Model()
+
+    criterion = nn.MSELoss(reduction='mean')
+
+    optimizer = torch.optim.Adam(model.parameters(),lr=0.01) #  Model parameters are the layers of model
+    losses = []
+
+    for ii in range(epochs):
+
+        ## MODEL PREDICTION
+        y_pred = model.forward(X_train)
 
 
-## GENERATE TRAINING DATA
-batch_size = 50
-function_select = 1
+        ## CALCULATE LOSS/ERROR
+        loss = criterion(y_pred,y_train)
+        losses.append(loss)
 
-x1 = np.linspace(-3.0, 3.0, num=batch_size)
-x2 = np.linspace(-3.0, 3.0, num=batch_size)
+        if ii%10 == 1:
+            print(f"epoch {ii} and loss is: {loss}")
 
-X1,X2 = np.meshgrid(x1,x2)
+        ## BACKPROPAGATION
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-X = np.stack((X1.flatten(),X2.flatten()),axis=1)
-
-
-
-## GENERATE LABELS
-y = my2dfun(X1,X2,function_select).reshape(-1,1)
-
+    torch.save(model,'Func_approx_2D.pt')
+    return model
 
 
-## SPLIT DATA FEATURES INTO TRAINING DATA & TESTING DATA
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=33)
+if __name__ == '__main__':
+
+    ## GENERATE TRAINING DATA
+    batch_size = 50
+    epochs = 10_000
+
+    range_train = [-3,3]
+
+    x1 = np.linspace(range_train[0],range_train[1],batch_size).reshape(-1,1)
+    x2 = np.linspace(range_train[0],range_train[1],batch_size).reshape(-1,1)
+    X1,X2 = np.meshgrid(x1,x2)
+
+    y1,y2 = myfun(X1,X2)
+
+    data_array = np.stack((X1.flatten(),X2.flatten(),y1.flatten(),y2.flatten())).T
+    df = pd.DataFrame(data_array,columns=['X1','X2','y1','y2'])
+
+    ## SPLIT DATA FEATURES INTO TRAINING AND TESTING DATA
+    train, test = train_test_split(df,test_size=0.2,random_state=33)
+
+    ## CONVERT DATA INTO TENSORS
+    X_train = torch.FloatTensor(train[['X1','X2']].to_numpy())
+    X_test = torch.FloatTensor(test[['X1','X2']].to_numpy())
+
+    y_train = torch.FloatTensor(train[['y1','y2']].to_numpy())
+    y_test = torch.FloatTensor(test[['y1','y2']].to_numpy())
 
 
-## CONVERT DATA INTO TENSORS
-X_train = torch.FloatTensor(X_train)
-X_test = torch.FloatTensor(X_test)
-
-y_train = torch.FloatTensor(y_train)
-y_test = torch.FloatTensor(y_test)
 
 
-## INITIALIZE NEURAL NETWORK MODEL
-torch.manual_seed(32)
-model = Model()
-
-## DEFINE OPTIMIZER AND LOSS FUNCTION (CRITERION)
-criterion = nn.MSELoss(reduction='mean')
-optimizer = torch.optim.Adam(model.parameters(),lr=0.01) # Model parameters are the layers of model
 
 
-epochs = 10000
-losses = []
 
-for i in range(epochs):
 
-    ## MODEL PREDICTION
-    y_pred = model.forward(X_train)
+    train_model(epochs,X_train,y_train)
+    model = torch.load('Func_approx_2D.pt')
 
-    ## CALCULATE LOSS/ERROR
-    loss = criterion(y_pred,y_train)
-    losses.append(loss)
+    ## DEFINE EVALUATION RANGE 
+    x1_eval = np.linspace(-3,3,10).reshape(-1,1)
+    x2_eval = np.linspace(-3,3,10).reshape(-1,1)
+    X1_eval,X2_eval = np.meshgrid(x1_eval,x2_eval)
+    X_eval = np.stack((X1_eval.flatten(),X2_eval.flatten()),axis=1)
+    X_eval = torch.FloatTensor(X_eval)
 
-    if i%10 == 1:
-        print(f"epoch {i} and loss is: {loss}")
 
+    with torch.no_grad():
+        y_eval = model.forward(X_eval)
+
+
+    fig = plt.figure(1,figsize=(12,6))
+    ds = 10 # Downscale
+
+    ## SUBPLOT 1 (FUNCTION)
+    ax1 = fig.add_subplot(2,3,1,projection='3d')
+    ax1.plot_trisurf(
+        X_eval[:,0].flatten(),
+        X_eval[:,1].flatten(),
+        myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[0],
+        cmap='viridis',linewidth=0.2,antialiased=True)
+    ax1.set_xlabel('x1')
+    ax1.set_ylabel('x2')
+    ax1.set_title('Function')
+    ax1.set_xlim(-3,3)
+    ax1.set_ylim(-3,3)
+
+    ## SUBPLOT 2 (PREDICTION)
+    ax2 = fig.add_subplot(2,3,2,projection='3d')
+    ax2.plot_trisurf(
+        X_eval[:,0].flatten(),
+        X_eval[:,1].flatten(),
+        y_eval[:,0].flatten(),
+        cmap='viridis',linewidth=0.2,antialiased=True)
+    ax2.set_xlabel('x1')
+    ax2.set_ylabel('x2')
+    ax2.set_title('Prediction')
+    ax2.set_xlim(-3,3)
+    ax2.set_ylim(-3,3)
+
+    ## SUBPLOT 3 (ERROR)
+    ax2 = fig.add_subplot(2,3,3,projection='3d')
+    ax2.plot_trisurf(
+        X_eval[:,0].flatten(),
+        X_eval[:,1].flatten(),
+        np.abs(y_eval[:,0].flatten()-myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[0]),
+        cmap='viridis',linewidth=0.2,antialiased=True)
+    ax2.set_xlabel('x1')
+    ax2.set_ylabel('x2')
+    ax2.set_title('Error')
+    ax2.set_xlim(-3,3)
+    ax2.set_ylim(-3,3)
+
+    ## SUBPLOT 4 (FUNCTION)
+    ax1 = fig.add_subplot(2,3,4,projection='3d')
+    ax1.plot_trisurf(
+        X_eval[:,0].flatten(),
+        X_eval[:,1].flatten(),
+        myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[1],
+        cmap='viridis',linewidth=0.2,antialiased=True)
+    ax1.set_xlabel('x1')
+    ax1.set_ylabel('x2')
+    ax1.set_title('Function')
+    ax1.set_xlim(-3,3)
+    ax1.set_ylim(-3,3)
+
+    ## SUBPLOT 5 (PREDICTION)
+    ax2 = fig.add_subplot(2,3,5,projection='3d')
+    ax2.plot_trisurf(
+        X_eval[:,0].flatten(),
+        X_eval[:,1].flatten(),
+        y_eval[:,1].flatten(),
+        cmap='viridis',linewidth=0.2,antialiased=True)
+    ax2.set_xlabel('x1')
+    ax2.set_ylabel('x2')
+    ax2.set_title('Prediction')
+    ax2.set_xlim(-3,3)
+    ax2.set_ylim(-3,3)
+
+    ## SUBPLOT 6 (ERROR)
+    ax2 = fig.add_subplot(2,3,6,projection='3d')
+    ax2.plot_trisurf(
+        X_eval[:,0].flatten(),
+        X_eval[:,1].flatten(),
+        np.abs(y_eval[:,1].flatten()-myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[1]),
+        cmap='viridis',linewidth=0.2,antialiased=True)
+    ax2.set_xlabel('x1')
+    ax2.set_ylabel('x2')
+    ax2.set_title('Error')
+    ax2.set_xlim(-3,3)
+    ax2.set_ylim(-3,3)
     
 
-    ## BACKPROPAGATION
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
 
-
-
-# ## PLOT LOSS VS EPOCHS
-# plt.plot(range(epochs),losses)
-# plt.ylabel('Loss')
-# plt.xlabel('Epoch')
-
-
-
-## DEFINE EVALUATION RANGE
-# X_eval = np.linspace(-3,10,batch_size).reshape(-1,1)
-# X_eval = torch.FloatTensor(X_eval)
-with torch.no_grad():
-    y_eval = model.forward(X_test)
-
-with torch.no_grad():
-    y_eval2 = model.forward(torch.FloatTensor(np.array((2,2))))
-print(y_eval2)
-
-## PLOT NETWORK OUTPUTS
-fig = plt.figure(1, figsize=(12,6))
-
-ax1 = fig.add_subplot(2,3,1,projection='3d')
-ax1.plot_trisurf(X[::5,0],X[::5,1],y[::5].flatten(),cmap='viridis', linewidth=0.2, antialiased=True)
-ax1.set_xlabel('x1')
-ax1.set_ylabel('x2')
-ax1.set_title('Function')
-
-ax2 = fig.add_subplot(2,3,2,projection='3d')
-ax2.plot_trisurf(X_test[:,0],X_test[:,1],y_eval.flatten(),cmap='viridis', linewidth=0.2, antialiased=True)
-ax2.set_xlabel('x1')
-ax2.set_ylabel('x2')
-ax2.set_title('Prediction')
-
-
-
-fig.tight_layout()
-plt.show()
+    fig.tight_layout()
+    plt.show()
