385d0b0739ca839a1c6f376c5de47aa7dff717e1
bhabas <bhabas@psu.edu>
2021-12-02 09:24:18 -0500

    Tests for policy NN

--- crazyflie_projects/Policy_Mapping/NeuralNetwork/func_approx_My.py
+++ crazyflie_projects/Policy_Mapping/NeuralNetwork/func_approx_My.py
@@ -80,9 +80,10 @@ if __name__ == '__main__':
 
     ## GENERATE TRAINING DATA
     batch_size = 50
-    epochs = 50_000
+    epochs = 20_000
 
     df = pd.read_csv(f"{DATAPATH}/Wide-Long_2-Policy_Summary.csv")
+    df['flip_height_mean'] = 2.1 - df['flip_height_mean']
     df = df.query('landing_rate_4_leg >= 0.6')
 
     
@@ -112,156 +113,80 @@ if __name__ == '__main__':
         y_eval = model.forward(X_test)
 
     for ii in range(len(y_eval)):
-        # print(f"LR_test: {y_test[ii,1].numpy():.1f} \t LR_eval: {y_eval[ii,1].numpy():.1f}")
         print(f"My_test: {y_test[ii,0].numpy():.1f} \t My_eval: {y_eval[ii,0].numpy():.1f} \t%Error: {((y_test[ii,0]-y_eval[ii,0])/y_test[ii,0]).numpy():.2f}")
 
-    # ## DEFINE EVALUATION RANGE 
-    # x1_eval = np.linspace(-3,3,10).reshape(-1,1)
-    # x2_eval = np.linspace(-3,3,10).reshape(-1,1)
-    # X1_eval,X2_eval = np.meshgrid(x1_eval,x2_eval)
-    # X_eval = np.stack((X1_eval.flatten(),X2_eval.flatten()),axis=1)
-    # X_eval = torch.FloatTensor(X_eval)
+    ## PLOT REGIONS
+    fig = plt.figure(1,figsize=(10,5))
+    cmap = mpl.cm.jet
+    norm = mpl.colors.Normalize(vmin=0,vmax=10)
 
 
-    # with torch.no_grad():
-    #     y_eval = model.forward(X_eval)
+    ## SUBPLOT 1 (FUNCTION)
+    ax1 = fig.add_subplot(1,2,1,projection='3d')
+    ax1.scatter(
+        df['OF_y_flip_mean'],
+        df['RREV_flip_mean'],
+        df['flip_height_mean'],
+        c = df['My_d'],
+        cmap=cmap,norm=norm,alpha=0.8,depthshade=False
+    )
+
+    ax1.set_xlabel('OF_y')
+    ax1.set_xlim(-20,0)
+    ax1.set_xticks([-20,-15,-10,-5,0])
 
 
+    ax1.set_ylabel('RREV')
+    ax1.set_ylim(0,8)
+    ax1.set_yticks([0,2,4,6,8])
 
+    ax1.set_zlabel('d_ceiling')
+    ax1.set_zlim(0,1.0)
+    ax1.set_zticks([0,0.2,0.4,0.6,0.8,1.0])
 
+    ## SUBPLOT 2 (PREDICTION)
+    RREV_eval = np.linspace(2,6,10).reshape(-1,1)
+    OF_y_eval = np.linspace(0,-15,10).reshape(-1,1)
+    d_ceil_eval = np.linspace(0.1,1,10).reshape(-1,1)
+    RREV_eval,OF_y_eval,d_ceil_eval = np.meshgrid(RREV_eval,OF_y_eval,d_ceil_eval)
+    data_array = np.stack((RREV_eval.flatten(),OF_y_eval.flatten(),d_ceil_eval.flatten())).T
+    df = pd.DataFrame(data_array,columns=['RREV','OF_y','d_ceil',])
 
-# ## PLOT REGIONS
-#     fig = plt.figure(1,figsize=(8,7))
+    X_plot = torch.FloatTensor(df[['RREV','OF_y','d_ceil']].to_numpy())
+    with torch.no_grad():
+        y_plot = model.forward(X_plot)
 
-
-#     ## SUBPLOT 1 (FUNCTION)
-#     ax1 = fig.add_subplot(2,2,1,projection='3d')
-#     cmap = mpl.cm.jet
-#     norm = mpl.colors.Normalize(vmin=0,vmax=1.0)
-#     ax1.scatter(
-#         df['OF_y_flip_mean'],
-#         df['RREV_flip_mean'],
-#         2.1-df['flip_height_mean'],
-#         c = df['landing_rate_4_leg'],
-#         cmap=cmap,norm=norm,alpha=0.8,depthshade=False
-#     )
-
-#     ax1.set_xlabel('OF_y')
-#     ax1.set_xlim(-20,0)
-#     ax1.set_xticks([-20,-15,-10,-5,0])
-
-
-#     ax1.set_ylabel('RREV')
-#     ax1.set_ylim(0,8)
-#     ax1.set_yticks([0,2,4,6,8])
-
-#     ax1.set_zlabel('d_ceiling')
-#     ax1.set_zlim(0,1.0)
-#     ax1.set_zticks([0,0.2,0.4,0.6,0.8,1.0])
-
-#     ## SUBPLOT 3 (FUNCTION)
-#     ax3 = fig.add_subplot(2,2,3,projection='3d')
-#     cmap = mpl.cm.jet
-#     norm = mpl.colors.Normalize(vmin=0,vmax=10.0)
-#     ax3.scatter(
-#         df['OF_y_flip_mean'],
-#         df['RREV_flip_mean'],
-#         2.1-df['flip_height_mean'],
-#         c = df['My_d'],
-#         cmap=cmap,norm=norm,alpha=0.8,depthshade=False
-#     )
-
-#     ax3.set_xlabel('OF_y')
-#     ax3.set_xlim(-20,0)
-#     ax3.set_xticks([-20,-15,-10,-5,0])
-
-
-#     ax3.set_ylabel('RREV')
-#     ax3.set_ylim(0,8)
-#     ax3.set_yticks([0,2,4,6,8])
-
-#     ax3.set_zlabel('d_ceiling')
-#     ax3.set_zlim(0,1.0)
-#     ax3.set_zticks([0,0.2,0.4,0.6,0.8,1.0])
+    df['My'] = y_plot.numpy()
+    # df = df.query('LR >= 0.6')
 
     
-#     # ax1.plot_trisurf(
-#     #     X_eval[:,0].flatten(),
-#     #     X_eval[:,1].flatten(),
-#     #     myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[0],
-#     #     cmap='viridis',linewidth=0.2,antialiased=True)
-#     # ax1.set_xlabel('x1')
-#     # ax1.set_ylabel('x2')
-#     # ax1.set_title('Function')
-#     # ax1.set_xlim(-3,3)
-#     # ax1.set_ylim(-3,3)
+    ax2 = fig.add_subplot(1,2,2,projection='3d')
+    ax2.scatter(
+        df['OF_y'],
+        df['RREV'],
+        df['d_ceil'],
+        c = df['My'],
+        cmap=cmap,norm=norm,alpha=0.8,depthshade=False
+    )
 
-#     # ## SUBPLOT 2 (PREDICTION)
-#     # ax2 = fig.add_subplot(2,3,2,projection='3d')
-#     # ax2.plot_trisurf(
-#     #     X_eval[:,0].flatten(),
-#     #     X_eval[:,1].flatten(),
-#     #     y_eval[:,0].flatten(),
-#     #     cmap='viridis',linewidth=0.2,antialiased=True)
-#     # ax2.set_xlabel('x1')
-#     # ax2.set_ylabel('x2')
-#     # ax2.set_title('Prediction')
-#     # ax2.set_xlim(-3,3)
-#     # ax2.set_ylim(-3,3)
+    ax2.set_xlabel('OF_y')
+    ax2.set_xlim(-20,0)
+    ax2.set_xticks([-20,-15,-10,-5,0])
 
-#     # ## SUBPLOT 3 (ERROR)
-#     # ax2 = fig.add_subplot(2,3,3,projection='3d')
-#     # ax2.plot_trisurf(
-#     #     X_eval[:,0].flatten(),
-#     #     X_eval[:,1].flatten(),
-#     #     np.abs(y_eval[:,0].flatten()-myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[0]),
-#     #     cmap='viridis',linewidth=0.2,antialiased=True)
-#     # ax2.set_xlabel('x1')
-#     # ax2.set_ylabel('x2')
-#     # ax2.set_title('Error')
-#     # ax2.set_xlim(-3,3)
-#     # ax2.set_ylim(-3,3)
 
-#     # ## SUBPLOT 4 (FUNCTION)
-#     # ax1 = fig.add_subplot(2,3,4,projection='3d')
-#     # ax1.plot_trisurf(
-#     #     X_eval[:,0].flatten(),
-#     #     X_eval[:,1].flatten(),
-#     #     myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[1],
-#     #     cmap='viridis',linewidth=0.2,antialiased=True)
-#     # ax1.set_xlabel('x1')
-#     # ax1.set_ylabel('x2')
-#     # ax1.set_title('Function')
-#     # ax1.set_xlim(-3,3)
-#     # ax1.set_ylim(-3,3)
+    ax2.set_ylabel('RREV')
+    ax2.set_ylim(0,8)
+    ax2.set_yticks([0,2,4,6,8])
 
-#     # ## SUBPLOT 5 (PREDICTION)
-#     # ax2 = fig.add_subplot(2,3,5,projection='3d')
-#     # ax2.plot_trisurf(
-#     #     X_eval[:,0].flatten(),
-#     #     X_eval[:,1].flatten(),
-#     #     y_eval[:,1].flatten(),
-#     #     cmap='viridis',linewidth=0.2,antialiased=True)
-#     # ax2.set_xlabel('x1')
-#     # ax2.set_ylabel('x2')
-#     # ax2.set_title('Prediction')
-#     # ax2.set_xlim(-3,3)
-#     # ax2.set_ylim(-3,3)
-
-#     # ## SUBPLOT 6 (ERROR)
-#     # ax2 = fig.add_subplot(2,3,6,projection='3d')
-#     # ax2.plot_trisurf(
-#     #     X_eval[:,0].flatten(),
-#     #     X_eval[:,1].flatten(),
-#     #     np.abs(y_eval[:,1].flatten()-myfun(X_eval[:,0].flatten(),X_eval[:,1].flatten())[1]),
-#     #     cmap='viridis',linewidth=0.2,antialiased=True)
-#     # ax2.set_xlabel('x1')
-#     # ax2.set_ylabel('x2')
-#     # ax2.set_title('Error')
-#     # ax2.set_xlim(-3,3)
-#     # ax2.set_ylim(-3,3)
+    ax2.set_zlabel('d_ceiling')
+    ax2.set_zlim(0,1.0)
+    ax2.set_zticks([0,0.2,0.4,0.6,0.8,1.0])
+        
     
 
 
-#     fig.tight_layout()
-#     plt.show()
+    # fig.tight_layout()
+    plt.show()
+
+
+
