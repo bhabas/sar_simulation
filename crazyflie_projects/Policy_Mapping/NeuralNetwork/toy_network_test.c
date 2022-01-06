2e63dd79aea23944809bc5ef5a880648ff7e9ebb
bhabas <bhabas@psu.edu>
2022-01-06 09:33:14 -0500

    Rewriting matrix library

--- crazyflie_projects/Policy_Mapping/NeuralNetwork/toy_network_test.c
+++ crazyflie_projects/Policy_Mapping/NeuralNetwork/toy_network_test.c
@@ -6,49 +6,43 @@
 #include "nml.h"
 float Sigmoid(float x)
 {
-    return 1/(1+exp(-x));
+    // return 1/(1+exp(-x));
+    return 2*x;;
 }
 
 
 int main()
 {
-    // DEFINE MATRICES FROM FILE
-    const char *f_W1 = "data/W_1.data";
-    nml_mat *W1 = nml_mat_fromfile(f_W1);
 
-    const char *f_b1 = "data/b_1.data";
-    nml_mat *b1 = nml_mat_fromfile(f_b1);
+    double W1_array[16] = {
+        1.0, 2.0, 3.0, 4.0, 
+        1.0, 2.0, 3.0, 4.0, 
+        1.0, 2.0, 3.0, 4.0,
+        1.0, 2.0, 3.0, 4.0}; // 4x4
+    nml_mat *W1 = nml_mat_from(4, 4, 16, W1_array);
+
+    double X1_array[4] = {1.0, 2.0, 3.0, 4.0}; // 4x1
+    nml_mat *X1 = nml_mat_from(4, 1, 4, X1_array);
+
+    nml_mat *WX = nml_mat_dot(W1,X1); // 4x3 * 3x1 = 4x1
+
+    double b1_array[4] = {1.0, 2.0, 3.0, 4.0}; // 4x1
+    nml_mat *b1 = nml_mat_from(4, 1, 4, b1_array);
+
+    // nml_mat_add_r(WX,b1);
+
+    // nml_mat *a1 = nml_mat_funcElement(WX,Sigmoid);
 
 
-    const char *f_W2 = "data/W_2.data";
-    nml_mat *W2 = nml_mat_fromfile(f_W2);
-
-    const char *f_b2 = "data/b_2.data";
-    nml_mat *b2 = nml_mat_fromfile(f_b2);
-
-
-
-    double X_array[3] = {1.0, 2.0, 3.0};
-    nml_mat *X = nml_mat_from(3, 1, 3, X_array);
-
-
-    // a = sig(W*X + b)
-    nml_mat *a1 = nml_mat_dot(W1, X);
+    nml_mat_print(WX);
     nml_mat_print(b1);
-    // printf("%d",b1->num_rows);
-    // nml_mat *a2 = nml_mat_add(a1,b1);
-    // nml_mat *a3 = nml_mat_funcElement(a2,Sigmoid);
-
-    // nml_mat_print(a1);
-
-    nml_mat_free(X);
+    nml_mat_add_r(WX,b1);
 
     nml_mat_free(W1);
+    nml_mat_free(X1);
     nml_mat_free(b1);
-    nml_mat_free(W2);
-    nml_mat_free(b2);
-    nml_mat_free(a1);
-
+    // nml_mat_free(WX);
+    // nml_mat_free(a1);
 
 
     return 0;
