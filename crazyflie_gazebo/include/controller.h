86312eae2dcee7c9bdd5112f36b642f3c32fbb4d
bhabas <bhabas@psu.edu>
2022-01-08 18:20:05 -0500

    Working motorspeed estimation but can't connect to publisher

--- crazyflie_gazebo/include/controller.h
+++ crazyflie_gazebo/include/controller.h
@@ -30,8 +30,9 @@
 #include "stabilizer_types.h"
 #include "nml.h"
 
-void stateEstimator(state_t *state);
-void commanderGetSetpoint();
+void stateEstimator(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
+void commanderGetSetpoint(setpoint_t *setpoint, const state_t *state);
+
 
 using namespace std;
 
@@ -46,10 +47,175 @@ void controllerGTCInit(void);
 bool controllerGTCTest(void);
 void controllerGTCReset(void);
 void controllerGTCTraj(void);
-void controllerGTC(state_t *state, const uint32_t tick);
-void GTC_Command();
+void controllerGTC(control_t *control, setpoint_t *setpoint,
+                                         const sensorData_t *sensors,
+                                         const state_t *state,
+                                         const uint32_t tick);
+void GTC_Command(setpoint_t *setpoint);
+
+// SYSTEM PARAMETERS
+static float m = 0.037; // [g]
+static float g = 9.81f;
+struct mat33 J; // Rotational Inertia Matrix [kg*m^2]
+static float h_ceiling = 2.10f; // [m]
+
+static float d = 0.040f;    // COM to Prop [m]
+static float dp = 0.028284; // COM to Prop along x-axis [m]
+                            // [dp = d*sin(45 deg)]
+
+static float const kf = 2.2e-8f;    // Thrust Coeff [N/(rad/s)^2]
+static float const c_tf = 0.00618f;  // Moment Coeff [Nm/N]
 
 
+// LOGGING VARIABLES
+static float cmd_thrust;
+static float cmd_roll;
+static float cmd_pitch;
+static float cmd_yaw;
+
+// STATE ERRORS
+static struct vec e_x;  // Pos-error [m]
+static struct vec e_v;  // Vel-error [m/s]
+static struct vec e_PI;  // Pos. Integral-error [m*s]
+
+static struct vec e_R;  // Rotation-error [rad]
+static struct vec e_w;  // Omega-error [rad/s]
+static struct vec e_RI; // Rot. Integral-error [rad*s]
+
+// STATE VALUES
+static struct vec statePos = {0.0f,0.0f,0.0f};         // Pos [m]
+static struct vec stateVel = {0.0f,0.0f,0.0f};         // Vel [m/s]
+static struct quat stateQuat = {0.0f,0.0f,0.0f,1.0f};  // Orientation
+static struct vec stateOmega = {0.0f,0.0f,0.0f};       // Angular Rate [rad/s]
+
+static struct mat33 R; // Orientation as rotation matrix
+static struct vec stateEul = {0.0f,0.0f,0.0f}; // Pose in Euler Angles [YZX Notation]
+
+// DESIRED STATES
+static struct vec x_d = {0.0f,0.0f,0.0f}; // Pos-desired [m]
+static struct vec v_d = {0.0f,0.0f,0.0f}; // Vel-desired [m/s]
+static struct vec a_d = {0.0f,0.0f,0.0f}; // Acc-desired [m/s^2]
+
+static struct quat quat_d = {0.0f,0.0f,0.0f,1.0f}; // Orientation-desired [qx,qy,qz,qw]
+static struct vec eul_d = {0.0f,0.0f,0.0f};        // Euler Angle-desired [rad? deg? TBD]
+static struct vec omega_d = {0.0f,0.0f,0.0f};      // Omega-desired [rad/s]
+static struct vec domega_d = {0.0f,0.0f,0.0f};     // Ang. Acc-desired [rad/s^2]
+
+static struct vec b1_d = {1.0f,0.0f,0.0f};    // Desired body x-axis in global coord. [x,y,z]
+static struct vec b2_d;    // Desired body y-axis in global coord.
+static struct vec b3_d;    // Desired body z-axis in global coord.
+static struct vec b3;      // Current body z-axis in global coord.
+
+static struct mat33 R_d;   // Desired rotational matrix from b_d vectors
+
+
+
+
+
+static struct vec e_3 = {0.0f, 0.0f, 1.0f}; // Global z-axis
+
+static struct vec F_thrust_ideal;           // Ideal thrust vector
+static float F_thrust = 0.0f;               // Desired body thrust [N]
+static float F_thrust_max = 0.64f;          // Max possible body thrust [N}]
+static struct vec M;                        // Desired body moments [Nm]
+static struct vec M_d = {0.0f,0.0f,0.0f};   // Desired moment [N*mm]
+static float Moment_flag = false;
+
+
+// TEMPORARY CALC VECS/MATRICES
+static struct vec temp1_v; 
+static struct vec temp2_v;
+static struct vec temp3_v;
+static struct vec temp4_v;
+static struct mat33 temp1_m;  
+
+static struct vec P_effort;
+static struct vec R_effort;
+
+static struct mat33 RdT_R; // Rd' * R
+static struct mat33 RT_Rd; // R' * Rd
+static struct vec Gyro_dyn;
+
+
+
+// MOTOR THRUSTS
+static float f_thrust_g; // Motor thrust - Thrust [N]
+static float f_roll_g;   // Motor thrust - Roll   [N]
+static float f_pitch_g;  // Motor thrust - Pitch  [N]
+static float f_yaw_g;    // Motor thrust - Yaw    [N]
+
+static int32_t f_thrust_pwm; 
+static int32_t f_roll_pwm;   
+static int32_t f_pitch_pwm; 
+static int32_t f_yaw_pwm;  
+
+
+// INIT CTRL GAIN VECTORS
+static struct vec Kp_p; // Pos. Proportional Gains
+static struct vec Kd_p; // Pos. Derivative Gains
+static struct vec Ki_p; // Pos. Integral Gains  
+
+static struct vec Kp_R; // Rot. Proportional Gains
+static struct vec Kd_R; // Rot. Derivative Gains
+static struct vec Ki_R; // Rot. Integral Gains
+
+static float dt = (float)(1.0f/RATE_500_HZ);
+
+// CONTROLLER PARAMETERS
+static bool attCtrlEnable = false;
+static bool tumbled = false;
+static bool motorstop_flag = false;
+static bool errorReset = false;
+
+// OPTICAL FLOW STATES
+static float RREV = 0.0f; // [rad/s]
+static float OF_x = 0.0f; // [rad/s]
+static float OF_y = 0.0f; // [rad/s] 
+static bool flip_flag = false;
+
+
+
+
+
+// STATE VALUES AT FLIP TRIGGER
+static float RREV_tr = 0.0f;
+static float OF_x_tr = 0.0f;
+static float OF_y_tr = 0.0f;
+
+static struct vec statePos_tr = {0.0f,0.0f,0.0f};         // Pos [m]
+static struct vec stateVel_tr = {0.0f,0.0f,0.0f};         // Vel [m/s]
+static struct quat stateQuat_tr = {0.0f,0.0f,0.0f,1.0f};  // Orientation
+static struct vec stateOmega_tr = {0.0f,0.0f,0.0f};       // Angular Rate [rad/s]
+
+
+
+// POLICY VARIABLES
+static float RREV_thr = 0.0f;
+static float G1 = 0.0f;
+static float G2 = 0.0f;
+
+static bool policy_armed_flag = false;
+
+
+// MOTOR VARIABLES
+static uint32_t M1_pwm = 0; 
+static uint32_t M2_pwm = 0; 
+static uint32_t M3_pwm = 0; 
+static uint32_t M4_pwm = 0; 
+
+static float MS1 = 0;
+static float MS2 = 0;
+static float MS3 = 0;
+static float MS4 = 0;
+
+// TRAJECTORY VARIABLES
+static float s_0 = 0.0f;
+static float v = 0.0f;
+static float a = 0.0f;
+static float t = 0.0f;
+static float T = 0.0f;
+static uint8_t traj_type = 0;
+static bool execute_traj = false;
 
 
 
@@ -59,21 +225,48 @@ class Controller
         // CONSTRUCTOR TO START PUBLISHERS AND SUBSCRIBERS (Similar to Python's __init__() )
         Controller(ros::NodeHandle *nh){
 
+            // MS_Publisher = nh->advertise<crazyflie_msgs::MS>("/MS",1);
+
+
             viconState_Subscriber = nh->subscribe("/env/vicon_state",1,&Controller::vicon_Callback,this,ros::TransportHints().tcpNoDelay());
             imu_Subscriber = nh->subscribe("/cf1/imu",1,&Controller::imu_Callback,this,ros::TransportHints().tcpNoDelay());
             OF_Subscriber = nh->subscribe("/cf1/OF_sensor",1,&Controller::OF_Callback,this,ros::TransportHints().tcpNoDelay()); 
 
-            // state.position.x = 1.0;
-            controllerThread = std::thread(&Controller::startController, this);
+
+
+
+
+
+
+            setpoint.position.x = 0.0f;
+            setpoint.position.y = 0.0f;
+            setpoint.position.z = 0.0f;
+
+            setpoint.velocity.x = 0.0f;
+            setpoint.velocity.y = 0.0f;
+            setpoint.velocity.z = 0.0f;
+
+            setpoint.acceleration.x = 0.0f;
+            setpoint.acceleration.y = 0.0f;
+            setpoint.acceleration.z = 0.0f;
+
             
 
+
+
+
+            // START CONTROLLER
+            controllerThread = std::thread(&Controller::startController, this);           
         }
 
         // DEFINE FUNCTION PROTOTYPES
         void startController();
         void vicon_Callback(const nav_msgs::Odometry::ConstPtr &msg);
         void imu_Callback(const sensor_msgs::Imu::ConstPtr &msg);
-        void OF_Callback(const nav_msgs::Odometry::ConstPtr &msg);           
+        void OF_Callback(const nav_msgs::Odometry::ConstPtr &msg);   
+
+        // ros::Publisher MS_Publisher;
+
 
 
 
@@ -86,6 +279,7 @@ class Controller
         ros::Subscriber viconState_Subscriber;
         ros::Subscriber imu_Subscriber;
         ros::Subscriber OF_Subscriber;
+
 
 
         
@@ -111,10 +305,12 @@ class Controller
         // DEFINE THREAD OBJECTS
         std::thread controllerThread;
 
+        control_t control;
+        setpoint_t setpoint;
+        sensorData_t sensorData;
         state_t state;
-        sensorData_t sensors;
-        
         uint32_t tick = 0;
+        
 
         
 
@@ -154,13 +350,13 @@ void Controller::imu_Callback(const sensor_msgs::Imu::ConstPtr &msg){
     state.attitudeQuaternion.z = _quaternion.z;
     state.attitudeQuaternion.w = _quaternion.w;
 
-    sensors.gyro.x = _omega.x;
-    sensors.gyro.y = _omega.y;
-    sensors.gyro.z = _omega.z;
+    sensorData.gyro.x = _omega.x;
+    sensorData.gyro.y = _omega.y;
+    sensorData.gyro.z = _omega.z;
 
-    sensors.acc.x = _accel.x;
-    sensors.acc.y = _accel.y;
-    sensors.acc.z = _accel.z;
+    sensorData.acc.x = _accel.x;
+    sensorData.acc.y = _accel.y;
+    sensorData.acc.z = _accel.z;
     
 
 }
@@ -190,9 +386,9 @@ void Controller::startController()
     
     while(ros::ok)
     {
-        stateEstimator(&state);
-        commanderGetSetpoint();
-        controllerGTC(&state, tick);
+        stateEstimator(&state, &sensorData, &control, tick);
+        commanderGetSetpoint(&setpoint, &state);
+        controllerGTC(&control, &setpoint, &sensorData, &state, tick);
 
 
         tick++;
@@ -201,10 +397,74 @@ void Controller::startController()
 }
 
 
-void stateEstimator(state_t *state)
-{
-}
-void commanderGetSetpoint()
+void stateEstimator(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick)
 {
 
 }
+
+void commanderGetSetpoint(setpoint_t *setpoint, const state_t *state)
+{
+
+}
+
+// EXPLICIT FUNTIONS
+
+// Converts thrust in Newtons to their respective PWM values
+static inline int32_t thrust2PWM(float f) 
+{
+    // Conversion values calculated from self motor analysis
+    float a = 3.31e4;
+    float b = 1.12e1;
+    float c = 8.72;
+    float d = 3.26e4;
+
+    float s = 1.0f; // sign of value
+    int32_t f_pwm = 0;
+
+    s = f/fabsf(f);
+    f = fabsf(f);
+    
+    f_pwm = a*tanf((f-c)/b)+d;
+
+    return s*f_pwm;
+
+}      
+
+// Converts thrust in PWM to their respective Newton values
+static inline float PWM2thrust(int32_t M_PWM) 
+{
+    // Conversion values calculated from PWM to Thrust Curve
+    // Linear Fit: Thrust [g] = a*PWM + b
+    float a = 3.31e4;
+    float b = 1.12e1;
+    float c = 8.72;
+    float d = 3.26e4;
+
+    float f = b*atan2f(M_PWM-d,a)+c;
+    // float f = (a*M_PWM + b); // Convert thrust to grams
+
+    if(f<0)
+    {
+      f = 0;
+    }
+
+    return f;
+}
+
+
+// Limit PWM value to accurate motor curve limit (60,000)
+uint16_t limitPWM(int32_t value) 
+{
+  if(value > 60000)
+  {
+    value = 60000;
+  }
+  else if(value < 0)
+  {
+    value = 0;
+  }
+
+  return (uint16_t)value;
+}
+
+
--- crazyflie_gazebo/src/controller.cpp
+++ crazyflie_gazebo/src/controller.cpp
@@ -1,18 +1,272 @@
-#include <iostream>
 
 #include "controller.h"
 
-#include <ros/ros.h>
+// XY POSITION PID
+float P_kp_xy = 0.5f;
+float P_kd_xy = 0.4f;
+float P_ki_xy = 0.0f;
+float i_range_xy = 0.3f;
 
+// Z POSITION PID
+float P_kp_z = 1.2f;
+float P_kd_z = 0.35f;
+float P_ki_z = 0.0f;
+float i_range_z = 0.25f;
 
+// XY ATTITUDE PID
+float R_kp_xy = 0.002f;
+float R_kd_xy = 0.0025f;
+float R_ki_xy = 0.0f;
+float i_range_R_xy = 1.0f;
 
-void controllerGTC(state_t *state, const uint32_t tick)
+// Z ATTITUDE PID
+float R_kp_z = 0.005f;
+float R_kd_z = 0.002f;
+float R_ki_z = 0.000;
+float i_range_R_z = 0.5f;
+void controllerGTCInit(void)
 {
-    // printf("HELLO\n");
+    controllerGTCTest();
+    controllerGTCReset();
+    printf("GTC Initiated\n");
+}
+
+void controllerGTCReset(void)
+{
+    printf("GTC Reset\n");
+    // Reset errors to zero
+    e_PI = vzero();
+    e_RI = vzero();
+
+    x_d = mkvec(0.0f,0.0f,0.0f);
+    v_d = mkvec(0.0f,0.0f,0.0f);
+    a_d = mkvec(0.0f,0.0f,0.0f);
     
-    printf("%.5f\n",state->attitudeQuaternion.w);
+
+    tumbled = false;
+    motorstop_flag = false;
+
+    Moment_flag = false;
+    policy_armed_flag = false;
+    flip_flag = false;
+
+    t = 0;
+    execute_traj = false;
 
 
+
+
+}
+
+bool controllerGTCTest(void)
+{
+    return true;
+}
+
+void GTC_Command(setpoint_t *setpoint)
+{   
+
+}
+
+void controllerGTCTraj()
+{
+
+}
+
+crazyflie_msgs::MS MS_msg;
+
+void controllerGTC(control_t *control, setpoint_t *setpoint,
+                                         const sensorData_t *sensors,
+                                         const state_t *state,
+                                         const uint32_t tick) 
+{
+    if (RATE_DO_EXECUTE(RATE_500_HZ, tick)) {
+
+        // if (setpoint->GTC_cmd_rec == true)
+        //     {
+                
+        //         GTC_Command(setpoint);
+        //         setpoint->GTC_cmd_rec = false;
+        //     }
+
+        // if (errorReset){
+        //     controllerGTCReset();
+        //     errorReset = false;
+        //     }
+
+        // if(execute_traj){
+        //     controllerGTCTraj();
+        // }
+
+        // SYSTEM PARAMETERS 
+        J = mdiag(1.65717e-5f, 1.66556e-5f, 2.92617e-5f); // Rotational Inertia of CF [kg m^2]
+
+        // CONTROL GAINS
+        Kp_p = mkvec(P_kp_xy,P_kp_xy,P_kp_z);
+        Kd_p = mkvec(P_kd_xy,P_kd_xy,P_kd_z);
+        Ki_p = mkvec(P_ki_xy,P_ki_xy,P_ki_z);
+
+        Kp_R = mkvec(R_kp_xy,R_kp_xy,R_kp_z);
+        Kd_R = mkvec(R_kd_xy,R_kd_xy,R_kd_z);
+        Ki_R = mkvec(R_ki_xy,R_ki_xy,R_ki_z);
+
+        // =========== STATE DEFINITIONS =========== //
+        statePos = mkvec(state->position.x, state->position.y, state->position.z);                      // [m]
+        stateVel = mkvec(state->velocity.x, state->velocity.y, state->velocity.z);                      // [m]
+        stateOmega = mkvec(radians(sensors->gyro.x), radians(sensors->gyro.y), radians(sensors->gyro.z));   // [rad/s]
+        stateQuat = mkquat(state->attitudeQuaternion.x,
+                        state->attitudeQuaternion.y,
+                        state->attitudeQuaternion.z,
+                        state->attitudeQuaternion.w);
+
+
+        RREV = stateVel.z/(h_ceiling - statePos.z);
+        OF_x = stateVel.y/(h_ceiling - statePos.z);
+        OF_y = stateVel.x/(h_ceiling - statePos.z);
+
+        // =========== STATE SETPOINTS =========== //
+        x_d = mkvec(setpoint->position.x,setpoint->position.y,setpoint->position.z);             // Pos-desired [m]
+        v_d = mkvec(setpoint->velocity.x, setpoint->velocity.y, setpoint->velocity.z);             // Vel-desired [m/s]
+        a_d = mkvec(setpoint->acceleration.x, setpoint->acceleration.y, setpoint->acceleration.z); // Acc-desired [m/s^2]
+
+        omega_d = mkvec(radians(setpoint->attitudeRate.roll),
+                        radians(setpoint->attitudeRate.pitch),
+                        radians(setpoint->attitudeRate.yaw));         // Omega-desired [rad/s]
+        domega_d = mkvec(radians(0.0f), radians(0.0f), radians(0.0f));  // Omega-Accl. [rad/s^2]
+
+        eul_d = mkvec(radians(setpoint->attitude.roll),
+                        -radians(setpoint->attitude.pitch), 
+                        radians(setpoint->attitude.yaw));
+        quat_d = rpy2quat(eul_d); // Desired orientation from eul angles [ZYX NOTATION]
+
+        // =========== ROTATION MATRIX =========== //
+        // R changes Body axes to be in terms of Global axes
+        // https://www.andre-gaschler.com/rotationconverter/
+        R = quat2rotmat(stateQuat); // Quaternion to Rotation Matrix Conversion
+        b3 = mvmul(R, e_3);         // Current body vertical axis in terms of global axes | [b3 = R*e_3] 
+
+        // TUMBLE DETECTION
+        if (b3.z <= 0){
+            tumbled = true;
+        }
+
+        // =========== TRANSLATIONAL EFFORT =========== //
+        e_x = vsub(statePos, x_d); // [e_x = pos-x_d]
+        e_v = vsub(stateVel, v_d); // [e_v = vel-v_d]
+
+        // POS. INTEGRAL ERROR
+        e_PI.x += (e_x.x)*dt;
+        e_PI.x = clamp(e_PI.x, -i_range_xy, i_range_xy);
+
+        e_PI.y += (e_x.y)*dt;
+        e_PI.y = clamp(e_PI.y, -i_range_xy, i_range_xy);
+
+        e_PI.z += (e_x.z)*dt;
+        e_PI.z = clamp(e_PI.z, -i_range_z, i_range_z);
+
+
+        /* [F_thrust_ideal = -kp_x*e_x*(kp_x_flag) + -kd_x*e_v + -kI_x*e_PI*(kp_x_flag) + m*g*e_3 + m*a_d] */
+        temp1_v = veltmul(vneg(Kp_p), e_x);
+        temp2_v = veltmul(vneg(Kd_p), e_v);
+        temp3_v = veltmul(vneg(Ki_p), e_PI);
+        P_effort = vadd3(temp1_v,temp2_v,temp3_v);
+
+        temp1_v = vscl(m*g, e_3); // Feed-forward term
+        temp2_v = vscl(m, a_d);
+
+        F_thrust_ideal = vadd3(P_effort, temp1_v,temp2_v); 
+
+        // =========== DESIRED BODY AXES =========== // 
+        b3_d = vnormalize(F_thrust_ideal);
+        b2_d = vnormalize(vcross(b3_d, b1_d));      // [b3_d x b1_d] | body-fixed horizontal axis
+        temp1_v = vnormalize(vcross(b2_d, b3_d));
+        R_d = mcolumns(temp1_v, b2_d, b3_d);        // Desired rotation matrix from calculations
+
+
+        // ATTITUDE CONTROL
+        if (attCtrlEnable){ 
+            R_d = quat2rotmat(quat_d); // Desired rotation matrix from att. control
+        }
+
+
+        // =========== ROTATIONAL ERRORS =========== // 
+        RdT_R = mmul(mtranspose(R_d), R);       // [R_d'*R]
+        RT_Rd = mmul(mtranspose(R), R_d);       // [R'*R_d]
+
+        temp1_v = dehat(msub(RdT_R, RT_Rd));    // [dehat(R_d'*R - R'*R)]
+        e_R = vscl(0.5f, temp1_v);              // Rotation error | [eR = 0.5*dehat(R_d'*R - R'*R)]
+
+        temp1_v = mvmul(RT_Rd, omega_d);        // [R'*R_d*omega_d]
+        e_w = vsub(stateOmega, temp1_v);        // Ang. vel error | [e_w = omega - R'*R_d*omega_d] 
+
+        // ROT. INTEGRAL ERROR
+        e_RI.x += (e_R.x)*dt;
+        e_RI.x = clamp(e_RI.x, -i_range_R_xy, i_range_R_xy);
+
+        e_RI.y += (e_R.y)*dt;
+        e_RI.y = clamp(e_RI.y, -i_range_R_xy, i_range_R_xy);
+
+        e_RI.z += (e_R.z)*dt;
+        e_RI.z = clamp(e_RI.z, -i_range_R_z, i_range_R_z);
+
+        // =========== CONTROL EQUATIONS =========== // 
+        /* [M = -kp_R*e_R - kd_R*e_w -ki_R*e_RI + Gyro_dyn] */
+
+        temp1_v = veltmul(vneg(Kp_R), e_R);     // [-kp_R*e_R]
+        temp2_v = veltmul(vneg(Kd_R), e_w);     // [-kd_R*e_w]
+        temp3_v = veltmul(vneg(Ki_R), e_RI);    // [-ki_R*e_RI]
+        R_effort = vadd3(temp1_v,temp2_v,temp3_v);
+
+        
+
+        /* Gyro_dyn = [omega x (J*omega)] - [J*( hat(omega)*R'*R_d*omega_d - R'*R_d*domega_d )] */
+        temp1_v = vcross(stateOmega, mvmul(J, stateOmega)); // [omega x J*omega]
+
+
+        temp1_m = mmul(hat(stateOmega), RT_Rd); //  hat(omega)*R'*R_d
+        temp2_v = mvmul(temp1_m, omega_d);      // (hat(omega)*R'*R_d)*omega_d
+        temp3_v = mvmul(RT_Rd, domega_d);       // (R'*R_d*domega_d)
+
+        temp4_v = mvmul(J, vsub(temp2_v, temp3_v)); // J*(hat(omega)*R'*R_d*omega_d - R'*R_d*domega_d)
+        Gyro_dyn = vsub(temp1_v,temp4_v);
+
+        F_thrust = vdot(F_thrust_ideal, b3);    // Project ideal thrust onto b3 vector [N]
+        M = vadd(R_effort,Gyro_dyn);            // Control moments [Nm]
+
+        // printvec(M);
+        printf("%.3f\n",F_thrust);
+
+        // =========== CONVERT THRUSTS AND MOMENTS TO PWM =========== // 
+        f_thrust_g = F_thrust/4.0f*Newton2g;
+        f_roll_g = M.x/(4.0f*dp)*Newton2g;
+        f_pitch_g = M.y/(4.0f*dp)*Newton2g;
+        f_yaw_g = M.z/(4.0*c_tf)*Newton2g;
+
+        f_thrust_g = clamp(f_thrust_g,0.0,f_MAX*0.8);    // Clamp thrust to prevent control saturation
+
+        if(motorstop_flag){ // Cutoff all motor values
+            f_thrust_g = 0.0f;
+            f_roll_g = 0.0f;
+            f_pitch_g = 0.0f;
+            f_yaw_g = 0.0f;
+        }
+
+        
+        M1_pwm = limitPWM(thrust2PWM(f_thrust_g + f_roll_g - f_pitch_g + f_yaw_g)); // Add respective thrust components and limit to (0 <= PWM <= 60,000)
+        M2_pwm = limitPWM(thrust2PWM(f_thrust_g + f_roll_g + f_pitch_g - f_yaw_g));
+        M3_pwm = limitPWM(thrust2PWM(f_thrust_g - f_roll_g + f_pitch_g + f_yaw_g));
+        M4_pwm = limitPWM(thrust2PWM(f_thrust_g - f_roll_g - f_pitch_g - f_yaw_g));
+
+        MS1 = sqrt(PWM2thrust(M1_pwm)*g2Newton/kf);
+        MS2 = sqrt(PWM2thrust(M2_pwm)*g2Newton/kf);
+        MS3 = sqrt(PWM2thrust(M3_pwm)*g2Newton/kf);
+        MS4 = sqrt(PWM2thrust(M4_pwm)*g2Newton/kf);
+
+        MS_msg.MotorSpeeds = {(uint16_t)MS1,(uint16_t)MS2,(uint16_t)MS3,(uint16_t)MS4};
+        // MS_Publisher.publish(MS_msg);
+        printf("MS1: %.1f \t MS2: %.1f \t MS3: %.1f \t MS4: %.1f \n",MS1,MS2,MS3,MS4);
+
+    }
     
 }
 
