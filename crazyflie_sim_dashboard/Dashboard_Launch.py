4b5a303beb9d92a53e6837c1e688e94d4b52fff8
bhabas <bhabas@psu.edu>
2022-01-17 08:25:57 -0500

    Removed excess files

--- crazyflie_sim_dashboard/Dashboard_Launch.py
+++ crazyflie_sim_dashboard/Dashboard_Launch.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
-
-import sys  # We need sys so that we can pass argv to QApplication
+from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import pyqtgraph as pg
+
+import os,rospkg,rospy
+import numpy as np
+import pandas as pd
+import sys  # We need sys so that we can pass argv to QApplication
 from dashboard_node import DashboardNode
 from nav_msgs.msg import Odometry
-import os,rospkg
-import rospy
+
 
 ## ADD CRAZYFLIE_SIMULATION DIRECTORY TO PYTHONPATH SO ABSOLUTE IMPORTS CAN BE USED
-BASE_PATH = os.path.dirname(rospkg.RosPack().get_path('crazyflie_data'))
+BASE_PATH = os.path.dirname(rospkg.RosPack().get_path('crazyflie_projects'))
 sys.path.insert(0,BASE_PATH)
 
 
-DashNode=DashboardNode()
+class Dashboard(QMainWindow,DashboardNode):
+    def __init__(self):
+        super().__init__()
+        super(DashboardNode).__init__()
 
-
-class Dashboard(QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(Dashboard, self).__init__(*args, **kwargs)
-        ## LOOK INTO INITIALIZING DASHBOARDNODE CLASS
 
         #LOAD UI
-        loadUi(f'{BASE_PATH}/crazyflie_sim_dashboard/mainwindow.ui', self)
-
+        loadUi(f'{BASE_PATH}/crazyflie_sim_dashboard/DashboardWindow.ui', self)
 
         self.plot_list = [
             self.Pos_Graph,
@@ -34,7 +32,8 @@ class Dashboard(QMainWindow):
             self.Dist_Graph,
             self.Eul_Graph,
             self.OF_Graph,
-            self.Omega_Graph,]
+            self.Omega_Graph,
+            ]
 
 
         self.Battery_Voltage.setValue(50)
@@ -43,6 +42,7 @@ class Dashboard(QMainWindow):
         self.pauseButton.clicked.connect(self.pause_plots)
         self.rescaleButton.clicked.connect(self.rescale_plots)
         self.clearButton.clicked.connect(self.clear_plots)
+        self.paused = False
 
         ## ON TIMER TIMEOUTS EXECUTE FUNCTIONS
         self.timer = pg.QtCore.QTimer(self)
@@ -50,11 +50,12 @@ class Dashboard(QMainWindow):
         self.timer.timeout.connect(self.check_Vicon_Comm)
         self.timer.start(500) # number of milliseconds (every 1000) for next update
 
-        self.paused = False
+        
+        self.printSumthin()
 
     def update_LCD(self):
-        self.K_ep_LCD.display(DashNode.k_ep)
-        self.K_run_LCD.display(DashNode.k_run)
+        self.K_ep_LCD.display(self.k_ep)
+        self.K_run_LCD.display(self.k_run)
 
     def check_Vicon_Comm(self): ## CHECK IF RECEIVING VALID VICON DATA
         try:
@@ -62,7 +63,6 @@ class Dashboard(QMainWindow):
             self.Vicon_Connection.setStyleSheet("background-color: rgb(44, 160, 44);")
         except:
             self.Vicon_Connection.setStyleSheet("background-color: rgb(214,39,40);")
-
 
 
     def pause_plots(self):
@@ -92,13 +92,13 @@ class Dashboard(QMainWindow):
             plot.clear_data()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    ## INITIALIZE APPLICATION
+    ## INITIALIZE APPLICATION   
     app = QApplication(sys.argv)
 
     ## INITIALIZE DASHBOARD WINDOW
-    dashboard = Dashboard()
-    dashboard.show()
+    myApp = Dashboard()
+    myApp.show()
 
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
