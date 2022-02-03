a78b8672e2b625500c0fe7a31f5695729df3a3a0
bhabas <bhabas@psu.edu>
2022-02-03 11:26:01 -0500

    Working conversion from str to NML matrix

---
+++ crazyflie_gazebo/src/example copy.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "nml.h"
+
+// Compile Statement
+// gcc example.c nml.c nml_util.c -I /home/bhabas/catkin_ws/src/crazyflie_simulation/crazyflie_gazebo/include  -Wall -o example -lm -Wall && ./example 
+
+/*
+Seperate arrays into strings grouped by "*"
+Seperate string by "," to get first line
+Seperate first line by " " to get dimensions
+Allocate matrix by dimensions
+Loop through rows
+    Loop through columns
+        Add value to matrix
+
+*/
+
+
+static char str[] = {
+    "3 2,"
+    "1  2,"
+    "3  4,"
+    "5  6,"
+    "*"
+    "3 3,"
+    "1  2  3,"
+    "4  5  6,"
+    "7  8  9,"
+
+};
+
+
+nml_mat* nml_mat_fromstr(char* array_str)
+{
+    unsigned int num_rows = 0, num_cols = 0;
+
+}
+
+
+int main()
+{   
+
+
+    // EXTRACT MATRIX DIMENSIONS
+
+    char array_list[5000], array_str[1024], line_str[1024];
+    char *array_token, *line_token, *value_token;
+    char *save_ptr1, *save_ptr2, *save_ptr3;
+
+    strcpy(array_list,str);
+
+
+    // ITERATE THROUGH ARRAYS
+    array_token = strtok_r(array_list,"*",&save_ptr1); // Collect first array
+    while(array_token!=NULL)
+    {   
+
+        line_token = strtok_r(array_token,",",&save_ptr2); // Collect first line
+        printf("%s\n",line_token);
+
+        
+        // DETERMINE DIMENSIONS FROM FIRST LINE
+        value_token = strtok_r(line_token," ",&save_ptr3); // Collect first value from first line
+        int m = atoi(value_token);
+
+        value_token = strtok_r(NULL," ",&save_ptr3); // Collect second value from first line
+        int n = atoi(value_token);
+        printf("m: %d \t n: %d\n",m,n);
+
+
+        
+
+        // ITERATE THROUGH LINES
+        line_token = strtok_r(NULL,",",&save_ptr2); // Move to next line
+        for (int i = 0; i < m; i++)
+        {
+            printf("%s\n",line_token);
+
+            value_token = strtok_r(line_token," ",&save_ptr3); // Collect first value from first line
+            for (int j = 0; j < n; j++)
+            {
+                printf("%s\n",value_token);
+                value_token = strtok_r(NULL," ",&save_ptr3); // Move to next value
+            }
+            printf("\n");
+            
+
+            line_token = strtok_r(NULL,",",&save_ptr2);
+        }
+        
+
+        
+        array_token = strtok_r(NULL, "*", &save_ptr1);
+
+    }
+
+    
+
+
+    // // get the initial tokens
+    // token1 = strtok_r(buf_1, " ", &save_ptr1);
+    // token2 = strtok_r(buf_2, " ", &save_ptr2);
+    // token3 = strtok_r(buf_3, " ", &save_ptr3);
+
+    // while(token1 && token2 && token3) {
+    //     // do stuff with tokens
+
+    //     // get next tokens
+    //     token1 = strtok_r(NULL, " ", &save_ptr1);
+    //     token2 = strtok_r(NULL, " ", &save_ptr2);
+    //     token3 = strtok_r(NULL, " ", &save_ptr3);
+    // }
+
+
+    // // EXTRACT ARRAYS
+    // char* array_token = strtok(str, "*");
+    // while(array_token!= NULL){
+
+    //     char* array_str;
+    //     strcpy(array_str,array_token);
+    //     // char* line_token = strtok(array_token,",");
+    //     printf("%s\n",array_str);
+
+
+
+    //     // printf("m: %d \t n: %d\n",m,n);
+
+
+
+
+
+    
+    //     array_token = strtok(NULL, "*");
+    // }
+
+
+    // char* token = strtok(token_Line, " ");
+
+
+
+
+
+    
+
+    return 0;
+}
+
+
--- crazyflie_gazebo/src/example.c
+++ crazyflie_gazebo/src/example.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "nml.h"
+
+// Compile Statement
+// gcc example.c nml.c nml_util.c -I /home/bhabas/catkin_ws/src/crazyflie_simulation/crazyflie_gazebo/include  -Wall -o example -lm -Wall && ./example 
 
 /*
 Seperate arrays into strings grouped by "*"
@@ -17,9 +21,9 @@ Loop through rows
 
 static char str[] = {
     "3 2,"
-    "1  2,"
-    "3  4,"
-    "5  6,"
+    "1.1  2,"
+    "3.4  4,"
+    "5.653  6,"
     "*"
     "3 3,"
     "1  2  3,"
@@ -28,109 +32,54 @@ static char str[] = {
 
 };
 
+
+nml_mat* nml_mat_fromstr(char* str)
+{
+    unsigned int num_rows = 0, num_cols = 0;
+    char array_str[1024],line_str[1024];
+    char *line_token, *value_token;
+    char *save_ptr1, *save_ptr2;
+
+    strcpy(array_str,str);
+    line_token = strtok_r(array_str,",",&save_ptr1);
+
+    // DETERMINE DIMENSIONS FROM FIRST LINE
+    value_token = strtok_r(line_token," ",&save_ptr2); // Collect first value of line
+    num_rows = atoi(value_token);
+
+    value_token = strtok_r(NULL," ",&save_ptr2); // Collect second value of line
+    num_cols = atoi(value_token);
+    nml_mat* r = nml_mat_new(num_rows,num_cols);
+
+    // ITERATE THROUGH LINES
+    line_token = strtok_r(NULL,",",&save_ptr1); // Move to next line
+    for (int i = 0; i < num_rows; i++)
+    {
+
+        value_token = strtok_r(line_token," ",&save_ptr2);
+        for (int j = 0; j < num_cols; j++)
+        {
+            r->data[i][j] = atof(value_token);
+            value_token = strtok_r(NULL," ",&save_ptr2);
+        }
+        
+        line_token = strtok_r(NULL,",",&save_ptr1);
+    }
     
+
+
+    return r;
+
+
+}
 
 
 int main()
 {   
-    // EXTRACT MATRIX DIMENSIONS
+    nml_mat* X;
 
-    char array_list[1024], array_str[1024], line_str[1024];
-    char *save_ptr1, *save_ptr2, *save_ptr3;
-    char *array_token, *line_token, *value_token;
-
-    strcpy(array_list,str);
-
-
-    // ITERATE THROUGH ARRAYS
-    array_token = strtok_r(array_list,"*",&save_ptr1); // Collect first array
-    while(array_token!=NULL)
-    {   
-
-        line_token = strtok_r(array_token,",",&save_ptr2); // Collect first line
-        printf("%s\n",line_token);
-
-        
-        // DETERMINE DIMENSIONS FROM FIRST LINE
-        value_token = strtok_r(line_token," ",&save_ptr3); // Collect first value from first line
-        int m = atoi(value_token);
-
-        value_token = strtok_r(NULL," ",&save_ptr3); // Collect second value from first line
-        int n = atoi(value_token);
-        printf("m: %d \t n: %d\n",m,n);
-
-
-        
-
-        // ITERATE THROUGH LINES
-        line_token = strtok_r(NULL,",",&save_ptr2); // Move to next line
-        for (int i = 0; i < m; i++)
-        {
-            printf("%s\n",line_token);
-
-            value_token = strtok_r(line_token," ",&save_ptr3); // Collect first value from first line
-            for (int j = 0; j < n; j++)
-            {
-                printf("%s\n",value_token);
-                value_token = strtok_r(NULL," ",&save_ptr3); // Move to next value
-            }
-            printf("\n");
-            
-
-            line_token = strtok_r(NULL,",",&save_ptr2);
-        }
-        
-
-        
-        array_token = strtok_r(NULL, "*", &save_ptr1);
-
-    }
-
-    
-
-
-    // // get the initial tokens
-    // token1 = strtok_r(buf_1, " ", &save_ptr1);
-    // token2 = strtok_r(buf_2, " ", &save_ptr2);
-    // token3 = strtok_r(buf_3, " ", &save_ptr3);
-
-    // while(token1 && token2 && token3) {
-    //     // do stuff with tokens
-
-    //     // get next tokens
-    //     token1 = strtok_r(NULL, " ", &save_ptr1);
-    //     token2 = strtok_r(NULL, " ", &save_ptr2);
-    //     token3 = strtok_r(NULL, " ", &save_ptr3);
-    // }
-
-
-    // // EXTRACT ARRAYS
-    // char* array_token = strtok(str, "*");
-    // while(array_token!= NULL){
-
-    //     char* array_str;
-    //     strcpy(array_str,array_token);
-    //     // char* line_token = strtok(array_token,",");
-    //     printf("%s\n",array_str);
-
-
-
-    //     // printf("m: %d \t n: %d\n",m,n);
-
-
-
-
-
-    
-    //     array_token = strtok(NULL, "*");
-    // }
-
-
-    // char* token = strtok(token_Line, " ");
-
-
-
-
+    X = nml_mat_fromstr(str);
+    nml_mat_print(X);
 
     
 
