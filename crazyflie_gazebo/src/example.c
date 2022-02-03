ad8572e9a000157abdf2033629bafc0e2ef4d8f4
bhabas <bhabas@psu.edu>
2022-02-03 12:39:41 -0500

    Created function to initialize NN layers and scalers

--- crazyflie_gazebo/src/example.c
+++ crazyflie_gazebo/src/example.c
@@ -1,61 +1,52 @@
 #include <example.h>
-
+#include "values.h"
 // Compile Statement
 // gcc example.c nml.c nml_util.c -I /home/bhabas/catkin_ws/src/crazyflie_simulation/crazyflie_gazebo/include  -Wall -o example -lm -Wall && ./example 
 
-/*
-Seperate arrays into strings grouped by "*"
-Seperate string by "," to get first line
-Seperate first line by " " to get dimensions
-Allocate matrix by dimensions
-Loop through rows
-    Loop through columns
-        Add value to matrix
 
-*/
-
-
-
-
-
-
-static char str[] = {
-    "3 2,"
-    "1.1  2,"
-    "3.4  4,"
-    "5.653  6,"
-    "*"
-    "3 3,"
-    "1  2  3,"
-    "4  5  6,"
-    "7  8  9,"
-
-};
-
-int main()
-{   
-    nml_mat* X1;
-    
+void initNN_Layers(Scaler* scaler,nml_mat* W[], nml_mat* b[], char path[],int numLayers)
+{
     char array_list[2048];
     char* array_token;
     char* save_ptr;
 
     strcpy(array_list,str);
 
+
     array_token = strtok_r(array_list,"*",&save_ptr);
-    
-    while(array_token!=NULL)
+    scaler->mean = nml_mat_fromstr(array_token);
+    array_token = strtok_r(NULL,"*",&save_ptr);
+
+    scaler->std = nml_mat_fromstr(array_token);
+    array_token = strtok_r(NULL,"*",&save_ptr);
+
+
+    nml_mat_print(scaler->mean);
+    nml_mat_print(scaler->std);
+
+    for (int i = 0; i < numLayers; i++)
     {
-        printf("%s\n",array_token);
-        X1 = nml_mat_fromstr(array_token);
-        nml_mat_print(X1);
+        W[i] = nml_mat_fromstr(array_token);
+        nml_mat_print(W[i]);
+        array_token = strtok_r(NULL,"*",&save_ptr);
+        b[i] = nml_mat_fromstr(array_token);
+        nml_mat_print(b[i]);
         array_token = strtok_r(NULL,"*",&save_ptr);
     }
 
-    
-    
+}
 
-    
+int main()
+{   
+
+    static Scaler scaler;
+
+    static nml_mat* W[3];
+    static nml_mat* b[3];
+
+    initNN_Layers(&scaler,W,b,str,3);
+
+    nml_mat_print(W[2]);
 
     return 0;
 }
