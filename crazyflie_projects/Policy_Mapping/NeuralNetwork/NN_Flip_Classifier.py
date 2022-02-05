f5f7b1c26e7f50f983791eb8774c50b82c2322d2
bhabas <bhabas@psu.edu>
2022-02-05 09:04:20 -0500

    Added model training function

--- crazyflie_projects/Policy_Mapping/NeuralNetwork/NN_Flip_Classifier.py
+++ crazyflie_projects/Policy_Mapping/NeuralNetwork/NN_Flip_Classifier.py
@@ -43,6 +43,8 @@ class NN_Flip_Classifier():
     def __init__(self,model_config):
         self.model_config = model_config
         self.modelInitials()
+        # self.loadModel()
+
         self.scaler = None
         self.loadScaler()
 
@@ -52,6 +54,9 @@ class NN_Flip_Classifier():
         charB = str[str.find("-")+1] # [L]ong
 
         self.model_initials = charA+charB
+
+    # def loadModel(self):
+    #     self.model = torch.load(f'{BASEPATH}/Pickle_Files/NN_Flip_Classifier_{self.model_initials}.pt')
 
     def createScaler(self,X):
 
@@ -70,7 +75,120 @@ class NN_Flip_Classifier():
 
 
     def loadScaler(self):
-        pass
+        
+        arr = np.loadtxt(
+            open(f"{BASEPATH}/Info/Scaler_Flip_{self.model_initials}.csv", "rb"),
+            delimiter=",",
+            skiprows=1)
+
+        self.scaler = preprocessing.StandardScaler()
+        self.scaler.mean_ = arr[:,0]
+        self.scaler.scale_ = arr[:,1]
+
+    def scaleData(self,X):
+
+        return self.scaler.transform(X)
+
+    def unscaleData(self,X_scaled):
+
+        return self.scaler.inverse_transform(X_scaled)
+
+    def modelForward(self,X):
+        X_scaled = self.scaleData(X)
+
+        with torch.no_grad():
+            y_pred = self.model.forward(X_scaled)
+
+        return y_pred
+
+    def trainModel(self,X_train,y_train,X_test,y_test,epochs=500):
+
+        ## CONVERT DATA ARRAYS TO TENSORS
+        X_train = torch.FloatTensor(X_train)
+        X_test = torch.FloatTensor(X_test)
+
+        y_train = torch.FloatTensor(y_train)
+        y_test = torch.FloatTensor(y_test)
+
+        ## INIT MODEL AND OPTIMIZER
+        self.model = NN_Model()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01)
+        class_weight = [0.1, 0.5] # Weights as binary classes [0,1]
+    
+        ## DEFINE TRAINING LOSS
+        weights = np.where(y_train==1,class_weight[1],class_weight[0])      # Convert class weights to element weights
+        criterion = nn.BCELoss(weight=torch.FloatTensor(weights))       
+        losses_train = []
+        accuracies_train = []
+
+        ## DEFINE VALIDATION LOSS
+        weights_val = np.where(y_test==1,class_weight[1],class_weight[0])   # Convert class weights to element weights
+        criterion_val = nn.BCELoss(weight=torch.FloatTensor(weights_val))   
+        losses_test = []
+        accuracies_test = []
+
+        for ii in range(epochs):
+
+            ## MODEL PREDICTION
+            y_pred_train = self.model.forward(X_train)
+
+            ## CALC TRAINING LOSS
+            loss_train = criterion(y_pred_train,y_train)
+            losses_train.append(loss_train.item())
+
+            ## CALC TRAINING ACCURACY
+            pred_cutoff = 0.5 
+            y_pred_train_class = np.where(y_pred_train.detach().numpy() < pred_cutoff,0,1)
+            accuracy_train = balanced_accuracy_score(y_train[:,0],y_pred_train_class)
+            accuracies_train.append(accuracy_train)
+
+            
+            ## CALC VALIDATION LOSS
+            with torch.no_grad():
+                y_pred_test = self.model.forward(X_test)
+
+            loss_test = criterion_val(y_pred_test,y_test)
+            losses_test.append(loss_test.item())
+
+            ## CALC TESTING ACCURACY
+            y_pred_test_class = np.where(y_pred_test.detach().numpy() < pred_cutoff,0,1)
+            accuracy = balanced_accuracy_score(y_test[:,0],y_pred_test_class)
+            accuracies_test.append(accuracy)
+
+            if ii%10 == 1:
+                print(f"epoch {ii} and loss is: {loss_train}")
+
+            ## BACKPROPAGATION
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+
+
+        torch.save(self.model,f'{BASEPATH}/Pickle_Files/NN_Flip_Classifier_{self.model_initials}.pt')
+
+        ## PLOT LOSSES AND ACCURACIES
+        fig = plt.figure(1,figsize=(12,8))
+        ax1 = fig.add_subplot(2,1,1)
+        ax1.plot(losses_train,label="Training Loss")
+        ax1.plot(losses_test,label="Validation Loss")
+        ax1.set_ylabel("Loss")
+        ax1.set_title("Losses")
+        ax1.set_ylim(0)
+        ax1.legend()
+        ax1.grid()
+
+        ax2 = fig.add_subplot(2,1,2)
+        ax2.plot(accuracies_train,label="Training Accuracry")
+        ax2.plot(accuracies_test,label="Validation Accuracy")
+        ax2.set_ylim(0,1.1)
+        ax2.set_ylabel("Classification Accuracy")
+        ax2.set_title("Balanced Accuracy")
+        ax2.grid()
+        ax2.legend(loc="lower right")
+
+
+        plt.tight_layout()
+        plt.show()
 
 
 
@@ -96,6 +214,24 @@ if __name__ == "__main__":
     y = np.where(y < 0.8,0,1)
 
     X = np.stack((RREV,OF_y,d_ceil),axis=1)
+
+    ## SPLIT DATA FEATURES INTO TRAINING AND TESTING SETS
+    data_array = np.stack((RREV,OF_y,d_ceil,y),axis=1)
+    df = pd.DataFrame(data_array,columns=['RREV','OF_y','d_ceil','y'])
+    # df = df.sort_values(by='y')
+
+    train_df, test_df = train_test_split(df,test_size=0.25,random_state=73)
+    test_df = test_df.sort_values(by='y')
+
+    X_train = train_df[['RREV','OF_y','d_ceil']].to_numpy()
+    X_test = test_df[['RREV','OF_y','d_ceil']].to_numpy()
+    y_train = train_df[['y']].to_numpy()
+    y_test = test_df[['y']].to_numpy()
+
+
+
     FC.createScaler(X)
+    FC.trainModel(X_train,y_train,X_test,y_test,epochs=500)
+    # FC.modelForward(X)
 
 
