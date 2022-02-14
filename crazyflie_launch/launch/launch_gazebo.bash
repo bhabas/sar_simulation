2cf639340a75f4ef4dfd8db5262cfc171e46aab2
bhabas <bhabas@psu.edu>
2022-02-14 17:40:30 -0500

    Updated launch file

--- crazyflie_launch/launch/launch_gazebo.bash
+++ crazyflie_launch/launch/launch_gazebo.bash
@@ -6,7 +6,8 @@ source ~/catkin_ws/devel/setup.bash
 roslaunch crazyflie_launch params.launch
 MODEL_NAME=$(rosparam get /MODEL_NAME)
 GUI_FLAG=$(rosparam get /GUI_FLAG)
-roslaunch crazyflie_launch crazyflie_sitl.launch world_name:="ceiling" model_name:=$MODEL_NAME gui:=$GUI_FLAG
+WORLD_NAME=$(rosparam get /WORLD_NAME)
+roslaunch crazyflie_launch crazyflie_gazebo.launch world_name:=$WORLD_NAME model_name:=$MODEL_NAME gui:=$GUI_FLAG
 # Script does not move past this line
 
 # Command | ROS Package (not strictly folder name) | launch file | launch parameters
