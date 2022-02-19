77cce10a6322b5ceb07ffd974fb77b3e63653f9d
bhabas <bhabas@psu.edu>
2022-02-19 11:52:48 -0500

    Initialized stabilizer loop

--- crazyflie_control2/src/stabilizer.cpp
+++ crazyflie_control2/src/stabilizer.cpp
@@ -1,9 +1,27 @@
+#include "stabilizer.h"
+#include "stabilizer_types.h"
 #include "nml.h"
 #include "controller_gtc.h"
+#include <ros/ros.h>
+
+
+uint32_t tick;
 
 int main(int argc, char **argv)
 {
-    // ros::init(argc, argv, "test_include_library");
-    // ros::NodeHandle nh;
-    printHello();
+
+    ros::init(argc, argv, "Stabilizer_Node");
+    ros::NodeHandle nh;
+
+    tick = 1;
+    ros::Rate rate(RATE_MAIN_LOOP);
+
+    while(ros::ok)
+    {
+        printHello();
+        rate.sleep();
+
+        tick++;
+    }
+    return 0;
 }
