6c1f702122ca660bd5df0583314e41424a18103f
bhabas <bhabas@psu.edu>
2022-02-19 19:53:12 -0500

    Working OF plugin with body relative values not world

--- crazyflie_gazebo/include/OF_Sensor_plugin.h
+++ crazyflie_gazebo/include/OF_Sensor_plugin.h
@@ -9,15 +9,12 @@
 
 
 #include <ros/ros.h>
-#include "crazyflie_msgs/RLCmd.h"
-#include "crazyflie_msgs/MS.h"
-#define g2Newton (9.81f/1000.0f)
-#define Newton2g (1000.0f/9.81f)
+
 
 
 namespace gazebo {
 
-    class GazeboMotorPlugin: public ModelPlugin
+    class OF_SensorPlugin: public ModelPlugin
     {
         public:
             
@@ -25,20 +22,35 @@ namespace gazebo {
             void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
             void OnUpdate();
 
-            // void MotorSpeedCallback(const crazyflie_msgs::MS::ConstPtr &msg);
-
 
         private:
             physics::WorldPtr world_;
             physics::ModelPtr model_;
-            physics::JointPtr joint_ptr;
+            // physics::JointPtr joint_ptr;
             physics::LinkPtr link_ptr;
 
 
             event::ConnectionPtr updateConnection;
 
-            ros::NodeHandle nh;
-            // ros::Subscriber MS_Subscriber = nh.subscribe<crazyflie_msgs::MS>("/MS", 1, &GazeboMotorPlugin::MotorSpeedCallback, this, ros::TransportHints().tcpNoDelay());
+            std::string linkName;
+
+            double h_ceiling = 2.10;
+
+            double Vx_rel = 0.0; // [m/s]
+            double Vy_rel = 0.0; // [m/s]
+            double Vz_rel = 0.0; // [m/s]
+            double d_ceil = 0.0; // [m]
+
+            double Tau = 0.0;   // [s]
+            double RREV = 0.0;  // [rad/s]
+            double OFx = 0.0;   // [rad/s]
+            double OFy = 0.0;   // [rad/s]
+
+
+            
+
+            // ros::NodeHandle nh;
+            // ros::Subscriber MS_Subscriber = nh.subscribe<crazyflie_msgs::MS>("/MS", 1, &OF_SensorPlugin::MotorSpeedCallback, this, ros::TransportHints().tcpNoDelay());
     };
 
 }
--- crazyflie_gazebo/src/OF_Sensor_plugin.cpp
+++ crazyflie_gazebo/src/OF_Sensor_plugin.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
-#include <motor_plugin.h>
+#include "OF_Sensor_plugin.h"
 
 
 namespace gazebo
 {
 
     // This gets called when model is loaded and pulls values from sdf file
-    void GazeboMotorPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr _sdf)
+    void OF_SensorPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr _sdf)
     {
-        gzmsg << "Loading GazeboMotorPlugin\n";
+        gzmsg << "Loading OF_SensorPlugin\n";
         model_ = parent;
 
-        
+        linkName = _sdf->GetElement("bodyName")->Get<std::string>();
+        gzmsg << "\t Link Name:\t" << linkName << std::endl;
+        link_ptr = model_->GetLink(linkName);
+
 
         // RUN FUNCTION EACH TIME SIMULATION UPDATES
-        updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&GazeboMotorPlugin::OnUpdate, this));
+        updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&OF_SensorPlugin::OnUpdate, this));
+
+        // ROS_WARN("loaded ModelPush Plugin with parent...%s",model_->GetName().c_str());
 
         std::cout << "\n\n";
     }
 
-    void GazeboMotorPlugin::OnUpdate()
+    void OF_SensorPlugin::OnUpdate()
     {
-        
+        // DEFINE
+        Vx_rel = link_ptr->RelativeLinearVel().X();
+        Vy_rel = link_ptr->RelativeLinearVel().Y();
+        Vz_rel = link_ptr->RelativeLinearVel().Z();
+        d_ceil = h_ceiling - link_ptr->WorldPose().Z();
+
+        Tau = d_ceil/Vz_rel;
+        RREV = Vz_rel/d_ceil;
+        OFx = -Vy_rel/d_ceil;
+        OFy = -Vx_rel/d_ceil;
+
+        printf("Tau: %.3f \t RREV: %.3f \t OFx: %.3f \t OFy: %.3f \t d_ceil: %.3f\n",Tau,RREV,OFx,OFy,d_ceil);
+
+        model_->SetLinearVel(ignition::math::Vector3d(0.5, 0.5, 2.5));
+
     }
 
 
@@ -31,5 +50,5 @@ namespace gazebo
 
     // }
 
-    GZ_REGISTER_MODEL_PLUGIN(GazeboMotorPlugin);
+    GZ_REGISTER_MODEL_PLUGIN(OF_SensorPlugin);
 }
