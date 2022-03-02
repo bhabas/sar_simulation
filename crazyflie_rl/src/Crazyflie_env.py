39a800fa7cfb819575a0f3636776d3fc8755a7e0
bhabas <bhabas@psu.edu>
2022-03-02 17:04:59 -0500

    Added callback to crazyflie_env with main data

--- crazyflie_rl/src/Crazyflie_env.py
+++ crazyflie_rl/src/Crazyflie_env.py
@@ -117,7 +117,7 @@ class CrazyflieEnv:
 
         ## INITIALIZE IMPACT VALUES
         self.impact_flag = False
-        self.body_contact = False   # Flag if model body impacts ceiling plane
+        self.BodyContact_flag = False   # Flag if model body impacts ceiling plane
 
         self.t_impact = 0.0
         self.posCF_impact = [0,0,0]
@@ -216,21 +216,165 @@ class CrazyflieEnv:
     ##   Publishers/Subscribers 
     # ============================
 
-    def CF_StateDataCallback(self,msg):
+    def CF_StateDataCallback(self,StateData_msg):
 
-        pass
+        self.t = np.round(StateData_msg.header.stamp.to_sec(),4)
 
-    def CF_FlipDataCallback(self,msg):
+        self.posCF = np.round([ StateData_msg.Pose.position.x,
+                                StateData_msg.Pose.position.y,
+                                StateData_msg.Pose.position.z],3)
 
-        pass
+        
 
-    def CF_ImpactDataCallback(self,msg):
+        self.quatCF = np.round([StateData_msg.Pose.orientation.x,
+                                StateData_msg.Pose.orientation.y,
+                                StateData_msg.Pose.orientation.z,
+                                StateData_msg.Pose.orientation.w],3)
 
-        pass
+        self.eulCF = np.round([ StateData_msg.Eul.x,
+                                StateData_msg.Eul.y,
+                                StateData_msg.Eul.z],3)
 
-    def CF_MiscDataCallback(self,msg):
+        ## CF_TWIST
+        self.velCF = np.round([ StateData_msg.Twist.linear.x,
+                                StateData_msg.Twist.linear.y,
+                                StateData_msg.Twist.linear.z],3)
 
-        pass
+        self.omegaCF = np.round([StateData_msg.Twist.angular.x,
+                                 StateData_msg.Twist.angular.y,
+                                 StateData_msg.Twist.angular.z],3)
+
+        ## CF_VISUAL STATES
+        self.Tau = np.round(StateData_msg.Tau,3)
+        self.OFx = np.round(StateData_msg.OFx,3)
+        self.OFy = np.round(StateData_msg.OFy,3)
+        self.RREV = np.round(StateData_msg.RREV,3)
+        self.d_ceil = np.round(StateData_msg.D_ceil,3)
+
+        ## CONTROLLER ACTIONS
+        self.FM = np.round([StateData_msg.FM[0],
+                            StateData_msg.FM[1],
+                            StateData_msg.FM[2],
+                            StateData_msg.FM[3]],3)
+
+        self.MS_pwm = np.round([StateData_msg.MS_PWM[0],
+                                StateData_msg.MS_PWM[1],
+                                StateData_msg.MS_PWM[2],
+                                StateData_msg.MS_PWM[3]],0)
+
+        self.NN_flip = np.round(StateData_msg.NN_flip,3)
+        self.NN_policy = np.round(StateData_msg.NN_policy,3)
+
+        self.x_d = np.round([StateData_msg.x_d.x,
+                             StateData_msg.x_d.y,
+                             StateData_msg.x_d.z],3)
+
+        self.v_d = np.round([StateData_msg.v_d.x,
+                             StateData_msg.v_d.y,
+                             StateData_msg.v_d.z],3)
+
+        self.a_d = np.round([StateData_msg.a_d.x,
+                             StateData_msg.a_d.y,
+                             StateData_msg.a_d.z],3)
+
+    def CF_FlipDataCallback(self,FlipData_msg):
+
+        ## FLIP FLAGS
+        self.flip_flag = FlipData_msg.flip_flag
+
+        self.t_tr = np.round(FlipData_msg.header.stamp.to_sec(),4)
+
+        ## CF_POSE (FLIP)
+        self.posCF_tr = np.round([FlipData_msg.Pose_tr.position.x,
+                                  FlipData_msg.Pose_tr.position.y,
+                                  FlipData_msg.Pose_tr.position.z],3)
+
+        self.quatCF_tr = np.round([FlipData_msg.Pose_tr.orientation.x,
+                                   FlipData_msg.Pose_tr.orientation.y,
+                                   FlipData_msg.Pose_tr.orientation.z,
+                                   FlipData_msg.Pose_tr.orientation.w],3)
+
+        self.eulCF_tr = np.round([FlipData_msg.Eul_tr.x,
+                                  FlipData_msg.Eul_tr.y,
+                                  FlipData_msg.Eul_tr.z],3)
+
+        ## CF_TWIST (FLIP)
+        self.velCF_tr = np.round([FlipData_msg.Twist_tr.linear.x,
+                                  FlipData_msg.Twist_tr.linear.y,
+                                  FlipData_msg.Twist_tr.linear.z],3)
+
+        self.omegaCF_tr = np.round([FlipData_msg.Twist_tr.angular.x,
+                                    FlipData_msg.Twist_tr.angular.y,
+                                    FlipData_msg.Twist_tr.angular.z],3)
+
+        ## CF_VISUAL STATES (FLIP)
+        self.Tau_tr = np.round(FlipData_msg.Tau_tr,3)
+        self.OFx_tr = np.round(FlipData_msg.OFx_tr,3)
+        self.OFy_tr = np.round(FlipData_msg.OFy_tr,3)
+        self.RREV_tr = np.round(FlipData_msg.RREV_tr,3)
+        self.d_ceil_tr = np.round(FlipData_msg.D_ceil_tr,3)
+
+        ## CONTROLLER ACTIONS
+        self.FM_tr = np.round([FlipData_msg.FM_tr[0],
+                               FlipData_msg.FM_tr[1],
+                               FlipData_msg.FM_tr[2],
+                               FlipData_msg.FM_tr[3]],3)
+
+
+        self.NN_tr_flip = np.round(FlipData_msg.NN_tr_flip,3)
+        self.NN_tr_policy = np.round(FlipData_msg.NN_tr_policy,3)
+
+    def CF_ImpactDataCallback(self,ImpactData_msg):
+
+        ## IMPACT FLAGS
+        self.impact_flag = ImpactData_msg.impact_flag
+        self.BodyContact_flag = ImpactData_msg.BodyContact_flag
+
+        self.t_impact = np.round(ImpactData_msg.header.stamp.to_sec(),4)
+
+        ## CF_POSE (IMPACT)
+        self.posCF_impact = np.round([ImpactData_msg.Pose_impact.position.x,
+                                      ImpactData_msg.Pose_impact.position.y,
+                                      ImpactData_msg.Pose_impact.position.z],3)
+
+        
+
+        self.quatCF_impact = np.round([ImpactData_msg.Pose_impact.orientation.x,
+                                       ImpactData_msg.Pose_impact.orientation.y,
+                                       ImpactData_msg.Pose_impact.orientation.z,
+                                       ImpactData_msg.Pose_impact.orientation.w],3)
+
+        self.eulCF_impact = np.round([ImpactData_msg.Eul_impact.x,
+                                      ImpactData_msg.Eul_impact.y,
+                                      ImpactData_msg.Eul_impact.z],3)
+
+        ## CF_TWIST (IMPACT)
+        self.velCF_impact = np.round([ImpactData_msg.Twist_impact.linear.x,
+                                      ImpactData_msg.Twist_impact.linear.y,
+                                      ImpactData_msg.Twist_impact.linear.z],3)
+
+        self.omegaCF_impact = np.round([ImpactData_msg.Twist_impact.angular.x,
+                                        ImpactData_msg.Twist_impact.angular.y,
+                                        ImpactData_msg.Twist_impact.angular.z],3)
+
+        ## IMPACT FORCES
+        self.Force_impact = np.round([ImpactData_msg.Force_impact.x,
+                                      ImpactData_msg.Force_impact.y,
+                                      ImpactData_msg.Force_impact.z],3)
+
+        self.impact_magnitude = np.round(ImpactData_msg.Impact_Magnitude,3)
+
+        ## STICKY PAD CONNECTIONS
+        self.pad_connections = ImpactData_msg.Pad_Connections
+        self.Pad1_Contact = ImpactData_msg.Pad1_Contact
+        self.Pad2_Contact = ImpactData_msg.Pad2_Contact
+        self.Pad3_Contact = ImpactData_msg.Pad3_Contact
+        self.Pad4_Contact = ImpactData_msg.Pad4_Contact
+
+
+    def CF_MiscDataCallback(self,MiscData_msg):
+
+        self.V_battery = np.round(MiscData_msg.battery_voltage,4)
     
 
     def RL_Publish(self):
