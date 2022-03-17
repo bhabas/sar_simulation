9c22f9ca0ddd6a42e5a7af71dd0bd3cb94e972eb
bhabas <bhabas@psu.edu>
2022-03-17 10:23:44 -0400

    Updated analytical example with final derivation

--- crazyflie_projects/Featureless_TTC/Analytical_Example_Angled-Flight.py
+++ crazyflie_projects/Featureless_TTC/Analytical_Example_Angled-Flight.py
@@ -12,17 +12,18 @@ L = 0.05    # [m]
 ## CAMERA PARAMETERS
 WIDTH_PIXELS = 160
 HEIGHT_PIXELS = 160
-FPS = 100                # Frame Rate [1/s]
+FPS = 100               # Frame Rate [1/s]
 w = 3.6e-6              # Pixel width [m]
-f = 0.66e-3/2             # Focal Length [m]
+f = 0.66e-3/2           # Focal Length [m]
 # f_effective = f/2 # halve focal length if half the pixels
 O_x = WIDTH_PIXELS/2    # Pixel X_offset [pixels]
 O_y = HEIGHT_PIXELS/2   # Pixel Y_offset [pixels]
 
 
-z_0 = 1.3    # Camera height [m]
-vz = -1    # Camera velocity [m/s]
-vx = 0.0
+d_0 = 0.6   # Initial Camera height [m]
+vz = 0.7    # Camera velocity [m/s]
+vx = 0.3
+vy = 0.3
 
 ## PRE-ALLOCATE IMAGE ARRAY [pixels]
 u_p = np.arange(0,WIDTH_PIXELS,1)
@@ -41,19 +42,19 @@ def I_continuous(u_p,z_0,t):
     return I_0/2 * np.sin(2*np.pi*(u*d/(f*L) + vx*t/L)) + I_0/2
 
 
-def I_pixel(u_p,v_p,z_0,t):
+def I_pixel(u_p,v_p,d_0,t):
 
     ## CONVERT PIXEL INDEX TO METERS
     u = (u_p - O_x)*w + w/2 
     v = (v_p - O_y)*w + w/2 
 
-    z = z_0 + vz*t
+    d = d_0 - vz*t
 
     ## RETURN AVERAGE BRIGHTNESS VALUE OVER PIXEL
     # I = I_0/2 * (f*L/(np.pi*z*w) * np.sin(np.pi*z*w/(f*L) * np.sin(2*np.pi*(z*u/(f*L) + vx*t/L))) + 1)
 
-    I_x = I_0/2*(np.sin(2*np.pi*(z*u/(f*L) + vx*t/L) ) + 1)
-    I_y = I_0/2*(np.sin(2*np.pi*(z*v/(f*L) + 0*t/L) ) + 1)
+    I_x = I_0/2*(np.sin(2*np.pi*(d*u/(f*L) + vx*t/L) ) + 1)
+    I_y = I_0/2*(np.sin(2*np.pi*(d*v/(f*L) + vy*t/L) ) + 1)
     I = (I_x+I_y)/2
 
 
@@ -67,7 +68,7 @@ def I_pixel(u_p,v_p,z_0,t):
 ## IMAGE SENSOR PLOT
 fig = plt.figure()
 ax = fig.add_subplot(111)
-im = ax.imshow(I_pixel(U_p,V_p,z_0,0), interpolation='none', 
+im = ax.imshow(I_pixel(U_p,V_p,d_0,0), interpolation='none', 
                 vmin=0, vmax=255, cmap=cm.gray,
                 origin='upper',
 )
@@ -85,20 +86,21 @@ def cam_alg(Cur_img,Prev_img):
         [-1,0,1]
     ]) 
     Ky = 1/8 *np.array([ # SOBEL KERNEL Y
-        [ 1, 2, 1],
+        [-1,-2,-1],
         [ 0, 0, 0],
-        [-1,-2,-1]])
+        [ 1, 2, 1]
+    ])
 
-    Ix = np.zeros_like(Cur_img)
-    Iy = np.zeros_like(Cur_img)
+    Iu = np.zeros_like(Cur_img)
+    Iv = np.zeros_like(Cur_img)
 
-    Cur_img = cv.GaussianBlur(Cur_img,(5,5),0)
-    Prev_img = cv.GaussianBlur(Prev_img,(5,5),0)
+    # Cur_img = cv.GaussianBlur(Cur_img,(5,5),0)
+    # Prev_img = cv.GaussianBlur(Prev_img,(5,5),0)
 
     for i in range(1,HEIGHT_PIXELS - 1): 
         for j in range(1,WIDTH_PIXELS - 1):
-            Ix[i,j] = np.sum(Cur_img[i-1:i+2,j-1:j+2] * Kx)/(w)
-            Iy[i,j] = -np.sum(Cur_img[i-1:i+2,j-1:j+2] * Ky)/(w)
+            Iu[i,j] = np.sum(Cur_img[i-1:i+2,j-1:j+2] * Kx)/w
+            Iv[i,j] = np.sum(Cur_img[i-1:i+2,j-1:j+2] * Ky)/w
 
 
 
@@ -106,26 +108,28 @@ def cam_alg(Cur_img,Prev_img):
 
     U = (U_p - O_x)*w + w/2 
     V = (V_p - O_y)*w + w/2
-    G = U*Ix + V*Iy
+    G = U*Iu + V*Iv
 
     X = np.array([
-        [np.sum(Ix**2),np.sum(Ix*Iy),np.sum(G*Ix)],
-        [np.sum(Ix*Iy),np.sum(Iy**2),np.sum(G*Iy)],
-        [np.sum(G*Ix),np.sum(G*Iy),np.sum(G**2)]
+        [f*np.sum(Iu**2), f*np.sum(Iu*Iv), np.sum(G*Iu)],
+        [f*np.sum(Iu*Iv), f*np.sum(Iv**2), np.sum(G*Iv)],
+        [f*np.sum(G*Iu),  f*np.sum(G*Iv),  np.sum(G**2)]
     ])
 
-    y = -np.array([[np.sum(Ix*It)],[np.sum(Iy*It)],[np.sum(G*It)]])
+    y = np.array([[-np.sum(Iu*It)],[-np.sum(Iv*It)],[np.sum(-G*It)]])
 
     b = np.linalg.pinv(X)@y
 
     
-    return b
+    return b.flatten()
 
 
 Tau_act_List = []
 Tau_est_List = []
-Vx_act_List = []
-Vx_est_List = []
+OFy_act_List = []
+OFy_ext_List = []
+OFx_act_List = []
+OFx_ext_List = []
 t_List = []
 
 
@@ -133,33 +137,42 @@ t_List = []
 num_sec = 3
 def animate_func(i):
 
-    t_contact = z_0/np.abs(vz)
-    t = i/FPS # time [s]
-    if t>=t_contact:
+
+    ## UPDATE CAMERA POSITION
+    t = i/FPS   # time [s]
+
+    d = (d_0 - vz*t)    # Distance to Ceiling [m]
+    Tau_act = d/vz
+    OFy_act = -vx/d
+    OFx_act = -vy/d
+
+    ## STOP ANIMATION IF PAST CEILING SURFACE
+    if Tau_act <= 0:
         return
+    
 
-    Cur_img = I_pixel(U_p,V_p,z_0,t)
-    Prev_img = I_pixel(U_p,V_p,z_0,t-1/FPS)
+    ## CALCULATE OPTICAL FLOW ESTIMATES
+    Cur_img = I_pixel(U_p,V_p,d_0,t)
+    Prev_img = I_pixel(U_p,V_p,d_0,t-1/FPS)
     b = cam_alg(Cur_img,Prev_img)
-    d = (z_0 + vz*t)
-    Tau_act = -d/vz
-    Vx_act = -vx/d
+    
+    print(f"Tau_act: {Tau_act:.3f} | Tau_est: {1/b[2]:.3f}")
+    print(f"OFy_act: {OFy_act:.3f} | OFy_est: {b[0]:.3f}")
+    print(f"OFx_act: {OFx_act:.3f} | OFx_est: {b[1]:.3f}\n")
 
 
-    print(f"Tau_act: {Tau_act:.3f} | Tau_est: {1/b[2,0]:.3f}")
-    print(f"Vx_act: {Vx_act:.3f} | Vx_est: {b[0,0]/f:.3f}")
-
-
-    Tau_est_List.append(1/b[2,0])
+    ## APPEN OPTICAL FLOW ESTIMATES TO LIST FOR PLOTTING
+    Tau_est_List.append(1/b[2])
     Tau_act_List.append(Tau_act)
-    Vx_act_List.append(Vx_act)
-    Vx_est_List.append(b[0,0]/f)
-
+    OFy_act_List.append(OFy_act)
+    OFy_ext_List.append(b[0])
+    OFx_act_List.append(OFx_act)
+    OFx_ext_List.append(b[1])
 
     t_List.append(t)
 
     ## UPDATE IMAGE
-    # im.set_array(I_pixel(U_p,V_p,z_0,t))
+    # im.set_array(I_pixel(U_p,V_p,d_0,t))
     
 anim = animation.FuncAnimation(fig, 
                                animate_func, 
@@ -170,27 +183,48 @@ anim = animation.FuncAnimation(fig,
 
 plt.show()
 
+## TAU PLOT
 fig2 = plt.figure(1)
 ax = fig2.add_subplot(111)
 
-ax.plot(t_List,Tau_est_List,'rx',label="Tau_algortihm")
+ax.plot(t_List,Tau_est_List,'rx',label="Tau_estimate")
 ax.plot(t_List,Tau_act_List,label="Tau_actual")
+ax.set_title('Tau Estimation')
+ax.set_xlabel('Time [s]')
+ax.set_ylabel('Tau [s]')
 ax.grid()
 ax.legend()
 
 fig2.tight_layout()
 
 
-
+## OFy PLOT
 fig3 = plt.figure(2)
 ax = fig3.add_subplot(111)
 
-ax.plot(t_List,Vx_est_List,'rx',label="OFy_estimate")
-ax.plot(t_List,Vx_act_List,label="OFy_actual")
+ax.plot(t_List,OFy_ext_List,'rx',label="OFy_estimate")
+ax.plot(t_List,OFy_act_List,label="OFy_actual")
+ax.set_title('OFy Estimation')
+ax.set_xlabel('Time [s]')
+ax.set_ylabel('OFy [rad/s]')
 ax.grid()
 ax.legend()
 
 fig3.tight_layout()
+
+## OFx PLOT
+fig4 = plt.figure(3)
+ax = fig4.add_subplot(111)
+
+ax.plot(t_List,OFx_ext_List,'rx',label="OFx_estimate")
+ax.plot(t_List,OFx_act_List,label="OFx_actual")
+ax.set_title('OFx Estimation')
+ax.set_xlabel('Time [s]')
+ax.set_ylabel('OFx [rad/s]')
+ax.grid()
+ax.legend()
+
+fig4.tight_layout()
 
 plt.show()
 
