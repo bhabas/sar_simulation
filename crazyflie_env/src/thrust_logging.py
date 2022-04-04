d67a0185654c5fd8b11c4aaa9ef7e61e5099886a
bhabas <bhabas@psu.edu>
2022-04-04 14:23:11 -0400

    Cleaned up results for thrust decay verification

--- crazyflie_env/src/thrust_logging.py
+++ crazyflie_env/src/thrust_logging.py
@@ -15,6 +15,7 @@ sys.path.insert(1,BASE_PATH)
 
 from Crazyflie_env import CrazyflieEnv
 from crazyflie_msgs.msg import MS
+from crazyflie_msgs.msg import RLCmd
 
 
 
@@ -33,13 +34,62 @@ if __name__ == '__main__':
 
     def logging_Callback(msg):
 
-        if msg.Motor_Number == 1:
+        if msg.Motor_Number == 4:
             with open(filePath,mode='a') as data_file:
                 data_writer = csv.writer(data_file,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 data_writer.writerow([np.round(env.t,3),np.round(msg.MotorThrust,3),np.round(msg.MotorThrust_actual,3)])
 
 
     rospy.Subscriber("/CF_Internal/MS",MS,logging_Callback,queue_size=1)
+    rospy.Publisher('/RL/cmd',RLCmd,queue_size=10)
+    input("Start")
+
+    t_start = env.t
+    thrust_d = 0
+    OnceFlag1 = True
+    OnceFlag2 = True
+    OnceFlag3 = True
+    OnceFlag4 = True
+    OnceFlag5 = True
 
 
-    rospy.spin()
+    while True:
+        t = env.t - t_start
+
+        if t>=0 and OnceFlag1:
+            OnceFlag1 = False
+            thrust_d = 0.0
+            env.step('thrusts',[0,0,0],thrust_d)
+            print(f"Thrust[g] Cmd: {thrust_d:.3f}")
+
+
+        if t >= 5 and OnceFlag2:
+            OnceFlag2 = False
+            thrust_d = 8.14
+            env.step('thrusts',[0,0,0],thrust_d)
+            print(f"Thrust[g] Cmd: {thrust_d:.3f}")
+
+        if t >= 10 and OnceFlag3:
+            OnceFlag3 = False
+            thrust_d = 14.3
+            env.step('thrusts',[0,0,0],thrust_d)
+            print(f"Thrust[g] Cmd: {thrust_d:.3f}")
+
+        if t >= 15 and OnceFlag4:
+            OnceFlag4 = False
+            thrust_d = 5.3
+            env.step('thrusts',[0,0,0],thrust_d)
+            print(f"Thrust[g] Cmd: {thrust_d:.3f}")
+
+        if t >= 20 and OnceFlag5:
+            OnceFlag5 = False
+            thrust_d = 12.0
+            env.step('thrusts',[0,0,0],thrust_d)
+            print(f"Thrust[g] Cmd: {thrust_d:.3f}")
+
+        if t>=25:
+            env.step('thrusts',[0,0,0],0)
+            break
+
+    input("End Here")
+
