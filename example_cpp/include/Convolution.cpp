07138542b39b46c47afa83db4b34ac58132916bd
hufflycodes <wesley.c.huff@gmail.com>
2022-04-13 11:13:54 -0400

    Un-generalized 3x3 convolution working

--- example_cpp/include/Convolution.cpp
+++ example_cpp/include/Convolution.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 
 //Initial test image
 int img[9] = {10,12,19,13,20,7,6,12,18};
@@ -13,18 +14,22 @@ int ky0[3] = {-1,-2,-1};
 int ky2[3] = {1,2,1};
 float Output[9] = {0}; //init all entries to be zero
 
-bool Convolution(unsigned int in, float out, int ImgX, int ImgY); //init function
+int Ix[9] = {0};
+int Iy[9] = {0};
+
+bool Convolution(int in[9], int ImgX, int ImgY); //init function
 
 
 int main(){
     
    // Convolution(); //init function
-    
+    Convolution(img,3,3);
     return 1;
 
 }
 
-bool Convolution(unsigned int in, float out, int ImgX, int ImgY,int Ksize) {
+bool Convolution(int in[9], int ImgX, int ImgY) 
+{
 
     //Where the convolution starts
     int X = 1;
@@ -34,21 +39,75 @@ bool Convolution(unsigned int in, float out, int ImgX, int ImgY,int Ksize) {
     int y1;
     int y2;
 
-    for(int j = 0; j < (ImgX - 1)*(ImgY - 1); j++) // How many times the kernel center moves around the image
+    //Rolling sum
+    int Xsum = 0;
+    int Ysum = 0;
+    
+    for(int j = 0; j < (ImgX - 2)*(ImgY - 2); j++) // How many times the kernel center moves around the image
     {
 
-        if(X == ImgX - 1){ //if the edge of the kernel hits the edge of the image
+        //GENERALIZE FOR CHANGE IN KERNEL SIZE
+        if(X == ImgX - 1) //if the edge of the kernel hits the edge of the image
+        { 
         
             X = 1; //move the kernel back to the left edge of the image
             Y++; //and slide the kernel down the image
 
         }
+
         //sub Kernel indexing locations
-        int x0 = X - 1, x1 = X - 1, x2 = X - 1; 
+        int x0 = X , x1 = X , x2 = X; 
         int y0 = Y * ImgX;
         int y2 = y0 + (ImgX * 2); //skipping the middle y kernel so move the last Ysub kernel two rows down
 
-        int i0 = x0 + 
+        //Can do this in the aboce step just doing this for readability
+        int i0 = x0 - 1;
+        int i1 = i0 + ImgX;
+        int i2 = i1 + ImgX;
+
+        // ######  DEBUGGING  ######
+        /*
+        std::cout << "i0: " << i0 << "\n";
+        std::cout << "i1: " << i1 << "\n";
+        std::cout << "i2: " << i2 << "\n";
+        */
+
+        //GENERALIZE FOR CHANGE IN KERNEL SIZE
+        for(int k = 0; k < 3; k++){
+
+            //Sub kernel 0
+            Xsum += kx0[k] * img[i0 + k];
+            Ysum += ky0[k] * img[i0 + k];
+
+            //Sub kernel 1 (skipping ky1)
+            Xsum += kx1[k] * img[i1 + k];
+
+            //Sub kernel 2
+            Xsum += kx2[k] * img[i2 + k];
+            Ysum += ky2[k] * img[i2 + k];
+
+        }
+
+        //Sum assigned to middle value
+        Ix[i1 + 1] = Xsum;
+        Iy[i1 + 1] = Ysum;
+        X++;
+
+        for(int j = 0; j < 10; j++){
+
+            printf("%d\n",Ix[j]);
+
+        }
+
+        std::cout << "\n Iy \n";
+
+        for(int j = 0; j < 10; j++){
+
+            printf("%d\n",Iy[j]);
+
+        }
+
+        std::cout << "loop \n";
 
     }
 
