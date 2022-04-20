e2013e9d40aa8139e713e1feea1dcc6e7dd38095
bhabas <bhabas@psu.edu>
2022-04-19 21:12:22 -0400

    Updated P2P trajectory

--- crazyflie_projects/Trajectory_Generation/P2P_Trajectory.py
+++ crazyflie_projects/Trajectory_Generation/P2P_Trajectory.py
@@ -1,32 +1,55 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def Theta(s,theta_0,theta_f):
 
-    theta = theta_0 + s*(theta_f - theta_0)
+def Theta(t,T,theta_0,theta_f):
+    
+    return theta_0 + (3*t**2/T**2 - 2*t**3/T**3)*(theta_f - theta_0)
 
-    return theta
+def dTheta(t,T,theta_0,theta_f):
 
-def s(t,T):
-    a_0 = 0
-    a_1 = 0
-    a_2 = 3/T**2
-    a_3 = -2/T**3
+    return (6*t/T**2 - 6*t**2/T**3)*(theta_f - theta_0)
 
-    return a_0 + a_1*t + a_2*t**2 + a_3*t**3
+def ddTheta(t,T,theta_0,theta_f):
 
-t = np.linspace(0,2.5,50)
-s_val = s(t,t[-1])
+    return (6/T**2 - 12*t/T**3)*(theta_f - theta_0)
+
+
+a_max = 1.0 # [m/s^2]
+x_0 = 1.0
+x_f = 3.0
+
+T = np.sqrt(6/a_max*np.abs(x_f - x_0))
+t = np.linspace(0,T,50)
+
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(t,Theta(s_val,0,3))
+ax = fig.add_subplot(311)
+ax.plot(t,Theta(t,T,x_0,x_f))
 
 ax.set_xlim(0,5)
 ax.set_ylim(0,5)
 ax.set_xlabel('Time [s]')
 ax.set_ylabel('Position [m]')
 ax.grid()
+
+ax2 = fig.add_subplot(312)
+ax2.plot(t,dTheta(t,T,x_0,x_f))
+
+ax2.set_xlim(0,5)
+ax2.set_xlabel('Time [s]')
+ax2.set_ylabel('Velocity [m/s]')
+ax2.grid()
+
+
+ax3 = fig.add_subplot(313)
+ax3.plot(t,ddTheta(t,T,x_0,x_f))
+
+ax3.set_xlim(0,5)
+ax3.set_xlabel('Time [s]')
+ax3.set_ylabel('Acceleration [m/s^2]')
+ax3.grid()
+
 
 fig.tight_layout()
 plt.show()
