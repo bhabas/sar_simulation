a0854003b95a23de4e3196c391b05c1ede5d01bc
bhabas <bhabas@psu.edu>
2022-04-21 10:01:40 -0400

    NML solution example

--- example_cpp/src/Convolution.cpp
+++ example_cpp/src/Convolution.cpp
@@ -166,14 +166,48 @@ void Convolution_X_Y(unsigned char* input, int ImgX, int ImgY)
     //DO MATRIX MAGIC
     
 
-    //Create matrix pointers
-    nml_mat* lhs = nml_mat_from(3,3,9, LHS);
-    nml_mat* rhs = nml_mat_from(1,3,3,RHS); 
+    
+    // INITIALIZE MATRICES FROM DATA
+    nml_mat* m_A = nml_mat_from(3,3,9, LHS);
+    nml_mat* m_B = nml_mat_from(3,1,3,RHS); 
+
+    nml_mat_print(m_A);
+    nml_mat_print(m_B);
 
 
+    // SOLVE FOR OPTICAL FLOW VALUES
+    nml_mat_lup *LUP = nml_mat_lup_solve(m_A);
+    nml_mat* x = nml_ls_solve(LUP,m_B);
+    nml_mat_print(x);
 
-    nml_mat_free(lhs);
-    nml_mat_free(rhs);
+    // DEALLOCATE MATRIX MEMORY
+    nml_mat_free(m_A);
+    nml_mat_free(m_B);
+    nml_mat_lup_free(LUP);
+    nml_mat_free(x);
+
+
+    // // EXAMPLE SOLVE Ax = b
+
+    // double A[9] = {
+    //     1.0, 2.0, 3.0,
+    //     4.0, 5.0, 6.0,
+    //     7.0, 8.0, 1.0,
+    // };
+
+    // double b[3] = {5.0,3.0,1.0};
+    
+    // nml_mat* m_A = nml_mat_from(3,3,9,A);
+    // nml_mat* m_b = nml_mat_from(3,1,3,b);
+    // nml_mat_lup *LUP = nml_mat_lup_solve(m_A);
+
+    // nml_mat *x = nml_ls_solve(LUP, m_b);
+    // nml_mat_print(x);
+
+    // nml_mat_free(m_A);
+    // nml_mat_free(m_b);
+    // nml_mat_lup_free(LUP);
+    // nml_mat_free(x);
 
     // ======= DEBUGGING ========
 
