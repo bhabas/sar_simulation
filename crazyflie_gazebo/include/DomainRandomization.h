fe63bb8aa7e9e47b743c3027c6cab53800090700
bhabas <bhabas@psu.edu>
2022-04-28 10:15:28 -0400

    Update model inertia through Domain Randomizatin plugin

--- crazyflie_gazebo/include/DomainRandomization.h
+++ crazyflie_gazebo/include/DomainRandomization.h
@@ -6,15 +6,8 @@
 #include <gazebo/common/common.hh>
 
 
-
-
 #include <ros/ros.h>
-#include "crazyflie_msgs/RLCmd.h"
-#include "crazyflie_msgs/activateSticky.h"
 #include "crazyflie_msgs/domainRand.h"
-
-
-
 
 
 namespace gazebo {
@@ -25,7 +18,6 @@ namespace gazebo {
             
         protected:
             void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
-            void RLCmd_Callback(const crazyflie_msgs::RLCmd::ConstPtr &msg);
             bool UpdateInertia(crazyflie_msgs::domainRand::Request &req, crazyflie_msgs::domainRand::Response &res);
 
 
@@ -38,13 +30,12 @@ namespace gazebo {
             physics::InertialPtr inertia_ptr;
             ros::ServiceServer DomainRandService;
 
+            ros::NodeHandle nh;
             std::string linkName;
 
 
 
 
-            ros::NodeHandle nh;
-            ros::Subscriber MS_Subscriber = nh.subscribe<crazyflie_msgs::RLCmd>("/RL/cmd", 5, &DomainRand_plugin::RLCmd_Callback, this, ros::TransportHints().tcpNoDelay());
     };
 
 }
--- crazyflie_gazebo/src/DomainRandomization.cpp
+++ crazyflie_gazebo/src/DomainRandomization.cpp
@@ -23,49 +23,23 @@ namespace gazebo
 
     bool DomainRand_plugin::UpdateInertia(crazyflie_msgs::domainRand::Request &req, crazyflie_msgs::domainRand::Response &res)
     {
-        printf("ehere\n");
+        printf("[DOMAIN RAND PLUGIN] Inertia Values Updated\n");
+        inertia_ptr->SetMass(req.mass);
+        inertia_ptr->SetInertiaMatrix(
+            req.Inertia.x,
+            req.Inertia.y,
+            req.Inertia.z,
+            0.0,
+            0.0,
+            0.0);
+
+        link_ptr->UpdateMass();
+        link_ptr->Update();
+
+        res.srv_Success = true;
         return true;
     }
 
-
-    void DomainRand_plugin::RLCmd_Callback(const crazyflie_msgs::RLCmd::ConstPtr &msg)
-    {
-        if(msg->cmd_type == 50)
-        {
-            printf("Domain Rand Plugin\n");
-            // inertia_ptr->SetMass(1.0);
-            // inertia_ptr->SetInertiaMatrix(
-            //     15.83e-6,
-            //     17.00e-5,
-            //     31.19e-6,
-            //     0.0,
-            //     0.0,
-            //     0.0);
-            ignition::math::Pose3d pose(
-                ignition::math::Vector3d(0, 0, 0.2),
-                ignition::math::Quaterniond(1.0, 0.0, 0.0, 0.0)
-            );
-            inertia_ptr->SetCoG(pose);
-            link_ptr->UpdateMass();
-            link_ptr->Update();
-        }
-        if(msg->cmd_type == 51)
-        {
-            // printf("%.3f\n",inertia_ptr->CoG().);
-            std::cout << inertia_ptr->CoG() << std::endl;
-            // inertia_ptr->UpdateParameters()
-        }
-        else if(msg->cmd_type == 0)
-        {
-            ignition::math::Pose3d pose(
-                ignition::math::Vector3d(0, 0, 0.5),
-                ignition::math::Quaterniond(1.0, 0.0, 0.0, 0.0)
-            );
-            model_ptr->SetWorldPose(pose);
-            model_ptr->SetWorldTwist(ignition::math::Vector3d(0, 0, 0),ignition::math::Vector3d(0, 0, 0));
-
-        }
-    }
 
     GZ_REGISTER_MODEL_PLUGIN(DomainRand_plugin);
 }
