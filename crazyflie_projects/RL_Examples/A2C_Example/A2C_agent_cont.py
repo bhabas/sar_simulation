fe8bc6b277c744bcee7b24f7acc173afdaeccaa5
bhabas <bhabas@psu.edu>
2022-05-07 16:53:11 -0400

    Actor Critic example in continuous action space

--- crazyflie_projects/RL_Examples/A2C_Example/A2C_agent_cont.py
+++ crazyflie_projects/RL_Examples/A2C_Example/A2C_agent_cont.py
@@ -1,172 +1,109 @@
 import gym
+from gym import wrappers
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import torch
+import torch as T
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.distributions import Categorical
 
-DEVICE = "cuda:0"
-EPISODES = 2000
-GAMMA = 0.99
-NUM_STEPS = 50
-LR = 0.0001
-RENDER = True
-
-
-class A2C(nn.Module):
-    def __init__(self,num_inputs,num_actions,hidden_size,learning_rate=LR):
-        super(A2C,self).__init__()
-
-        self.num_inputs = num_inputs
-        self.num_actions = num_actions
-
-        self.base = nn.Sequential(
-            nn.Linear(num_inputs,hidden_size),
-            nn.ReLU(),
-        )
-
-        self.actor = nn.Sequential(
-            nn.Linear(hidden_size,num_actions),
-            nn.Softmax(dim=1),
-        )
-
-        self.critic = nn.Sequential(
-            nn.Linear(hidden_size,1)
-        )
+class GenericNetwork(nn.Module):
+    def __init__(self,lr,input_dims,fc1_dims,fc2_dims,n_actions):
+        super(GenericNetwork,self).__init__()
+        self.lr = lr
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
 
 
-        self.optimizer = optim.Adam(self.parameters(),lr=learning_rate)
-        self.to(DEVICE)
+        self.fc1 = nn.Linear(*self.input_dims,self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims,self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims,self.n_actions)
 
-    def forward(self,state):
+        self.optimizer = optim.Adam(self.parameters(),lr=self.lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device) # Send network to device
 
-        state = torch.tensor(state.reshape(1,-1),dtype=torch.float).to(DEVICE) # Convert state to tensor
+    def forward(self, observation):
+        state = T.tensor(observation,dtype=T.float).to(self.device)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
 
-        base_out = self.base(state)
-        policy_dist = self.actor(base_out)
-        V_state = self.critic(base_out)
+        return x
 
-        return V_state,policy_dist
+class Agent(object):
+    def __init__(self,alpha,beta,input_dims,gamma=0.99,n_actions=2,
+                    layer1_size=64,layer2_size=64,n_outputs=1):
+        self.gamma = gamma
+        self.log_probs = None
+        self.n_outputs = n_outputs
+        self.actor = GenericNetwork(alpha,input_dims,layer1_size,layer2_size,n_actions=2)   # Outputs mu and var
+        self.critic = GenericNetwork(beta,input_dims,layer1_size,layer2_size,n_actions=1)   # Outputs V_state
 
-    def choose_action(self,state):
+    def choose_action(self,observation):
+        mu,sigma = self.actor.forward(observation)
+        sigma = T.exp(sigma) # Ensures sigma is positive (TEST: softplus)
 
-        ## FIND ACTION PROBABILITIES FOR CURRENT STATE 
-        state = torch.tensor(state.reshape(1,-1),dtype=torch.float).to(DEVICE) # Convert state to tensor
-        value, policy_dist = self.Actor_Critic.forward(state) # Probability for each action
+        ## SAMPLE ACTION FROM DISTRIBUTION
+        action_probs = T.distributions.Normal(mu,sigma)
+        probs = action_probs.sample(sample_shape=T.Size([self.n_outputs]))
+        action = T.tanh(probs) # Clamp to [-1,1] range
 
-        ## SAMPLE FROM PROBABILITY DISTRIBUTION
-        dist = Categorical(policy_dist)
-        action = dist.sample()
+        ## CONVERT PROB. OF ACTION TO LOG PROB.
+        self.log_probs = action_probs.log_prob(probs).to(self.actor.device)
 
-        return action.item(),value.item(),policy_dist # Check for valid data types
+        return action.item()
+
+    def learn(self,state,reward,new_state,done):
+
+        ## ZERO NETWORK GRADIENTS
+        self.actor.optimizer.zero_grad()
+        self.critic.optimizer.zero_grad()
+
+        ## CALCULATE STATE VALUES AND REWARDS
+        critic_value = self.critic.forward(state)
+        critic_value_ = self.critic.forward(new_state)
+        reward = T.tensor(reward,dtype=T.float).to(self.actor.device)
+
+        ## CALC ADVANTAGE
+        delta = reward + self.gamma*critic_value*(1-int(done)) - critic_value_ # If episode is over then discard future term
+
+        ## CALCULATE LOSS
+        actor_loss = -self.log_probs*delta # Minimize policy gradient
+        critic_loss = delta**2  # Minimize loss
+
+        ## UPDATE NETWORKS
+        (actor_loss + critic_loss).backward()
+        self.actor.optimizer.step()
+        self.critic.optimizer.step()
 
 
-if __name__ == "__main__":
-    env = gym.make("CartPole-v1")
+if __name__ == '__main__':
+    agent = Agent(alpha=5e-6,beta=10e-5,input_dims=[2],gamma=0.99,
+            layer1_size=256,layer2_size=256)
 
-    num_inputs = env.observation_space.shape[0]
-    num_actions = env.action_space.n
-    
-    agent = A2C(num_inputs,num_actions,hidden_size=256)
-    ep_score_list = []
-    avg_ep_score_list = []
-    entropy_term = 0
-
-    for episode in range(EPISODES):
-
-        ## RESET EPISODE
+    env = gym.make('MountainCarContinuous-v0')
+    score_history = []
+    num_episodes = 100
+    for i in range(num_episodes):
         done = False
-        state = env.reset()
-        ep_score = 0
+        score = 0
+        observation = env.reset()
 
         while not done:
+            action = np.array(agent.choose_action(observation)).reshape((1,))
+            observation_,reward,done,info = env.step(action)
+            agent.learn(observation,reward,observation_,done)
+            observation = observation_
+            score += reward
 
-            ## INITIALIZE EPISODE MINIBATCH
-            log_probs = []
-            V_states = []
-            rewards = []
-
-            for k in range(NUM_STEPS):
-
-                if RENDER:
-                    # env.render()
-                    pass
-                
-                ## SELECT ACTION a_k USING ACTOR pi_theta
-                V_state, policy_dist = agent.forward(state)
-                V_state = V_state.item()
-                policy_dist_np = policy_dist.cpu().detach().numpy()
-                action = np.random.choice(num_actions,p=np.squeeze(policy_dist_np))
-
-                log_prob = torch.log(policy_dist.squeeze()[action])
-                entropy = -np.sum(np.mean(policy_dist_np)*np.log(policy_dist_np))
-
-                ## PERFORM ACTION a_k AND OBSERVE NEXT STATE s_k+1 AND REWARD r_k+1
-                next_state,reward,done,_ = env.step(action)
-
-                ## STORE IN EPISODE MINIBATCH
-                rewards.append(reward)
-                V_states.append(V_state)
-                log_probs.append(log_prob)
-                entropy_term += entropy
-
-                if done:
-                    break
-
-                state = next_state
-
-            ## IF s_n IS NOT TERMINAL: SET R=V(s_n) WITH CRITIC, ELSE R = 0
-            if not done:
-                V_f,_ = agent.forward(next_state)
-            else:
-                V_f = 0
-
-            ## CALCULATE DISCOUNTED RETURNS
-            Rs = np.zeros_like(V_states)
-            R = V_f
-            for k in reversed(range(len(V_states))):
-                R = rewards[k] + GAMMA*R
-                Rs[k] = R
-
-            
-
-            ## UPDATE ACTOR-CRITIC NETWORKS
-            V_states = torch.tensor(V_states,dtype=torch.float).to(DEVICE)
-            Rs = torch.tensor(Rs,dtype=torch.float).to(DEVICE)
-            log_probs = torch.stack(log_probs)
-
-            advantage = Rs - V_states
-            actor_loss = (-log_probs*advantage).mean()
-            critic_loss = advantage.pow(2).mean()
-            AC_Loss = actor_loss + critic_loss + 0.001*entropy_term*0.0
-
-            agent.optimizer.zero_grad()
-            AC_Loss.backward()
-            agent.optimizer.step()
-
-            ## UPDATE EPISODE SCORE
-            ep_score += np.sum(rewards)
-
-        ## APPEND EPISODE DATA
-        ep_score_list.append(ep_score)
-        avg_ep_score_list.append(np.nanmean(ep_score_list[-20:]))
-
-        if episode%100 == 0:
+        score_history.append(score)
+        if i%1 == 0:
             RENDER = True
-            print(f"EPISODE: {episode} Score: {np.sum(rewards)} MA_Reward: {np.nanmean(ep_score_list[-20:]):.2f}")
+            print(f"EPISODE: {i} Score: {score}")
         else:
             RENDER = False
-
-
-
-    # PLOT RESULTS
-    plt.plot(range(EPISODES),ep_score_list)
-    plt.plot(range(EPISODES), avg_ep_score_list)
-    plt.show()
-        
-
-
