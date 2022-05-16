954d4b6301c0551e8dfc325d01feb1fd4cc2adda
hufflycodes <wesley.c.huff@gmail.com>
2022-05-16 14:54:25 -0400

    Added OF plots and error bars to test optimal ceiling pattern

--- crazyflie_projects/Featureless_TTC/Gazebo_cpp_check.py
+++ crazyflie_projects/Featureless_TTC/Gazebo_cpp_check.py
@@ -19,12 +19,11 @@ class CameraClass:
 
         # Pre_init vars
         self.Tau_act_List = [0]
-        self.Tau_est_List = [0]
         self.Tau_cpp_est_List = [0]
         self.OFy_act_List = [0]
-        self.OFy_ext_List = [0]
+        self.OFy_est_List = [0]
         self.OFx_act_List = [0]
-        self.OFx_ext_List = [0]
+        self.OFx_est_List = [0]
         self.t_List = [0]
 
         self.Cur_img = np.array([])
@@ -33,6 +32,8 @@ class CameraClass:
         self.OFx = 0
         self.OFy = 0
         self.Tau_est_cpp = 0
+        self.OFy_est = 0
+        self.OFx_est = 0
         self.d_ceil = 0
         self.prev_time = 0
 
@@ -44,11 +45,7 @@ class CameraClass:
 
         self.time = np.round(Cam_msg.header.stamp.to_sec(),6) #grabbing timestamp
         self.Cur_img = np.frombuffer(Cam_msg.data,np.uint8).reshape(WIDTH_PIXELS,HEIGHT_PIXELS) #pulling in image to process
-
-        self.cam_alg() #once the images are updated time to process them
-
-        # if(self.t != self.prev_time):
-        #     self.cam_alg(self)
+        self.logs()
        
 
     def CF_StateDataCallback(self,StateData_msg):
@@ -57,70 +54,9 @@ class CameraClass:
         self.OFx = np.round(StateData_msg.OFx,3)
         self.OFy = np.round(StateData_msg.OFy,3)
         self.Tau_est_cpp = np.round(StateData_msg.Tau_est,3)
+        self.OFy_est = np.round(StateData_msg.OFy_est,3)
+        self.OFx_est = np.round(StateData_msg.OFx_est,3)
         self.d_ceil = np.round(StateData_msg.D_ceil,3)
-
-
-    def cam_alg(self):
-
-        O_up = WIDTH_PIXELS/2    # Pixel X_offset [pixels]
-        O_vp = HEIGHT_PIXELS/2   # Pixel Y_offset [pixels]
-        w = 3.6e-6              # Pixel width [m]
-        f = 0.66e-3/2           # Focal Length [m]
-
-        ## PRE-ALLOCATE IMAGE ARRAY [pixels]
-        u_p = np.arange(0,WIDTH_PIXELS,1)
-        v_p = np.arange(0,HEIGHT_PIXELS,1)
-        U_p,V_p = np.meshgrid(u_p,v_p)
-
-        Kx = 1/8 * np.array([ # NORMALIZED SOBEL KERNAL (U-DIRECTION)
-            [-1,0,1],
-            [-2,0,2],
-            [-1,0,1]
-        ]) 
-        Ky = 1/8 *np.array([ # NORMALIZED SOBEL KERNAL (V--DIRECTION)
-            [-1,-2,-1],
-            [ 0, 0, 0],
-            [ 1, 2, 1]
-        ])
-
-        Iu = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
-        Iv = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
-
-        U_grid = (U_p - O_up)*w + w/2 
-        V_grid = (V_p - O_vp)*w + w/2
-
-        ## FIND IMAGE GRADIENTS
-        for i in range(1,HEIGHT_PIXELS - 1): 
-            for j in range(1,WIDTH_PIXELS - 1):
-                Iu[i,j] = np.sum(self.Cur_img[i-1:i+2,j-1:j+2] * Kx)/w
-                Iv[i,j] = np.sum(self.Cur_img[i-1:i+2,j-1:j+2] * Ky)/w
-
-        It = (self.Cur_img - self.Prev_img)/(self.time - self.prev_time) # Time Gradient
-
-        print(f"Change in time: {(self.time - self.prev_time):.4f}") #checking to make sure dt is 1/120
-
-        G = U_grid*Iu + V_grid*Iv # Radial Gradient
-
-        ## SOLVE LEAST SQUARES PROBLEM
-        X = np.array([
-            [f*np.sum(Iu**2), f*np.sum(Iu*Iv), np.sum(G*Iu)],
-            [f*np.sum(Iu*Iv), f*np.sum(Iv**2), np.sum(G*Iv)],
-            [f*np.sum(G*Iu),  f*np.sum(G*Iv),  np.sum(G**2)]
-        ])
-
-        y = np.array([
-            [-np.sum(Iu*It)],
-            [-np.sum(Iv*It)],
-            [-np.sum(G*It)]
-                ])
-
-        ## SOLVE b VIA PSEUDO-INVERSE
-        self.b = np.linalg.pinv(X)@y
-        self.b = self.b.flatten()
-
-        self.Prev_img = self.Cur_img #set Previous image to Current one before reiterating 
-        self.prev_time = self.time
-        self.logs()
 
     
     def logs(self):
@@ -130,20 +66,19 @@ class CameraClass:
         OFy_act = self.OFy
         OFx_act = self.OFx
         
-        print(f"Tau_act: {Tau_act:.3f} | Tau_est: {1/self.b[2]:.3f} | Tau_est_cpp: {self.Tau_est_cpp}")
-        print(f"OFy_act: {OFy_act:.3f} | OFy_est: {self.b[0]:.3f}")
-        print(f"OFx_act: {OFx_act:.3f} | OFx_est: {self.b[1]:.3f}")
+        print(f"Tau_act: {Tau_act:.3f} | Tau_est_cpp: {self.Tau_est_cpp}")
+        print(f"OFy_act: {OFy_act:.3f} | OFy_est: {self.OFy_est:.3f}")
+        print(f"OFx_act: {OFx_act:.3f} | OFx_est: {self.OFx_est:.3f}")
         print(f"Distance to Ceiling {self.d_ceil:.3f}\n")
 
 
         ## APPEN OPTICAL FLOW ESTIMATES TO LIST FOR PLOTTING
-        self.Tau_est_List.append(1/self.b[2])
         self.Tau_act_List.append(Tau_act)
         self.Tau_cpp_est_List.append(self.Tau_est_cpp)
         self.OFy_act_List.append(OFy_act)
-        self.OFy_ext_List.append(self.b[0])
+        self.OFy_est_List.append(self.OFy_est)
         self.OFx_act_List.append(OFx_act)
-        self.OFx_ext_List.append(self.b[1])
+        self.OFx_est_List.append(self.OFx_est)
         self.t_List.append(self.time)
 
         if (self.d_ceil < 0.1):
@@ -152,13 +87,12 @@ class CameraClass:
 
     def plotter(self):
         
-        fig = plt.figure()
+        fig = plt.figure(1)
         
-
         ## PLOT TAU VALUES
         ax1 = fig.add_subplot(211)
-        ax1.plot(self.t_List,self.Tau_act_List,color='tab:blue',label='Tau')
-        ax1.plot(self.t_List,self.Tau_cpp_est_List,color='r',linestyle='--',label='Tau_est')
+        ax1.plot(self.t_List,self.Tau_act_List,color = 'tab:blue',label = 'Tau')
+        ax1.plot(self.t_List,self.Tau_cpp_est_List,color = 'r',linestyle = '--',label = 'Tau_est')
         ax1.grid()
         ax1.legend(loc='upper right')
         ax1.set_ylabel("Tau [s]")
@@ -168,7 +102,11 @@ class CameraClass:
         Tau_act = np.array(self.Tau_act_List) #make lists into arrays to do math on them
         Tau_est = np.array(self.Tau_cpp_est_List)
         ax2 = fig.add_subplot(212,sharex = ax1)
-        ax2.plot(self.t_List,(Tau_act - Tau_est),color='r',label="Error in Tau")
+        ax2.plot(self.t_List,(Tau_est - Tau_act),color = 'r',label = "Error in Tau")
+        ax2.hlines(y =  0.05, xmin = 0, xmax = self.t_List[-1],linestyle = "--", linewidth = 2, color = 'k') #plot desired error bounds
+        ax2.hlines(y = -0.05, xmin = 0, xmax = self.t_List[-1],linestyle = "--", linewidth = 2, color = 'k')
+        ax2.vlines(x = (self.t_List[-1] - 1), ymin = -0.05, ymax = 0.05, linestyle = "--", linewidth = 2, color = "k")
+        ax2.vlines(x = (self.t_List[-1]), ymin = -0.05, ymax = 0.05, linestyle = "--", linewidth = 2, color = "k")
         ax2.grid()
         ax2.legend(loc='upper right')
         ax2.set_ylabel("Error")
@@ -177,18 +115,43 @@ class CameraClass:
         plt.show()
 
 
-        # ## TAU PLOT
-        # # plt.plot(self.t_List,self.Tau_est_List,'rx',label="Tau_estimate")
-        # plt.plot(self.t_List,self.Tau_act_List,label="Tau_actual")
-        # plt.plot(self.t_List,self.Tau_cpp_est_List,label = "Tau_cpp_estimate")
-        # plt.title('Tau Estimation')
-        # plt.xlabel('Time [s]')
-        # plt.ylabel('Tau [s]')
-        # plt.grid()
-        # plt.legend()
-        # plt.show()
+        ## OFX_Y PLOTS
+        fig2 = plt.figure(2)
+        #OFy Plots
+        ax1 = fig2.add_subplot(221)
+        ax1.plot(self.t_List,self.OFy_act_List, color = "tab:blue", label = "OFy")
+        ax1.plot(self.t_List,self.OFy_est_List, color = "r", linestyle = "--", label = "OFy_est")
+        ax1.grid()
+        ax1.legend(loc='upper right')
+        ax1.set_ylabel("OFy [rad/s]")
+        ax1.set_xlabel("Time [s]")
+        #OFy error plots
+        ax2 = fig2.add_subplot(222,sharex = ax1)
+        OFy_act = np.array(self.OFy_act_List) #make lists into arrays to do math on them
+        OFy_est = np.array(self.OFy_est_List)
+        ax2.plot(self.t_List,(OFy_est - OFy_act),color = "r",label = "Error in OFy")
+        ax2.grid()
+        ax2.legend(loc='upper right')
+        ax2.set_ylabel("Error")
+        ax2.set_xlabel("Time [s]")
 
-
+        #OFx Plots
+        ax3 = fig2.add_subplot(223)
+        ax3.plot(self.t_List,self.OFx_act_List, color = "tab:blue", label = "OFx")
+        ax3.plot(self.t_List,self.OFx_est_List, color = "r", linestyle = "--", label = "OFx_est")
+        ax3.grid()
+        ax3.legend(loc='upper right')
+        ax3.set_ylabel("OFx [rad/s]")
+        ax3.set_xlabel("Time [s]")
+        #OFx error plots
+        ax4 = fig2.add_subplot(224,sharex = ax3)
+        OFy_act = np.array(self.OFy_act_List) #make lists into arrays to do math on them
+        OFy_est = np.array(self.OFy_est_List)
+        ax4.plot(self.t_List,(OFy_est - OFy_act),color = "r",label = "Error in OFy")
+        ax4.grid()
+        ax4.legend(loc='upper right')
+        ax4.set_ylabel("Error")
+        ax4.set_xlabel("Time [s]")
 
 
 if __name__ == '__main__':
