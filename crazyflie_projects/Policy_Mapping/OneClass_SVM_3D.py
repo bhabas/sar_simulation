0bed4ee0b929694f8bb4edbabbd110139a4fd812
bhabas <bhabas@psu.edu>
2022-05-20 20:33:46 -0400

    Working OC_SVM for scaled policy data

--- crazyflie_projects/Policy_Mapping/OneClass_SVM_3D.py
+++ crazyflie_projects/Policy_Mapping/OneClass_SVM_3D.py
@@ -1,27 +1,55 @@
 import numpy as np
+from sklearn import preprocessing
 from sklearn.svm import OneClassSVM
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import plotly.graph_objects as go
 
-## LOAD TRAINING DATA
-r = np.linspace(0,1,5)
-theta = np.linspace(0,2*np.pi,15)
-ZZ = np.linspace(-1,1,15)
+np.set_printoptions(suppress=True)
+BASEPATH = "/home/bhabas/catkin_ws/src/crazyflie_simulation/crazyflie_projects/Policy_Mapping"
 
-R,THETA,ZZ = np.meshgrid(r,theta,ZZ)
 
-XX = R*np.cos(THETA)
-YY = R*np.sin(THETA)
-ZZ = ZZ
 
-X2 = np.stack((XX.flatten(),YY.flatten(),ZZ.flatten()),axis=1)
+# np.random.seed(0)
+
+
+## LOAD DATA
+df_raw = pd.read_csv(f"{BASEPATH}/Data_Logs/NL_DR/NL_LR_Trials_DR.csv").dropna() # Collected data
+# df_raw = df_raw.query("60 <= phi_IC <= 70")
+df_raw = df_raw.query("LR_4leg >= 0.8")
+
+
+## ORGANIZE DATA
+Tau = df_raw["Tau_flip_mean"]
+OF_y = df_raw["OFy_flip_mean"]
+d_ceil = df_raw["D_ceil_flip_mean"]
+landing_rate = df_raw["LR_4leg"]
+
+X2 = np.stack((Tau,OF_y,d_ceil),axis=1)
+
+
+
+
+# ## LOAD TRAINING DATA
+# r = np.linspace(0,1,5)
+# theta = np.linspace(0,2*np.pi,15)
+# ZZ = np.linspace(-1,1,15)
+
+# R,THETA,ZZ = np.meshgrid(r,theta,ZZ)
+
+# XX = R*np.cos(THETA)
+# YY = R*np.sin(THETA)
+# ZZ = ZZ
+
+# X2 = np.stack((XX.flatten(),YY.flatten(),ZZ.flatten()),axis=1)
+scaler = preprocessing.StandardScaler().fit(X2)
+X2 = scaler.transform(X2)
 
 
 # Learn a frontier for outlier detection with several classifiers
-xx2, yy2, zz2 = np.meshgrid(np.linspace(-2, 2, 20), np.linspace(-2, 2, 20),np.linspace(-2, 2, 20))
-clf_name = "OCSVM"
-clf = OneClassSVM(nu=0.3, gamma=8)
+xx2, yy2, zz2 = np.meshgrid(np.linspace(-4, 4, 20), np.linspace(-4, 4, 20),np.linspace(-4, 4, 20))
+clf = OneClassSVM(nu=0.1,gamma=0.4)
 plt.figure(2)
 clf.fit(X2)
 Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel(), zz2.ravel()])
@@ -34,13 +62,15 @@ Z2 = Z2.reshape(xx2.shape)
 fig = go.Figure()
 
 fig.add_trace(
-    go.Isosurface(
+    go.Volume(
         x=xx2.flatten(),
         y=yy2.flatten(),
         z=zz2.flatten(),
         value = Z2.flatten(),
-        isomin=0.0,
-        isomax=0.01,
+        isomin=-0.1,
+        isomax=0.1,
+        surface_count=3,
+        colorscale='Viridis',
         opacity=0.1
         )
 )
@@ -55,9 +85,11 @@ fig.add_trace(
         mode='markers',
         marker=dict(
         size=3,
-        color=X2[:, 2],                # set color to an array/list of desired values
+        color=landing_rate,                # set color to an array/list of desired values+
+        cmin=0,
+        cmax=1,
         colorscale='Viridis',   # choose a colorscale
-        opacity=0.1
+        opacity=0.5
     )
     )
 )
