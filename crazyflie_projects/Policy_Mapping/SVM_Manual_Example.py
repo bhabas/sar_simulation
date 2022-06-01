f803093aca04ddfe3556d416fd1a61d075d6a505
bhabas <bhabas@psu.edu>
2022-06-01 15:19:47 -0400

    Finally working SVM manual classification

--- crazyflie_projects/Policy_Mapping/SVM_Manual_Example.py
+++ crazyflie_projects/Policy_Mapping/SVM_Manual_Example.py
@@ -5,25 +5,40 @@ import numpy as np
 
 
 # # we create 40 separable points
-# X, y = make_blobs(n_samples=40, centers=2, random_state=6)
+X, y = make_blobs(n_samples=40, centers=2, random_state=6)
+
+def kernel(x,xi,gamma):
+    return np.exp(-gamma*np.linalg.norm(x-xi)**2)
+    # np.sum(np.power((actual_value-predicted_value),2))
+
+def dec_func(clf,X_test):
+    val = 0
+    for ii in range(len(clf.support_vectors_)):
+        val += clf.dual_coef_[0,ii]*kernel(clf.support_vectors_[ii],X_test,clf._gamma)
+    val += clf.intercept_
+
+    return val
 
 
-X = np.array([
-    [0, 0], 
-    [1, 1]])
+# X = np.array([
+#     [0, 0], 
+#     [1, 1]])
     
-y = [0, 1]
+# y = [0, 1]
 # fit the model, don't regularize for illustration purposes
-clf = svm.SVC(kernel="linear")
+clf = svm.SVC(kernel="rbf")
 clf.fit(X, y)
 
-xx = np.linspace(-3,3,100)
-yy = np.linspace(-3,3,100)
-XX,YY = np.meshgrid(xx,yy)
+tmp = np.linspace(-10,10,100)
+XX,YY = np.meshgrid(tmp,tmp)
 X_contour = np.stack((XX.flatten(),YY.flatten()),axis=1)
 Z = clf.decision_function(X_contour)
+# Z = dec_func(clf,X_contour)
 
-X_test = np.array([[0.5,0.5]])
+X_test = np.array([[7.65,0.83]])
+# clf.dual_coef_[0,1]*kernel(clf.support_vectors_[1],X_test,clf._gamma)
+
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
