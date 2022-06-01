0a6b246022f10ab2ca06f0827210c466e31af9ed
bhabas <bhabas@psu.edu>
2022-06-01 13:17:27 -0400

    SVM work

--- crazyflie_projects/Policy_Mapping/SVM_Manual_Example.py
+++ crazyflie_projects/Policy_Mapping/SVM_Manual_Example.py
@@ -4,41 +4,33 @@ from sklearn.datasets import make_blobs
 import numpy as np
 
 
-# we create 40 separable points
-X, y = make_blobs(n_samples=40, centers=2, random_state=6)
+# # we create 40 separable points
+# X, y = make_blobs(n_samples=40, centers=2, random_state=6)
 
+
+X = np.array([
+    [0, 0], 
+    [1, 1]])
+    
+y = [0, 1]
 # fit the model, don't regularize for illustration purposes
-clf = svm.SVC(kernel="linear", C=1000)
+clf = svm.SVC(kernel="linear")
 clf.fit(X, y)
+
+xx = np.linspace(-3,3,100)
+yy = np.linspace(-3,3,100)
+XX,YY = np.meshgrid(xx,yy)
+X_contour = np.stack((XX.flatten(),YY.flatten()),axis=1)
+Z = clf.decision_function(X_contour)
+
+X_test = np.array([[0.5,0.5]])
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
+ax.scatter(X[:, 0], X[:, 1], c=y, s=30,)
 
-# get the separating hyperplane
-w = clf.coef_[0]
-a = -w[0] / w[1]
-xx = np.linspace(4,10)
-yy = a * xx - (clf.intercept_[0]) / w[1]
-
-margin = 1 / np.sqrt(np.sum(clf.coef_**2))
-yy_down = yy - np.sqrt(1 + a**2) * margin
-yy_up = yy + np.sqrt(1 + a**2) * margin
-
-ax.plot(xx, yy, "k-")
-ax.plot(xx, yy_down, "k--")
-ax.plot(xx, yy_up, "k--")
-
-# DecisionBoundaryDisplay.from_estimator(
-#     clf,
-#     X,
-#     plot_method="contour",
-#     colors="k",
-#     levels=[-1, 0, 1],
-#     alpha=0.5,
-#     linestyles=["--", "-", "--"],
-#     ax=ax,
-# )
+cs = ax.contour(XX,YY,Z.reshape(XX.shape),levels=[-1,0,1])
+ax.clabel(cs, inline=True)
 # plot support vectors
 ax.scatter(
     clf.support_vectors_[:, 0],
