91fc3bcc80c89f7343ee62916fcbb4db276ac4ed
bhabas <bhabas@psu.edu>
2022-06-04 15:47:45 -0400

    Added actor and critic networks

--- crazyflie_projects/DeepRL/CF_Env/CF_Env.py
+++ crazyflie_projects/DeepRL/CF_Env/CF_Env.py
@@ -5,22 +5,112 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
 
+import os
+
+class ActorNetwork(nn.Module):
+    def __init__(self, input_dims, alpha, fc1_dims=256, fc2_dims=256,
+            chkpt_dir='/home/bhabas/catkin_ws/src/crazyflie_simulation/crazyflie_projects/DeepRL/CF_Env'):
+        super(ActorNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
+        self.actor = nn.Sequential(
+                nn.Linear(input_dims, fc1_dims),
+                nn.ReLU(),
+                nn.Linear(fc1_dims, fc2_dims),
+                nn.ReLU(),
+                nn.Linear(fc2_dims, 1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+
+        return self.actor(state)
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class CriticNetwork(nn.Module):
+    def __init__(self, input_dims, alpha, fc1_dims=256, fc2_dims=256,
+            chkpt_dir='/home/bhabas/catkin_ws/src/crazyflie_simulation/crazyflie_projects/DeepRL/CF_Env'):
+        super(CriticNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'critic_torch_ppo')
+        self.critic = nn.Sequential(
+                nn.Linear(input_dims, fc1_dims),
+                nn.ReLU(),
+                nn.Linear(fc1_dims, fc2_dims),
+                nn.ReLU(),
+                nn.Linear(fc2_dims, 1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+
+        return self.critic(state)
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
 
 class Agent:
-    def __init__(self):
+    def __init__(self,input_dims=3,alpha=0.001):
         self.My = -6.7e-3
         self.tau_c = 0.24
         
-        self.Actor = None
-        self.Critic = None
+        self.actor = ActorNetwork(input_dims,alpha)
+        self.critic = CriticNetwork(input_dims, alpha)
+
+    def save_models(self):
+        print('... saving models ...')
+        self.actor.save_checkpoint()
+        self.critic.save_checkpoint()
+
+    def load_models(self):
+        print('... loading models ...')
+        self.actor.load_checkpoint()
+        self.critic.load_checkpoint()
+
+    def choose_action(self, observation):
+        state = T.tensor([observation], dtype=T.float).to(self.critic.device)
+
+        # ## SAMPLE ACTION FROM POLICY NETWORK
+        # dist = self.actor(state)
+        # action = dist.sample()
+
+        # log_prob = T.squeeze(dist.log_prob(action)).item()
+        # action = T.squeeze(action).item()
+
+        ## CALC STATE VALUE FROM CRITIC NETWORK
+        value = self.critic(state)
+        value = T.squeeze(value).item()
+
+        # return action, log_prob, value
+
+        action = self.actor(state)
+        action = T.squeeze(action).item()
+
+        print(action)
+
+        return self.tau_c,self.My
 
     def remember(self):
         pass
-
-    def get_action(self,tau,OFy,d_ceil):
-        
-        return self.tau_c,self.My
+       
 
 class CF_Env(): # Model class for Single Degree of Freedom Crazyflie
 
@@ -573,8 +663,10 @@ class CF_Env(): # Model class for Single Degree of Freedom Crazyflie
             tau = d_ceil/vz
             OFy = -vx/d_ceil
 
+            observation = [tau,OFy,d_ceil]
+
             ## PASS STATE TO POLICY NETWORK
-            self.tau_c,self.My = agent.get_action(tau,OFy,d_ceil)
+            self.tau_c,self.My = agent.choose_action(observation)
 
             return self.tau_c-tau    
         policy_output.terminal = True
@@ -961,13 +1053,18 @@ class CF_Env(): # Model class for Single Degree of Freedom Crazyflie
         ani = animation.FuncAnimation(fig,animate_func,fargs=(sol_t,model_states),
                                     init_func=init_lines,
                                     frames=len(sol_t),
-                                    interval=1000/100,
+                                    interval=10,
                                     blit=True,
                                     repeat=False)
+
+        plt.show(block=False)
+        plt.pause(10e-3*len(sol_t))
+        plt.close()
 
 
 if __name__ == '__main__':
 
+ 
     agent = Agent()
     env = CF_Env()
 
@@ -1012,7 +1109,8 @@ if __name__ == '__main__':
 
         if RENDER:
             env.animateTraj(sol_t,sol_y)
-            plt.show()
+            
+
 
         ## TRAIN AGENT
         # agent.remember(observation, action, prob, val, reward, done)
