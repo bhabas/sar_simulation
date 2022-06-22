89a537ec73bab5558597e7d0f11e6e56e8440cd8
bhabas <bhabas@psu.edu>
2022-06-22 10:41:52 -0400

    Working model for continuous tau trigger and action
    PPO works well
    SAC has better sample performance which is likely more important

--- crazyflie_projects/DeepRL/SB3_Load.py
+++ crazyflie_projects/DeepRL/SB3_Load.py
@@ -1,19 +1,20 @@
 from stable_baselines3 import PPO
-from Tau_Trigger_Env import Tau_Trigger_Env
 from Brake_Val_Env import Brake_Val_Env
 from Tau_Trigger_Cont_Env import Tau_Trigger_Cont_Env
+from Brake_Trigger_Env import Brake_Trigger_Env
+
 
 import gym
 
 # ## INITIATE ENVIRONMENT AND TRAINED MODEL5
 # env = gym.make("Pendulum-v1")
-env = Tau_Trigger_Cont_Env()
+env = Brake_Trigger_Env()
 env.reset()
 # env.env_name = "Brake_Val_Cont"
 
 ## SELECT MODEL FROM DIRECTORY
-models_dir = f"crazyflie_projects/DeepRL/models/{env.env_name}/PPO-08-58"
-model_path = f"{models_dir}/140.zip"
+models_dir = f"crazyflie_projects/DeepRL/models/{env.env_name}/PPO-10-12"
+model_path = f"{models_dir}/290.zip"
 
 
 model = PPO.load(model_path,env=env)
--- crazyflie_projects/DeepRL/SB3_Train.py
+++ crazyflie_projects/DeepRL/SB3_Train.py
@@ -1,25 +1,25 @@
 import os
 from datetime import datetime
-from stable_baselines3 import A2C,PPO
+from stable_baselines3 import A2C,PPO,SAC
 import gym
 
-from Tau_Trigger_Env import Tau_Trigger_Env
 from Brake_Val_Env import Brake_Val_Env
 from Tau_Trigger_Cont_Env import Tau_Trigger_Cont_Env
+from Brake_Trigger_Env import Brake_Trigger_Env
 
 ## COLLECT CURRENT TIME
 now = datetime.now()
 current_time = now.strftime("%H-%M")
 
 ## INITIATE ENVIRONMENT
-env = Tau_Trigger_Cont_Env()
+env = Brake_Trigger_Env()
 # env = Cont_Value_Pred_Env()
 # env = gym.make("Pendulum-v1")
 # env.env_name = "Pendulum"
 env.reset()
 
 ## CREATE MODEL AND LOG DIRECTORY
-models_dir = f"crazyflie_projects/DeepRL/models/{env.env_name}/PPO-{current_time}"
+models_dir = f"crazyflie_projects/DeepRL/models/{env.env_name}/SAC-{current_time}"
 log_dir = "/tmp/logs"
 
 if not os.path.exists(models_dir):
@@ -30,11 +30,11 @@ if not os.path.exists(log_dir):
 
 
 
-model = PPO(
+model = SAC(
     "MlpPolicy",
     env,
     gamma=0.99,
-    learning_rate=0.005,
+    learning_rate=0.001,
     use_sde=False,
     sde_sample_freq=4,
     verbose=1,
@@ -53,14 +53,14 @@ for i in range(1,60):
     model.learn(
         total_timesteps=TIMESTEPS, 
         reset_num_timesteps=reset_timesteps, 
-        tb_log_name=f"{env.env_name}-PPO-{current_time}"
+        tb_log_name=f"{env.env_name}-SAC-{current_time}"
     )
     model.save(f"{models_dir}/{TIMESTEPS*i//1000:d}.zip")
 
 # model.learn(
 #     total_timesteps=1000, 
 #     reset_num_timesteps=False, 
-#     tb_log_name=f"{env.env_name}-PPO-{current_time}"
+#     tb_log_name=f"{env.env_name}-SAC-{current_time}"
 # )
 
 ## RENDER TRAINED MODEL FOR N EPISODES
