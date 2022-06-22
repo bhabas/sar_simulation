6218b4627c137743f9b84b684dc564fe37c06292
bhabas <bhabas@psu.edu>
2022-06-22 16:49:09 -0400

    Cleaned up code and added step function

--- crazyflie_projects/DeepRL/CF_Env.py
+++ crazyflie_projects/DeepRL/CF_Env.py
@@ -31,9 +31,6 @@ class CF_Env():
         self.h_ceiling = 2.1
 
         # INTIIALIZE MODEL POSE
-        self.model_pose = self.get_pose(model_state=(0,0,0))
-
-
 
 
         self.state = None
@@ -57,7 +54,113 @@ class CF_Env():
         self.clock = None
         self.isopen = True
 
-    def get_pose(self,model_state=[0,0,0]):           
+    def step(self,action):
+
+        # err_msg = f"{action!r} ({type(action)}) invalid"
+        # assert self.action_space.contains(action), err_msg
+        # assert self.state is not None, "Call reset before using step method."
+        
+        x,x_dot,z,z_dot,theta,dtheta = self.state
+        # tau,d_ceil = self.obs
+
+        ## UPDATE STATE
+        self.t_step += 1
+
+        x_acc = 0
+        x = x + self.dt*x_dot
+        x_dot = x_dot + self.dt*x_acc
+
+        z_acc = 0
+        z = z + self.dt*z_dot
+        z_dot = z_dot + self.dt*z_acc
+        
+        theta_acc = 0
+        theta = theta + self.dt*dtheta
+        dtheta = dtheta + self.dt*theta_acc
+
+        self.state = (x,x_dot,z,z_dot,theta,dtheta)
+
+
+
+    def reset(self):
+        x, x_dot = 0.0, 0.2
+        z, z_dot = 0.2, 0.2
+        theta,dtheta = 0.0, -10
+        self.state = (x,x_dot,z,z_dot,theta,dtheta)
+
+    def render(self,mode=None):
+        
+
+        def c2p(Pos): ## CONVERTS COORDINATES TO PIXEL LOCATION      
+
+            x_offset = self.world_width/2
+            y_offset = 0.3
+
+            scale_x = self.screen_width/self.world_width
+            scale_y = self.screen_height/self.world_height
+            
+            x_p = (x_offset+Pos[0])*scale_x
+            y_p = (y_offset+Pos[1])*scale_y
+            
+            return (x_p,y_p)
+
+        ## DEFINE COLORS
+        WHITE = (255,255,255)
+        BLACK = (0,0,0)
+        BLUE = (29,123,243)
+        RED = (255,0,0)
+
+        ## INITIATE SCREEN AND CLOCK ON FIRST LOADING
+        if self.screen is None:
+            pygame.init()
+            self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))
+        
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+        if self.state is None:
+            return None
+
+        ## CREATE BACKGROUND SURFACE
+        self.surf = pygame.Surface((self.screen_width, self.screen_height))
+        self.surf.fill(WHITE)
+
+        x,x_dot,z,z_dot,theta,dtheta = self.state
+
+        Pose = self.get_pose(x,z,theta)
+
+        ## CREATE QUADROTOR
+        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[1]),width=3)
+        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[2]),width=3)
+        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[3]),width=3)
+        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[4]),width=3)
+        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[5]),width=3)
+        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[6]),width=3)
+
+        pygame.draw.circle(self.surf,RED,c2p(Pose[0]),5)
+
+
+        ## DRAW FLOOR LINE
+        pygame.draw.line(self.surf,BLACK,c2p((-5,0)),c2p((5,0)),width=2)
+
+        ## DRAW CEILING LINE
+        pygame.draw.line(self.surf,BLACK,c2p((-5,self.h_ceiling)),c2p((5,self.h_ceiling)),width=2)
+
+        
+
+        
+        
+
+        ## FLIP IMAGE SO X->RIGHT AND Y->UP
+        self.surf = pygame.transform.flip(self.surf, False, True)
+        self.screen.blit(self.surf,     (0, 0))
+
+
+
+        self.clock.tick(60)
+        pygame.display.flip()
+
+    def get_pose(self,x_pos,z_pos,theta):           
         """Returns position data of all model lines for a given state
 
         Args:
@@ -68,7 +171,6 @@ class CF_Env():
         """        
 
         (L,e,gamma,M_G,M_L,G,PD,I_G) = self.params
-        x_pos,z_pos,theta = model_state
 
 
         ## DEFINE LOCATION OF MODEL ORIGIN
@@ -102,119 +204,6 @@ class CF_Env():
 
         return (CG,P1,P2,L1,L2,Prop1,Prop2)
 
-    def reset(self):
-
-        self.state = (0,0,0)
-
-    
-
-    def render(self,mode=None):
-        
-
-        def c2p(Pos): ## CONVERTS COORDINATES TO PIXEL LOCATION      
-
-            x_offset = self.world_width/2
-            y_offset = 0.3
-
-            scale_x = self.screen_width/self.world_width
-            scale_y = self.screen_height/self.world_height
-            
-            x_p = (x_offset+Pos[0])*scale_x
-            y_p = (y_offset+Pos[1])*scale_y
-            
-            return (x_p,y_p)
-
-        ## DEFINE COLORS
-        WHITE = (255,255,255)
-        BLACK = (0,0,0)
-        BLUE = (29,123,243)
-        RED = (255,0,0)
-
-        ## INITIATE SCREEN AND CLOCK ON FIRST LOADING
-        if self.screen is None:
-            pygame.init()
-            self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))
-        
-        if self.clock is None:
-            self.clock = pygame.time.Clock()
-
-        # if self.state is None:
-        #     return None
-        # x = self.state
-
-        ## CREATE BACKGROUND SURFACE
-        self.surf = pygame.Surface((self.screen_width, self.screen_height))
-        self.surf.fill(WHITE)
-
-        self.state = [0,0.1,0]
-
-        Pose = self.get_pose(self.state)
-        # pass
-
-
-        # ## CREATE CART
-        # cartwidth = 50.0
-        # cartheight = 30.0
-
-        # l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
-        # cartx = x[0] * scale + self.screen_width / 2.0  # MIDDLE OF CART
-        # carty = 100  # TOP OF CART
-
-        # gfxdraw.hline(self.surf, 0, self.screen_width,0,BLACK)
-        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[1]),width=5)
-        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[2]),width=5)
-        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[3]),width=5)
-        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[4]),width=5)
-        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[5]),width=5)
-        pygame.draw.line(self.surf,BLACK,c2p(Pose[0]),c2p(Pose[6]),width=5)
-
-        pygame.draw.circle(self.surf,RED,c2p(Pose[0]),5)
-
-
-        ## DRAW FLOOR LINE
-        pygame.draw.line(self.surf,BLACK,c2p((-5,0)),c2p((5,0)),width=2)
-
-        ## DRAW CEILING LINE
-        pygame.draw.line(self.surf,BLACK,c2p((-5,self.h_ceiling)),c2p((5,self.h_ceiling)),width=2)
-
-        
-
-
-
-        # gfxdraw.vline(self.surf, int(self.x_d*scale + self.screen_width / 2.0), 0, self.screen_height,black)
-
-        # pygame.draw.line(self.surf,black,(0,0),(100,100),width=3)
-
-        # ## CREATE CART COORDS AND TRANSLATE TO PROPER LOCATION
-        # cart_coords = [(l, b), (l, t), (r, t), (r, b)]
-        # cart_coords = [(c[0] + cartx, c[1] + carty) for c in cart_coords]
-        # gfxdraw.filled_polygon(self.surf, cart_coords, black)
-
-
-
-        # ## CREATE QUADROTOR
-        # pygame.draw.line(self.surf,BLACK,(Pose[0]* scale + self.screen_width / 2.0),(Pose[3]* scale + self.screen_width / 2.0))
-
-        # if self.Once_flag == True:
-        #     gfxdraw.aacircle(self.surf,int(cartx),int(carty),5,red)
-        #     gfxdraw.filled_circle(self.surf,int(cartx),int(carty),5,red)
-        # else:
-        #     gfxdraw.aacircle(self.surf,int(cartx),int(carty),5,blue)
-        #     gfxdraw.filled_circle(self.surf,int(cartx),int(carty),5,blue)
-
-
-        
-        
-
-        ## FLIP IMAGE SO X->RIGHT AND Y->UP
-        self.surf = pygame.transform.flip(self.surf, False, True)
-        self.screen.blit(self.surf,     (0, 0))
-
-
-
-        self.clock.tick(45)
-        pygame.display.flip()
-
         
     def close(self):
         if self.screen is not None:
@@ -233,8 +222,7 @@ if __name__ == '__main__':
         env.reset()
         while not done:
             env.render()
-            pass
-            # obs,reward,done,info = env.step(env.action_space.sample())
+            env.step(0.0)
 
 
     env.close()
