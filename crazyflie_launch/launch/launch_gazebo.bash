8d9aac0b220c77cd924c3e5d8eafd2fba7e083fe
bhabas <bhabas@psu.edu>
2022-10-12 15:26:02 -0400

    Updated model spawning to work from new params

--- crazyflie_launch/launch/launch_gazebo.bash
+++ crazyflie_launch/launch/launch_gazebo.bash
@@ -4,7 +4,7 @@ source ~/catkin_ws/devel/setup.bash
 
 ## UPLOAD ROS PARAMS
 roslaunch crazyflie_launch params.launch
-MODEL_NAME=$(rosparam get /MODEL_NAME)
+CONFIG_NAME=$(rosparam get /QUAD_SETTINGS/Config)
 SURFACE_NAME=$(rosparam get /SURFACE_NAME)
 GROUND_NAME=$(rosparam get /GROUND_NAME)
 GUI_FLAG=$(rosparam get /GUI_FLAG)
@@ -13,7 +13,7 @@ PAUSE_FLAG=$(rosparam get /PAUSE_FLAG)
 ## START GAZEBO 
 roslaunch crazyflie_launch crazyflie_gazebo.launch \
     surface_name:=$SURFACE_NAME \
-    model_name:=$MODEL_NAME \
+    config_name:=$CONFIG_NAME \
     ground_name:=$GROUND_NAME \
     gui_flag:=$GUI_FLAG \
     pause_flag:=$PAUSE_FLAG \
