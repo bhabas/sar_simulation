Updated data compilation script and fix polar plot interpolation by using normalization

crazyflie_projects/SVL_Policy/Data_Analysis/ParamOptim_Value_Compiler.py
@@ -7,8 +7,8 @@ import numpy as np
 import pandas as pd
 import os
 import time
-import rospy
 import re
+import csv
 
 ## ADD CRAZYFLIE_SIMULATION DIRECTORY TO PYTHONPATH SO ABSOLUTE IMPORTS CAN BE USED
 import sys,rospkg
@@ -19,9 +19,10 @@ from crazyflie_logging.data_analysis.Data_Analysis import DataFile
 
 dataPath = f"{BASE_PATH}/crazyflie_logging/local_logs"
 compiledPath = f"{BASE_PATH}/crazyflie_projects/SVL_Policy/Data_Logs"
-compiledName = "NL_SVL_Policy_Trials.csv"
+compiledName = "NL_LR_Trials3.csv"
 
-df_list = []
+filePath = os.path.join(compiledPath,compiledName)
+
 file_index = 0
 num_files = len(os.listdir(dataPath))
 
@@ -31,10 +32,30 @@ start_time = time.time()
 end_time = time.time()
 time_delta = 20 # Estimated analysis time per file [s]
 alpha = 0.15 # Filter parameter to smooth out time estimate
-import fileinput
+
+
+## WRITE FILE HEADER
+with open(filePath,'w') as file:
+    writer = csv.writer(file,delimiter=',')
+    writer.writerow([
+        "Vel_d", "Phi_d", "Trial_num",
+        "LR_4Leg",
+
+        "Vel_flip","Phi_flip",
+
+        "Tau_flip_mean",
+        "OFy_flip_mean",
+        "D_ceil_flip_mean",
+        "My_mean",
+
+        "Vz_flip_mean",
+        "Vx_flip_mean",
+
+        "Mu","Sigma",
+    ])
 
 ## ITER OVER ALL FILES IN DIR
-for ii,fileName in enumerate(os.listdir(dataPath)): # Iter over all files in dir
+for ii,fileName in enumerate(sorted(os.listdir(dataPath))): # Iter over all files in dir
 
 
     ## ESTIMATE ANALYSIS TIME PER FILE
@@ -42,26 +63,21 @@ for ii,fileName in enumerate(os.listdir(dataPath)): # Iter over all files in dir
     time_delta = alpha*diff + (1-alpha)*(time_delta)
     start_time = time.time()
 
-    
-
     try: ## TRY OPENING FILE
 
         ## RECORD DATA FROM LOG FILE    
         print(f"Current File: {fileName} \t Index: {ii}/{num_files-1} \t Percentage: {100*ii/num_files:.2f}% \t Minutes to Completion: {time_delta/60*(num_files-ii):.1f}")
 
-        # with fileinput.FileInput(dataPath+fileName, inplace=True, backup='.bak') as file:
-        #     for line in file:
-        #         print(line.replace("--,--,Rollout Completed", "Rollout Completed"), end='')
         trial = DataFile(dataPath,fileName,dataType='Sim')
         trial.n_rollouts = 6
         
         ## TRIAL CONDITIONS
-        vel_IC,phi_IC = trial.grab_vel_IC_2D_angle()
-        trial_num = int(re.split("trial_",fileName)[1][:2])
+        Vel_IC,Phi_IC = trial.grab_vel_IC_2D_angle()
+        Trial_num = int(re.split("trial_",fileName)[1][:2])
 
 
-        vz_flip_mean,vz_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'vz')
-        vx_flip_mean,vx_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'vx')
+        Vz_flip_mean,vz_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'vz')
+        Vx_flip_mean,vx_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'vx')
         Tau_flip_mean,Tau_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'Tau')
         OFy_flip_mean,OFy_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'OF_y')
         D_ceil_flip_mean,D_ceil_flip_std,_ = trial.grab_trial_data(trial.grab_flip_state,'d_ceil')
@@ -69,30 +85,46 @@ for ii,fileName in enumerate(os.listdir(dataPath)): # Iter over all files in dir
         landing_rate_4leg,_,_,_ = trial.landing_rate()
         mu,sigma = trial.grab_finalPolicy()
 
+        Vel_flip = np.sqrt(Vx_flip_mean**2 + Vz_flip_mean**2)
+        Phi_flip = np.rad2deg(np.arctan2(Vz_flip_mean,Vx_flip_mean))
 
 
+        ## WRITE FILE HEADER
+        with open(filePath,'a') as file:
+            writer = csv.writer(file,delimiter=',')
+            writer.writerow([
+                Vel_IC, Phi_IC, Trial_num,
+                landing_rate_4leg,
 
-        df_list.append((
-            vel_IC, phi_IC, trial_num,
-            landing_rate_4leg,
+                np.round(Vel_flip,3),np.round(Phi_flip,3),
 
-            Tau_flip_mean,
-            OFy_flip_mean,
-            D_ceil_flip_mean,
-            My_mean,
+                Tau_flip_mean,
+                OFy_flip_mean,
+                D_ceil_flip_mean,
+                My_mean,
 
-            vz_flip_mean,
-            vx_flip_mean,
+                Vz_flip_mean,
+                Vx_flip_mean,
 
-            Tau_flip_std,
-            OFy_flip_std,
-            D_ceil_flip_std,
-            My_std,
+                mu,sigma,
+            ])
 
-            vz_flip_std,
-            vx_flip_std,
-            mu,sigma,
-        ))
+        # df_list.append((
+        #     Vel_IC, Phi_IC, Trial_num,
+        #     landing_rate_4leg,
+
+        #     Vel_flip,Phi_flip,
+
+        #     Tau_flip_mean,
+        #     OFy_flip_mean,
+        #     D_ceil_flip_mean,
+        #     My_mean,
+
+        #     Vz_flip_mean,
+        #     Vx_flip_mean,
+
+        #     mu,sigma,
+        # ))
 
 
 
@@ -103,32 +135,12 @@ for ii,fileName in enumerate(os.listdir(dataPath)): # Iter over all files in dir
 
     end_time = time.time()
     
-## WRITE FILE HEADER
-master_df = pd.DataFrame(df_list,columns=(
-    "vel_d", "phi_d", "trial_num",
-    "LR_4Leg",
 
-    "Tau_flip_mean",
-    "OFy_flip_mean",
-    "D_ceil_flip_mean",
-    "My_mean",
 
-    "vz_flip_mean",
-    "vx_flip_mean",
-
-    "Tau_flip_std",
-    "OFy_flip_std",
-    "D_ceil_flip_std",
-    "My_std",
-
-    "vz_flip_std",
-    "vx_flip_std",
-    "mu","sigma",
-))
-master_df = master_df.round(4)
-master_df[['vel_d','phi_d','trial_num']] = master_df[['vel_d','phi_d','trial_num']].round(2)
-master_df.sort_values(['vel_d','phi_d','trial_num'],ascending=[1,1,1],inplace=True)
-master_df.to_csv(f'{compiledPath}/{compiledName}',index=False,mode='w',header=True)
+# # master_df = master_df.round(4)
+# # master_df[['Vel_d','Phi_d','Trial_num']] = master_df[['Vel_d','Phi_d','Trial_num']].round(2)
+# # master_df.sort_values(['Vel_d','Phi_d','Trial_num'],ascending=[1,1,1],inplace=True)
+# # master_df.to_csv(f'{compiledPath}/{compiledName}',index=False,mode='w',header=True)
 
 
 

crazyflie_projects/SVL_Policy/Policy_Training/Policy_Training_SVL.py
@@ -585,48 +585,70 @@ class Policy_Trainer():
         )
         fig.show()
 
-    def plot_polar_smoothed(self,df,saveFig=False):
+    def plot_Landing_Rate(self,df,saveFig=False):
 
         ## COLLECT DATA
-        R = df.iloc[:]['vel_d']
-        Theta = df.iloc[:]['phi_d']
-        C = df.iloc[:]['LR_4Leg']
+        Vx = df.iloc[:]['Vx_flip_mean'].to_numpy()
+        Vz = df.iloc[:]['Vz_flip_mean'].to_numpy()
+        LR = df.iloc[:]['LR_4Leg'].to_numpy()
 
-        ## DEFINE INTERPOLATION GRID
-        R_list = np.linspace(R.min(),R.max(),num=25,endpoint=True).reshape(1,-1)
-        Theta_list = np.linspace(Theta.min(),Theta.max(),num=25,endpoint=True).reshape(1,-1)
-        R_grid, Theta_grid = np.meshgrid(R_list, Theta_list)
-        
-        ## INTERPOLATE DATA
-        LR_interp = griddata((R, Theta), C, (R_list, Theta_list.T), method='linear',fill_value=1e-6)
+        Vel_d = df.iloc[:]['Vel_d'].to_numpy()
+        Phi_d = df.iloc[:]['Phi_d'].to_numpy()
+
+        Vel_flip = df.iloc[:]['Vel_flip'].to_numpy()
+        Phi_flip = df.iloc[:]['Phi_flip'].to_numpy()
+
+
+        ## GENERATE INTERPOLATION GRID
+        Phi_list = np.linspace(20,90,50)
+        Vel_list = np.linspace(1.0,3.5,50)        
+        Phi_grid,Vel_grid = np.meshgrid(Phi_list,Vel_list)
+
+
+        ## NORMALIZE DATA BEFORE INTERPOLATION (ALWAYS NORMALIZE!!!)
+        def normalize(data,xmin,xmax):
+            data = data.astype(np.float64)
+            return (data-xmin)/(xmax-xmin)
+
+        Phi_min,Phi_max = Phi_flip.min(),Phi_flip.max()
+        Vel_min,Vel_max = Vel_flip.min(),Vel_flip.max()
+
+        Phi_norm = normalize(Phi_flip,Phi_min,Phi_max)
+        Vel_norm = normalize(Vel_flip,Vel_min,Vel_max)
+
+        Phi_grid_norm = normalize(Phi_grid,Phi_min,Phi_max)
+        Vel_grid_norm = normalize(Vel_grid,Vel_min,Vel_max)
         
 
-        ## INIT PLOT INFO
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='polar')
+        ## INTERPOLATE VALUES
+        interp_grid = (Phi_grid_norm, Vel_grid_norm)    # Interpolation grid
+        points = np.array((Phi_norm,Vel_norm)).T        # Known data points
+        values = LR                                     # Known data values
+        LR_interp = griddata(points, values, interp_grid, method='linear')
+
+
+        ## PLOT DATA
+        fig = plt.figure(figsize=(4,4))
+
         cmap = mpl.cm.jet
         norm = mpl.colors.Normalize(vmin=0,vmax=1)
-        
-        ax.contourf(np.radians(Theta_grid),R_grid,LR_interp,cmap=cmap,norm=norm,levels=30)
-        # ax.scatter(np.radians(Theta),R,c=C,cmap=cmap,norm=norm)
-        # ax.scatter(np.radians(Theta_grid).flatten(),R_grid.flatten(),c=LR_interp.flatten(),cmap=cmap,norm=norm)
 
+        ax = fig.add_subplot(projection='polar')
+        ax.contourf(np.radians(Phi_grid),Vel_grid,LR_interp,levels=30,cmap=cmap,norm=norm)
+        # ax.scatter(np.radians(Phi_grid.flatten()),Vel_grid.flatten(),c=grid_z0.flatten(),cmap=cmap,norm=norm)
+        # ax.scatter(np.radians(Phi_flip),Vel_flip,c=C,cmap=cmap,norm=norm)
         ax.set_thetamin(30)
         ax.set_thetamax(90)
         ax.set_rmin(0)
         ax.set_rmax(3.5)
-        
 
+        ax.set_xticks(np.radians([30,45,60,75,90]))
+        ax.set_yticks([0,1.0,2.0,3.0,3.5])
 
-        ## AXIS LABELS    
-        ax.text(np.radians(7.5),2,'Flight Velocity (m/s)',
-            rotation=18,ha='center',va='center')
-
-        ax.text(np.radians(60),4.5,'Flight Angle (deg)',
-            rotation=0,ha='left',va='center')
-
-        if saveFig==True:
-            plt.savefig(f'{"NL"}_Polar_LR.pdf',dpi=300)
+        plt.show()
+ 
+        # if saveFig==True:
+        #     plt.savefig(f'{self.model_initials}_Polar_LR.pdf',dpi=300)
 
         plt.show()
 
@@ -657,13 +679,17 @@ if __name__ == "__main__":
     torch.manual_seed(0)
     np.random.seed(0)
 
+
+
     ## DESIGNATE FILE PATHS
-    model_initials = "NL_DR"
+    FileName = "NL_LR_Trials3.csv"
+    # FileName = "NL_SVL_Policy_Trials.csv"
+    model_initials = FileName[:2]
     NN_Param_Path = f'{BASEPATH}/crazyflie_projects/SVL_Policy/Policy_Training/Info/NN_Layers_{model_initials}.h'
     SVM_Param_Path = f'{BASEPATH}/crazyflie_projects/SVL_Policy/Policy_Training/Info/SVM_Params_{model_initials}.h'
 
     FilePath = f"{BASEPATH}/crazyflie_projects/SVL_Policy/Data_Logs/"
-    FileName = "NL_LR_Trials.csv"
+    
 
     ## PRE-INITIALIZE MODELS
     NN_model = NN_Model()
@@ -678,7 +704,7 @@ if __name__ == "__main__":
     df_train = df_raw.query("LR_4Leg >= 0.8")
 
     ## MAX LANDING RATE DATAFRAME
-    idx = df_raw.groupby(['vel_d','phi_d'])['LR_4Leg'].transform(max) == df_raw['LR_4Leg']
+    idx = df_raw.groupby(['Vel_d','Phi_d'])['LR_4Leg'].transform(max) == df_raw['LR_4Leg']
     df_max = df_raw[idx].reset_index()
 
     ## ORGANIZE DATA
@@ -720,7 +746,7 @@ if __name__ == "__main__":
     # Policy.plotPolicyRegion(df_train,PlotBoundry=True,iso_level=0.0)
     # Policy.plotPolicyRegion(df_raw,PlotBoundry=True,iso_level=0.0)
     # Policy.plotPolicyRegion(df_raw,PlotBoundry=False,iso_level=0.0)
-    Policy.plot_polar_smoothed(df_max)
+    Policy.plot_Landing_Rate(df_max,saveFig=True)
     
     EXP_PATH = os.path.dirname(rospkg.RosPack().get_path('crazyflie_logging_exp'))
 