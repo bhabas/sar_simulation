2e4bba1c4058abf8c7a3a2e4d80c53a706f81e02
bhabas <bhabas@psu.edu>
2023-01-23 13:27:35 -0500

    Working optical flow calculation output but not verified

--- crazyflie_projects/Optical_Flow_Estimation/Camera_Parser.py
+++ crazyflie_projects/Optical_Flow_Estimation/Camera_Parser.py
@@ -67,20 +67,111 @@ class DataParser:
         for n in range(0,self.Camera_data.size): # Make each row an array of ints
             self.Camera_array[n] = np.fromstring(self.Camera_data[n], dtype=int, sep =' ')
 
-    def Plot_Image(self,n=0):
+    def grabImage(self,idx):
+
+        return self.Camera_array[idx].reshape(WIDTH_PIXELS,HEIGHT_PIXELS)
+
+    def grabState(self,state_str,idx):
+
+        return self.Data_df[state_str][idx]
+
+
+    def Plot_Image(self,image_array):
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
-        ax.imshow(self.Camera_array[n].reshape(WIDTH_PIXELS,HEIGHT_PIXELS), interpolation='none', 
+        ax.imshow(image_array.reshape(WIDTH_PIXELS,HEIGHT_PIXELS), interpolation='none', 
                 vmin=0, vmax=255, cmap=cm.gray,
                 origin='upper',)
 
         plt.show()
+
+    def OpticalFlow_Calc(self,cur_img,prev_img,delta_t):
+
+
+        ## DEFINE KERNALS USED TO CALCULATE INTENSITY GRADIENTS
+        Ku = np.array([ # SOBEL KERNAL (U-DIRECTION)
+            [-1,-2,-1],
+            [ 0, 0, 0],
+            [ 1, 2, 1]
+        ]) 
+
+        Kv = np.array([ # SOBEL KERNAL (V--DIRECTION)
+            [ -1, 0, 1],
+            [ -2, 0, 2],
+            [ -1, 0, 1]
+        ])
+
+        
+        ## PRE-ALLOCATE IMAGE ARRAY [pixels]
+        v_p = np.arange(0,WIDTH_PIXELS,1)
+        u_p = np.arange(0,HEIGHT_PIXELS,1)
+        V_p,U_p = np.meshgrid(v_p,u_p)
+
+        ## DEFINE IMAGE SENSOR COORDS IN [m]
+        V_grid = -((V_p - O_vp)*w + w/2)
+        U_grid =  ((U_p - O_up)*w + w/2)
+
+        ## PRE-ALLOCATE INTENSITY GRADIENTS
+        Iu = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+        Iv = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+
+
+        ## CALCULATE IMAGE GRADIENTS
+        for ii in range(1, HEIGHT_PIXELS-1): 
+            for jj in range(1, WIDTH_PIXELS-1):
+                Iu[ii,jj] = 1/(8*w)*np.sum(cur_img[ii-1:ii+2,jj-1:jj+2] * Ku)
+                Iv[ii,jj] = -1/(8*w)*np.sum(cur_img[ii-1:ii+2,jj-1:jj+2] * Kv)
+
+        ## CALCULATE TIME GRADIENT AND RADIAL GRADIENT
+        It = (cur_img - prev_img)/delta_t   # Time gradient
+        Ir = U_grid*Iu + V_grid*Iv          # Radial Gradient
+
+
+        ## SOLVE LEAST SQUARES PROBLEM
+        X = np.array([
+            [f*np.sum(Iu*Iu), f*np.sum(Iv*Iu), -np.sum(Ir*Iu)],
+            [f*np.sum(Iu*Iv), f*np.sum(Iv*Iv), -np.sum(Ir*Iv)],
+            [f*np.sum(Iu*Ir), f*np.sum(Iv*Ir), -np.sum(Ir*Ir)]
+        ])
+
+        y = np.array([
+            [np.sum(It*Iu)],
+            [np.sum(It*Iv)],
+            [np.sum(It*Ir )]
+        ])
+
+        ## SOLVE b VIA PSEUDO-INVERSE
+        b = np.linalg.pinv(X)@y
+        b = b.flatten()
+
+
+        #     self.OFy_est[n] = b[0]
+        #     self.OFx_est[n] = b[1]
+        #     self.Tau_est[n] = 1/(b[2])
+
+        #     Prev_img = Cur_img
+
+        return b
 
 
 
 if __name__ == '__main__':
 
     Parser = DataParser() #init class
-    Parser.Plot_Image()
+
+    idx = 1
+    prev_img = Parser.grabImage(idx-1)
+    cur_img = Parser.grabImage(idx)
+
+    t_prev = Parser.grabState('t',idx-1)
+    t_cur = Parser.grabState('t',idx)
+
+    delta_t = t_cur-t_prev
+
+
+    # Parser.Plot_Image(img)
+    # Parser.Plot_Image()
+    b = Parser.OpticalFlow_Calc(cur_img,prev_img,delta_t)
+    print(b[2]**-1)
