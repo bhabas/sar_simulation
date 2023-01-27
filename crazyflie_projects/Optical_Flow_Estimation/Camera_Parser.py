84e4f18ca79ec661db2b37d593c694aa4ea9d9e6
bhabas <bhabas@psu.edu>
2023-01-27 18:56:43 -0500

    Optimizing optical flow calc to be ints

--- crazyflie_projects/Optical_Flow_Estimation/Camera_Parser.py
+++ crazyflie_projects/Optical_Flow_Estimation/Camera_Parser.py
@@ -8,8 +8,8 @@ import cv2 as cv
 
 
 ## CAMERA PARAMETERS
-WIDTH_PIXELS = 160
-HEIGHT_PIXELS = 160
+WIDTH_PIXELS = 4
+HEIGHT_PIXELS = 4
 FPS = 60               # Frame Rate [1/s]
 w = 3.6e-6              # Pixel width [m]
 f = 0.66e-3/2          # Focal Length [m]
@@ -72,10 +72,10 @@ class DataParser:
             self.Theta_y_est = self.Data_df['Theta_y_est'].to_numpy()
 
 
-        ## CONVERT IMAGE DATA FROM STRING TO INTEGER
-        self.Camera_array = np.zeros((len(self.Camera_data), WIDTH_PIXELS*HEIGHT_PIXELS)) # Allocate space for camera data
-        for n in range(0,self.Camera_data.size): # Make each row an array of ints
-            self.Camera_array[n] = np.fromstring(self.Camera_data[n], dtype=int, sep =' ')
+        # ## CONVERT IMAGE DATA FROM STRING TO INTEGER
+        # self.Camera_array = np.zeros((len(self.Camera_data), WIDTH_PIXELS*HEIGHT_PIXELS)) # Allocate space for camera data
+        # for n in range(0,self.Camera_data.size): # Make each row an array of ints
+        #     self.Camera_array[n] = np.fromstring(self.Camera_data[n], dtype=int, sep =' ')
 
     def grabImage(self,idx):
 
@@ -101,66 +101,6 @@ class DataParser:
         plt.show()
 
     def OF_Calc_Raw(self,cur_img,prev_img,delta_t):
-
-        ## DEFINE KERNALS USED TO CALCULATE INTENSITY GRADIENTS
-        Ku = np.array([ # SOBEL KERNAL (U-DIRECTION)
-            [-1,-2,-1],
-            [ 0, 0, 0],
-            [ 1, 2, 1]
-        ]) 
-
-        Kv = np.array([ # SOBEL KERNAL (V--DIRECTION)
-            [ -1, 0, 1],
-            [ -2, 0, 2],
-            [ -1, 0, 1]
-        ])
-
-        
-        ## PRE-ALLOCATE IMAGE ARRAY [pixels]
-        v_p = np.arange(0,WIDTH_PIXELS,1)
-        u_p = np.arange(0,HEIGHT_PIXELS,1)
-        V_p,U_p = np.meshgrid(v_p,u_p)
-
-        ## DEFINE IMAGE SENSOR COORDS [m]
-        V_grid = -((V_p - O_up)*w + w/2)
-        U_grid =  ((U_p - O_vp)*w + w/2)
-
-        ## PRE-ALLOCATE INTENSITY GRADIENTS
-        Iu = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
-        Iv = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
-
-
-        ## CALCULATE IMAGE GRADIENTS
-        for ii in range(1, HEIGHT_PIXELS-1): 
-            for jj in range(1, WIDTH_PIXELS-1):
-                Iu[ii,jj] = 1/(8*w)*np.sum(cur_img[ii-1:ii+2,jj-1:jj+2] * Ku)
-                Iv[ii,jj] = -1/(8*w)*np.sum(cur_img[ii-1:ii+2,jj-1:jj+2] * Kv)
-
-        ## CALCULATE TIME GRADIENT AND RADIAL GRADIENT
-        It = (cur_img - prev_img)/delta_t   # Time gradient
-        Ir = U_grid*Iu + V_grid*Iv          # Radial Gradient
-
-
-        ## SOLVE LEAST SQUARES PROBLEM
-        X = np.array([
-            [f*np.sum(Iu*Iu), f*np.sum(Iv*Iu), -np.sum(Ir*Iu)],
-            [f*np.sum(Iu*Iv), f*np.sum(Iv*Iv), -np.sum(Ir*Iv)],
-            [f*np.sum(Iu*Ir), f*np.sum(Iv*Ir), -np.sum(Ir*Ir)]
-        ])
-
-        y = np.array([
-            [np.sum(It*Iu)],
-            [np.sum(It*Iv)],
-            [np.sum(It*Ir )]
-        ])
-
-        ## SOLVE b VIA PSEUDO-INVERSE
-        b = np.linalg.pinv(X)@y
-        b = b.flatten()
-
-        return b
-
-    def OF_Calc_Raw2(self,cur_img,prev_img,delta_t):
 
         ## DEFINE KERNALS USED TO CALCULATE INTENSITY GRADIENTS
         Kv = np.array([ # SOBEL KERNAL (U-DIRECTION)
@@ -220,6 +160,75 @@ class DataParser:
 
         return b
 
+    def OF_Calc_Opt(self,cur_img,prev_img,delta_t):
+
+        ## DEFINE KERNALS USED TO CALCULATE INTENSITY GRADIENTS
+        Kv = np.array([ # SOBEL KERNAL (U-DIRECTION)
+            [-1,-2,-1],
+            [ 0, 0, 0],
+            [ 1, 2, 1]
+        ]) 
+
+        Ku = np.array([ # SOBEL KERNAL (V--DIRECTION)
+            [ -1, 0, 1],
+            [ -2, 0, 2],
+            [ -1, 0, 1]
+        ])
+
+        
+        ## PRE-ALLOCATE IMAGE ARRAY [pixels]
+        u_p = np.arange(0,WIDTH_PIXELS,1)
+        v_p = np.arange(0,HEIGHT_PIXELS,1)
+        U_p,V_p = np.meshgrid(u_p,v_p)
+
+        ## PRE-ALLOCATE INTENSITY GRADIENTS
+        G_up = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+        G_vp = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+        G_rp = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+
+
+
+        Iv = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+        Iu = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+        Ir = np.zeros((HEIGHT_PIXELS,WIDTH_PIXELS))
+
+
+
+        ## CALCULATE IMAGE GRADIENTS
+        for v_p in range(1, HEIGHT_PIXELS-1): 
+            for u_p in range(1, WIDTH_PIXELS-1):
+                G_up[v_p,u_p] = np.sum(cur_img[v_p-1:v_p+2,u_p-1:u_p+2] * Ku)
+                G_vp[v_p,u_p] = np.sum(cur_img[v_p-1:v_p+2,u_p-1:u_p+2] * Kv)
+                G_rp[v_p,u_p] = (u_p - O_up + 1/2)*G_up[v_p,u_p] + (v_p - O_vp + 1/2)*G_vp[v_p,u_p]
+
+
+                Iu[v_p,u_p] = -1/(8*w)*G_up[v_p,u_p]
+                Iv[v_p,u_p] =  1/(8*w)*G_vp[v_p,u_p]
+                Ir[v_p,u_p] =  1/(8*w)*G_rp[v_p,u_p]*w
+
+        ## CALCULATE TIME GRADIENT AND RADIAL GRADIENT
+        It = (cur_img - prev_img)/delta_t   # Time gradient
+
+
+        ## SOLVE LEAST SQUARES PROBLEM
+        X = np.array([
+            [f*np.sum(Iv*Iv), f*np.sum(Iu*Iv), -np.sum(Ir*Iv)],
+            [f*np.sum(Iv*Iu), f*np.sum(Iu*Iu), -np.sum(Ir*Iu)],
+            [f*np.sum(Iv*Ir), f*np.sum(Iu*Ir), -np.sum(Ir*Ir)]
+        ])
+
+        y = np.array([
+            [np.sum(It*Iv)],
+            [np.sum(It*Iu)],
+            [np.sum(It*Ir )]
+        ])
+
+        ## SOLVE b VIA PSEUDO-INVERSE
+        b = np.linalg.pinv(X)@y
+        b = b.flatten()
+
+        return b
+
     def OpticalFlow_Writer(self):
 
         Theta_x_arr = [np.nan]
@@ -238,7 +247,7 @@ class DataParser:
             t_delta = t_cur - t_prev
 
             ## CALCULATE OPTICAL FLOW VALUES
-            Theta_x_est,Theta_y_est,Theta_z_est = self.OF_Calc_Raw2(cur_img,prev_img,t_delta)
+            Theta_x_est,Theta_y_est,Theta_z_est = self.OF_Calc_Opt(cur_img,prev_img,t_delta)
             print(Theta_z_est**-1)
 
             Theta_x_est,Theta_y_est,Tau_est = np.round([Theta_x_est,Theta_y_est,1/Theta_z_est],3)
@@ -268,29 +277,29 @@ class DataParser:
 if __name__ == '__main__':
 
     Parser = DataParser() 
-    Parser.OpticalFlow_Writer()
+    # Parser.OpticalFlow_Writer()
 
     t_prev = Parser.grabState('t',0)
     t_cur = Parser.grabState('t',1)
     t_delta = t_cur - t_prev
 
-    img_prev = Parser.grabImage(0)
-    img_cur = Parser.grabImage(1)
+    # img_prev = Parser.grabImage(0)
+    # img_cur = Parser.grabImage(1)
 
-    # img_cur = np.array([
-    #     [1,1,0,0],
-    #     [1,1,0,0],
-    #     [1,1,0,0],
-    #     [1,1,0,0]
-    #     ])
+    img_cur = np.array([
+        [1,1,0,0],
+        [1,1,0,0],
+        [1,1,0,0],
+        [1,1,0,0]
+        ])
 
-    # img_prev = np.array([
-    #     [0,0,1,1],
-    #     [0,0,1,1],
-    #     [0,0,1,1],
-    #     [0,0,1,1]
-    #     ])
+    img_prev = np.array([
+        [0,0,1,1],
+        [0,0,1,1],
+        [0,0,1,1],
+        [0,0,1,1]
+        ])
 
 
-    # print(Parser.OF_Calc_Raw(img_cur,img_prev,t_delta))
-    # print(Parser.OF_Calc_Raw2(img_cur,img_prev,t_delta))
+    print(Parser.OF_Calc_Raw(img_cur,img_prev,t_delta))
+    print(Parser.OF_Calc_Opt(img_cur,img_prev,t_delta))
