4e0daf21458207e7e7fad51f84702a721cbabe2a
bhabas <bhabas@psu.edu>
2023-01-29 16:41:28 -0500

    More plugin updates

--- crazyflie_gazebo/src/Model_Move.cpp
+++ crazyflie_gazebo/src/Model_Move.cpp
@@ -11,7 +11,7 @@ namespace gazebo
         gzmsg << "Loading GazeboMomentPlugin\n";
         model_ptr = parent;
 
-        linkName = _sdf->GetElement("bodyName")->Get<std::string>();
+        linkName = _sdf->GetElement("linkName")->Get<std::string>();
         gzmsg << "\t Link Name:\t" << linkName << std::endl;
         link_ptr = model_ptr->GetLink(linkName);
 
@@ -34,13 +34,18 @@ namespace gazebo
 
     bool ModelMove::Service_Callback(crazyflie_msgs::ModelMove::Request &req, crazyflie_msgs::ModelMove::Response &res)
     {
-        // vel_vec.Set(req.Vel.x,req.Vel.y,req.Vel.z);
+
         t_0 = ros::Time::now().toSec();
-        
+        vel.Set(req.Vel.x,req.Vel.y,req.Vel.z);
+        ang_vel.Set(req.Ang_Vel.x,req.Ang_Vel.y,req.Ang_Vel.z);
+        model_ptr->SetWorldTwist(vel,ang_vel);
+
 
         if (req.Reset_Pose == true)
         {
-            pose.Set(req.Position.x,0,0,0,0,0);
+            pos.Set(req.Pos.x,req.Pos.y,req.Pos.z);
+            eul.Set(req.Eul.x,req.Eul.y,req.Eul.z);
+            pose.Set(pos,eul);
             model_ptr->SetWorldPose(pose);
         }
         
@@ -48,18 +53,6 @@ namespace gazebo
 
         return true;
     }
-
-    //     }
-    //     else if(msg->cmd_type == 0)
-    //     {
-    //         ignition::math::Pose3d pose(
-    //             ignition::math::Vector3d(0, 0, 0.5),
-    //             ignition::math::Quaterniond(1.0, 0.0, 0.0, 0.0)
-    //         );
-    //         model_ptr->SetWorldPose(pose);
-    //         model_ptr->SetWorldTwist(ignition::math::Vector3d(0, 0, 0),ignition::math::Vector3d(0, 0, 0));
-    //         torque_vec.Set(0,0,0);
-    //         thrust_vec.Set(0,0,0);
 
     GZ_REGISTER_MODEL_PLUGIN(ModelMove);
 }
