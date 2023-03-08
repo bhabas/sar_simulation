f6f486c326121482573edb2bc5e1e9d07842b0a1
bhabas <bhabas@psu.edu>
2023-03-08 09:01:22 -0500

    Updated requirements and setup files

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='crazyflie_env',
-    packages=find_packages(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+   name='crazyflie_env',
+   version='3.0',
+   description='A useful module',
+   long_description=long_description,
+   author='Bryan Habas',
+   packages=setuptools.find_packages(),
+   python_requires=">=3.6",
+   install_requires=requirements,
+#    scripts=[
+#             'scripts/cool',
+#             'scripts/skype',
+#            ]
 )
