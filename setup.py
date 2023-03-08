05b5149425ebf5a3047c085bd2bf25986dab03cd
bhabas <bhabas@psu.edu>
2023-03-08 10:14:59 -0500

    Adding versioneer version control

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,15 +7,23 @@ with open("requirements.txt", "r") as fh:
     requirements = [line.strip() for line in fh]
 
 setuptools.setup(
-   name='crazyflie_env',
-   version='3.0',
-   description='A useful module',
-   long_description=long_description,
-   author='Bryan Habas',
-   packages=setuptools.find_packages(),
-   python_requires=">=3.6",
-   install_requires=requirements,
-   scripts=[
-            'sar_general_scripts/Control_Playground.py',
-           ]
+    name='crazyflie_env',
+    author='Bryan Habas',
+    maintainer='Bryan Habas',
+    maintainer_email='BHabas@psu.edu',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='A useful module',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=requirements,
+    scripts=[
+                'sar_general_scripts/Control_Playground.py',
+            ]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Ubuntu 20.04",
+    ],
 )
