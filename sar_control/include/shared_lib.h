c446a5eaa6df63a3d8e0c7521a0662695ae2fb14
bhabas <bhabas@psu.edu>
2023-04-11 11:26:34 -0400

    More controller progress

--- sar_control/include/shared_lib.h
+++ sar_control/include/shared_lib.h
@@ -20,8 +20,18 @@ struct GTC_CmdPacket{
 
 extern struct GTC_CmdPacket GTC_Cmd;
 
+// DECLARE SYSTEM PARAMETERS
+extern float m;     // [kg]
+extern float Ixx;   // [kg*m^2]
+extern float Iyy;   // [kg*m^2]
+extern float Izz;   // [kg*m^2]
+extern float dt;    // Controller cycle time
+
+extern float dp;    // COM to Prop along x-axis [m]
+extern float c_tf;  // Moment Coeff [Nm/N]
+
 // =================================
-//    CONTROL GAIN INITIALIZATION
+//    CONTROL GAIN DECLARATIONS
 // =================================
 // XY POSITION PID
 extern float P_kp_xy;
@@ -47,7 +57,8 @@ extern float R_kd_z;
 extern float R_ki_z;
 extern float i_range_R_z;
 
-// INIT CTRL GAIN VECTORS 
+
+// DECLARE CTRL GAIN VECTORS 
 extern struct vec Kp_p; // Pos. Proportional Gains 
 extern struct vec Kd_p; // Pos. Derivative Gains
 extern struct vec Ki_p; // Pos. Integral Gains  
@@ -55,6 +66,54 @@ extern struct vec Ki_p; // Pos. Integral Gains
 extern struct vec Kp_R; // Rot. Proportional Gains
 extern struct vec Kd_R; // Rot. Derivative Gains
 extern struct vec Ki_R; // Rot. Integral Gains
+
+
+// DECLARE CTRL GAIN FLAGS
+extern float kp_xf; // Pos. Gain Flag
+extern float kd_xf; // Pos. Derivative Gain Flag
+
+
+// DECLARE STATE VALUES
+extern struct vec statePos;     // Pos [m]
+extern struct vec stateVel;     // Vel [m/s]
+extern struct quat stateQuat;   // Orientation
+extern struct vec stateEul;     // Orientation in Euler Angles [YZX Notation]
+extern struct vec stateOmega;   // Angular Rate [rad/s]
+
+extern struct mat33 R;          // Orientation as rotation matrix
+extern struct vec b3;           // Current body z-axis in global coord.
+
+
+// DECLARE DESIRED STATES
+extern struct vec x_d;          // Pos-desired [m]
+extern struct vec v_d;          // Vel-desired [m/s]
+extern struct vec a_d;          // Acc-desired [m/s^2]
+
+extern struct quat quat_d;      // Orientation-desired [qx,qy,qz,qw]
+extern struct vec eul_d;        // Euler Angle-desired [deg]
+
+extern struct vec b1_d;         // Desired body x-axis in global coord. 
+extern struct vec b2_d;         // Desired body y-axis in global coord.
+extern struct vec b3_d;         // Desired body z-axis in global coord.
+extern struct mat33 R_d;        // Desired rotational matrix from b_d vectors
+
+
+extern struct vec omega_d;      // Omega-desired [rad/s]
+extern struct vec domega_d;     // Ang. Acc-desired [rad/s^2]
+
+
+// DECLARE CONTROLLER ACTUATIONS
+extern struct vec F_thrust_ideal;   // Ideal thrust vector [N]
+extern float F_thrust;              // Implemented body thrust [N]
+extern struct vec M;                // Implemented body moments [N*m]
+
+// DECLARE MOTOR THRUST ACTIONS
+extern float f_thrust_g;        // Motor thrust - Thrust [g]
+extern float f_roll_g;          // Motor thrust - Roll   [g]
+extern float f_pitch_g;         // Motor thrust - Pitch  [g]
+extern float f_yaw_g;           // Motor thrust - Yaw    [g]
+
+
 
 void GTC_Command(struct GTC_CmdPacket *GTC_Cmd);
 void controlOutput(const state_t *state, const sensorData_t *sensors);
--- sar_control/src/shared_lib.c
+++ sar_control/src/shared_lib.c
@@ -2,6 +2,20 @@
 
 struct GTC_CmdPacket GTC_Cmd;
 
+// INITIAL SYSTEM PARAMETERS
+float m = 34.3e-3f;         // [kg]
+float Ixx = 15.83e-6f;      // [kg*m^2]
+float Iyy = 17.00e-6f;      // [kg*m^2]
+float Izz = 31.19e-6f;      // [kg*m^2]
+// static struct mat33 J;      // Rotational Inertia Matrix [kg*m^2]
+
+const float g = 9.81f;                        // Gravity [m/s^2]
+const struct vec e_3 = {0.0f, 0.0f, 1.0f};    // Global z-axis
+float dt = (float)(1.0f/RATE_100_HZ);
+
+float dp = 0.0325;           // COM to Prop along x-axis [m]
+float c_tf = 0.00618f; // Moment Coeff [Nm/N]
+
 // =================================
 //    CONTROL GAIN INITIALIZATION
 // =================================
@@ -42,6 +56,77 @@ struct vec Kd_R; // Rot. Derivative Gains
 struct vec Ki_R; // Rot. Integral Gains
 
 
+// INIT GAIN FLAGS
+float kp_xf = 1; // Pos. Gain Flag
+float kd_xf = 1; // Pos. Derivative Gain Flag
+
+
+// INIT STATE VALUES
+struct vec statePos = {0.0,0.0f,0.0f};          // Pos [m]
+struct vec stateVel = {0.0f,0.0f,0.0f};         // Vel [m/s]
+struct quat stateQuat = {0.0f,0.0f,0.0f,1.0f};  // Orientation
+struct vec stateEul = {0.0f,0.0f,0.0f};         // Orientation in Euler Angles [YZX Notation]
+struct vec stateOmega = {0.0f,0.0f,0.0f};       // Angular Rate [rad/s]
+
+
+struct mat33 R;                                 // Orientation as rotation matrix
+struct vec b3 = {0.0f,0.0f,1.0f};               // Current body z-axis in global coord.
+
+
+
+
+// INIT DESIRED STATES
+struct vec x_d = {0.0f,0.0f,0.4f};      // Pos-desired [m]
+struct vec v_d = {0.0f,0.0f,0.0f};      // Vel-desired [m/s]
+struct vec a_d = {0.0f,0.0f,0.0f};      // Acc-desired [m/s^2]
+
+struct quat quat_d = {0.0f,0.0f,0.0f,1.0f}; // Orientation-desired [qx,qy,qz,qw]
+struct vec eul_d = {0.0f,0.0f,0.0f};        // Euler Angle-desired [deg]
+
+struct vec b1_d = {1.0f,0.0f,0.0f};         // Desired body x-axis in global coord. 
+struct vec b2_d = {0.0f,1.0f,0.0f};         // Desired body y-axis in global coord.
+struct vec b3_d = {0.0f,0.0f,1.0f};         // Desired body z-axis in global coord.
+struct mat33 R_d;                           // Desired rotational matrix from b_d vectors
+
+struct vec omega_d = {0.0f,0.0f,0.0f};      // Omega-desired [rad/s]
+struct vec domega_d = {0.0f,0.0f,0.0f};     // Ang. Acc-desired [rad/s^2]
+
+// STATE ERRORS
+struct vec e_x;  // Pos-error [m]
+struct vec e_v;  // Vel-error [m/s]
+struct vec e_PI; // Pos. Integral-error [m*s]
+
+struct vec e_R;  // Rotation-error [rad]
+struct vec e_w;  // Omega-error [rad/s]
+struct vec e_RI; // Rot. Integral-error [rad*s]
+
+// TEMPORARY CALC VECS/MATRICES
+static struct vec temp1_v; 
+static struct vec temp2_v;
+static struct vec temp3_v;
+// static struct vec temp4_v;
+// static struct mat33 temp1_m;  
+
+static struct vec P_effort; // Effort by positional PID
+// static struct vec R_effort; // Effor by rotational PID
+
+static struct mat33 RdT_R;  // Rd' * R
+static struct mat33 RT_Rd;  // R' * Rd
+// static struct vec Gyro_dyn;
+
+// CONTROLLER ACTUATIONS
+struct vec F_thrust_ideal = {0.0f,0.0f,1.0f};   // Ideal thrust vector [N]
+float F_thrust = 0.0f;                          // Implemented body thrust [N]
+struct vec M = {0.0f,0.0f,0.0f};                // Implemented body moments [N*m]
+
+// MOTOR THRUST ACTIONS
+float f_thrust_g = 0.0f; // Motor thrust - Thrust [g]
+float f_roll_g = 0.0f;   // Motor thrust - Roll   [g]
+float f_pitch_g = 0.0f;  // Motor thrust - Pitch  [g]
+float f_yaw_g = 0.0f;    // Motor thrust - Yaw    [g]
+
+
+
 void GTC_Command(struct GTC_CmdPacket *GTC_Cmd)
 {
     consolePrintf("Command Recieved:\n");
@@ -76,72 +161,71 @@ void controlOutput(const state_t *state, const sensorData_t *sensors)
 
     consolePrintf("Kp_p.x: %.3f\n",(double)P_kp_xy);
 
-    // // =========== STATE DEFINITIONS =========== //
-    // statePos = mkvec(state->position.x, state->position.y, state->position.z);                      // [m]
-    // stateVel = mkvec(state->velocity.x, state->velocity.y, state->velocity.z);                      // [m]
-    // stateOmega = mkvec(radians(sensors->gyro.x), radians(sensors->gyro.y), radians(sensors->gyro.z));   // [rad/s]
-    // stateQuat = mkquat(state->attitudeQuaternion.x,
-    //                 state->attitudeQuaternion.y,
-    //                 state->attitudeQuaternion.z,
-    //                 state->attitudeQuaternion.w);
+    // =========== STATE DEFINITIONS =========== //
+    statePos = mkvec(state->position.x, state->position.y, state->position.z);                      // [m]
+    stateVel = mkvec(state->velocity.x, state->velocity.y, state->velocity.z);                      // [m]
+    stateOmega = mkvec(radians(sensors->gyro.x), radians(sensors->gyro.y), radians(sensors->gyro.z));   // [rad/s]
+    stateQuat = mkquat(state->attitudeQuaternion.x,
+                    state->attitudeQuaternion.y,
+                    state->attitudeQuaternion.z,
+                    state->attitudeQuaternion.w);
 
-    // // EULER ANGLES EXPRESSED IN YZX NOTATION
-    // stateEul = quat2eul(stateQuat);
-    // stateEul.x = degrees(stateEul.x);
-    // stateEul.y = degrees(stateEul.y);
-    // stateEul.z = degrees(stateEul.z);
+    // EULER ANGLES EXPRESSED IN YZX NOTATION
+    stateEul = quat2rpy(stateQuat);
+    stateEul.x = degrees(stateEul.x);
+    stateEul.y = degrees(stateEul.y);
+    stateEul.z = degrees(stateEul.z);
     
-    // // =========== STATE SETPOINTS =========== //
-    // omega_d = mkvec(0.0f,0.0f,0.0f);    // Omega-desired [rad/s]
-    // domega_d = mkvec(0.0f,0.0f,0.0f);   // Omega-Accl. [rad/s^2]
+    // =========== STATE SETPOINTS =========== //
+    omega_d = mkvec(0.0f,0.0f,0.0f);    // Omega-desired [rad/s]
+    domega_d = mkvec(0.0f,0.0f,0.0f);   // Omega-Accl. [rad/s^2]
 
-    // eul_d = mkvec(0.0f,0.0f,0.0f);
-    // quat_d = rpy2quat(eul_d);           // Desired orientation from eul angles [ZYX NOTATION]
+    eul_d = mkvec(0.0f,0.0f,0.0f);
+    quat_d = rpy2quat(eul_d);           // Desired orientation from eul angles [ZYX NOTATION]
 
-    // // =========== ROTATION MATRIX =========== //
-    // // R changes Body axes to be in terms of Global axes
-    // // https://www.andre-gaschler.com/rotationconverter/
-    // R = quat2rotmat(stateQuat); // Quaternion to Rotation Matrix Conversion
-    // b3 = mvmul(R, e_3);         // Current body vertical axis in terms of global axes | [b3 = R*e_3] 
+    // =========== ROTATION MATRIX =========== //
+    // R changes Body axes to be in terms of Global axes
+    // https://www.andre-gaschler.com/rotationconverter/
+    R = quat2rotmat(stateQuat); // Quaternion to Rotation Matrix Conversion
+    b3 = mvmul(R, e_3);         // Current body vertical axis in terms of global axes | [b3 = R*e_3] 
         
 
 
-    // // =========== TRANSLATIONAL EFFORT =========== //
-    // e_x = vsub(statePos, x_d); // [e_x = pos-x_d]
-    // e_v = vsub(stateVel, v_d); // [e_v = vel-v_d]
+    // =========== TRANSLATIONAL EFFORT =========== //
+    e_x = vsub(statePos, x_d); // [e_x = pos-x_d]
+    e_v = vsub(stateVel, v_d); // [e_v = vel-v_d]
 
+    // POS. INTEGRAL ERROR
+    e_PI.x += (e_x.x)*dt;
+    e_PI.x = clamp(e_PI.x, -i_range_xy, i_range_xy);
 
-    // // POS. INTEGRAL ERROR
-    // e_PI.x += (e_x.x)*dt;
-    // e_PI.x = clamp(e_PI.x, -i_range_xy, i_range_xy);
+    e_PI.y += (e_x.y)*dt;
+    e_PI.y = clamp(e_PI.y, -i_range_xy, i_range_xy);
 
-    // e_PI.y += (e_x.y)*dt;
-    // e_PI.y = clamp(e_PI.y, -i_range_xy, i_range_xy);
+    e_PI.z += (e_x.z)*dt;
+    e_PI.z = clamp(e_PI.z, -i_range_z, i_range_z);
 
-    // e_PI.z += (e_x.z)*dt;
-    // e_PI.z = clamp(e_PI.z, -i_range_z, i_range_z);
+    /* [F_thrust_ideal = -kp_x*e_x*(kp_x_flag) + -kd_x*e_v + -kI_x*e_PI*(kp_x_flag) + m*g*e_3 + m*a_d] */
+    temp1_v = veltmul(vneg(Kp_p), e_x);
+    temp1_v = vscl(kp_xf,temp1_v);
+    temp2_v = veltmul(vneg(Kd_p), e_v);
+    temp1_v = vscl(kd_xf,temp1_v);
+    temp3_v = veltmul(vneg(Ki_p), e_PI);
+    P_effort = vadd3(temp1_v,temp2_v,temp3_v);
+    temp1_v = vscl(m*g, e_3); // Feed-forward term
+    temp2_v = vscl(m, a_d);
 
-    // /* [F_thrust_ideal = -kp_x*e_x*(kp_x_flag) + -kd_x*e_v + -kI_x*e_PI*(kp_x_flag) + m*g*e_3 + m*a_d] */
-    // temp1_v = veltmul(vneg(Kp_p), e_x);
-    // temp1_v = vscl(kp_xf,temp1_v);
-    // temp2_v = veltmul(vneg(Kd_p), e_v);
-    // temp1_v = vscl(kd_xf,temp1_v);
-    // temp3_v = veltmul(vneg(Ki_p), e_PI);
-    // P_effort = vadd3(temp1_v,temp2_v,temp3_v);
-    // temp1_v = vscl(m*g, e_3); // Feed-forward term
-    // temp2_v = vscl(m, a_d);
+    F_thrust_ideal = vadd3(P_effort, temp1_v,temp2_v); 
 
-    // F_thrust_ideal = vadd3(P_effort, temp1_v,temp2_v); 
+    // =========== DESIRED BODY AXES =========== // 
+    b3_d = vnormalize(F_thrust_ideal);
+    b2_d = vnormalize(vcross(b3_d, b1_d));      // [b3_d x b1_d] | body-fixed horizontal axis
+    temp1_v = vnormalize(vcross(b2_d, b3_d));
+    R_d = mcolumns(temp1_v, b2_d, b3_d);        // Desired rotation matrix from calculations
 
-    // // =========== DESIRED BODY AXES =========== // 
-    // b3_d = vnormalize(F_thrust_ideal);
-    // b2_d = vnormalize(vcross(b3_d, b1_d));      // [b3_d x b1_d] | body-fixed horizontal axis
-    // temp1_v = vnormalize(vcross(b2_d, b3_d));
-    // R_d = mcolumns(temp1_v, b2_d, b3_d);        // Desired rotation matrix from calculations
-
-    // // =========== ROTATIONAL ERRORS =========== // 
-    // RdT_R = mmul(mtranspose(R_d), R);       // [R_d'*R]
-    // RT_Rd = mmul(mtranspose(R), R_d);       // [R'*R_d]
+    // =========== ROTATIONAL ERRORS =========== // 
+    RdT_R = mmul(mtranspose(R_d), R);       // [R_d'*R]
+    RT_Rd = mmul(mtranspose(R), R_d);       // [R'*R_d]
 
     // temp1_v = dehat(msub(RdT_R, RT_Rd));    // [dehat(R_d'*R - R'*R)]
     // e_R = vscl(0.5f, temp1_v);              // Rotation error | [eR = 0.5*dehat(R_d'*R - R'*R)]
