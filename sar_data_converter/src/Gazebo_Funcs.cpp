f08540f6956d2c81efd79685beb00be2f9b22d3f
bhabas <bhabas@psu.edu>
2023-08-01 14:31:38 -0400

    Updating file names and location for crazyflie configs

--- sar_data_converter/src/Gazebo_Funcs.cpp
+++ sar_data_converter/src/Gazebo_Funcs.cpp
@@ -7,7 +7,7 @@
  */
 void SAR_DataConverter::activateStickyFeet()
 {
-    if(GZ_Model_Name != "Crazyflie_Base_Model")
+    if(GZ_Model_Name != "Base_Model")
     {
         sar_msgs::Activate_Sticky_Pads srv;
         srv.request.stickyFlag = Sticky_Flag;
