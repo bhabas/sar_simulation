7731b01b27772f2e0f704f7659a71072226e3792
bhabas <bhabas@psu.edu>
2023-08-02 15:16:52 -0400

    Working connections to spawn ground model

--- sar_launch/launch/launch_gazebo.bash
+++ sar_launch/launch/launch_gazebo.bash
@@ -14,22 +14,18 @@ PAUSE_FLAG=$(rosparam get /SIM_SETTINGS/Pause_Flag)
 ## LOAD SAR PARAMS
 SAR_TYPE=$(rosparam get /SAR_SETTINGS/SAR_Type)
 SAR_CONFIG=$(rosparam get /SAR_SETTINGS/SAR_Config)
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${SAR_TYPE}
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${SAR_TYPE}/Configs
 
 ## LOAD PLANE CONFIG PARAMS
 PLANE_TYPE=$(rosparam get /PLANE_SETTINGS/Plane_Type)
 PLANE_CONFIG=$(rosparam get /PLANE_SETTINGS/Plane_Config)
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${PLANE_TYPE}
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${PLANE_TYPE}/Configs
 
 ## LOAD PLANE CONFIG PARAMS
 GROUND_TYPE=$(rosparam get /GROUND_SETTINGS/Ground_Type)
 GROUND_CONFIG=$(rosparam get /GROUND_SETTINGS/Ground_Config)
-
-## CREATE MODEL PATH
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${SAR_TYPE}
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${SAR_TYPE}/Configs
-
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${PLANE_TYPE}
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${PLANE_TYPE}/Configs
-
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${GROUND_TYPE}
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_PATH}/sar_gazebo/models/${GROUND_TYPE}/Configs
 
@@ -42,6 +38,8 @@ roslaunch sar_launch Gazebo_Sim.launch \
     SAR_Model:=$SAR_CONFIG \
     Plane_Type:=$PLANE_TYPE \
     Plane_Config:=$PLANE_CONFIG \
-    Ground_Model:=$GROUND_NAME \
+    Ground_Type:=$GROUND_TYPE \
+    Ground_Config:=$GROUND_CONFIG \
+
 
 
