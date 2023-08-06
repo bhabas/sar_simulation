429191d266ba9a0a006e8e410891fb46518f8ff9
bhabas <bhabas@psu.edu>
2023-08-05 21:49:46 -0400

    This works for testing, look into detecting collision and joining

--- sar_gazebo/src/Leg_Connect_Plugin.cpp
+++ sar_gazebo/src/Leg_Connect_Plugin.cpp
@@ -11,23 +11,9 @@ namespace gazebo
         Model_Ptr = _parent;
         World_Ptr = Model_Ptr->GetWorld();
 
-        // std::cout << Marker_Ptr->GetName() << std::endl;
-
-
-        int max_attempts = 30;
-        int attempt = 0;
-
-        while (!World_Ptr->ModelByName("Marker") && attempt < max_attempts) {
-            gazebo::common::Time::MSleep(100);  // Sleep for 100ms
-            attempt++;
-        }
-        while (!World_Ptr->ModelByName("Debug") && attempt < max_attempts) {
-            gazebo::common::Time::MSleep(100);  // Sleep for 100ms
-            attempt++;
-        }
 
         physics::ModelPtr Marker_Ptr = World_Ptr->ModelByName("Marker");
-        physics::ModelPtr Surface_Ptr = World_Ptr->ModelByName("Debug");
+        physics::ModelPtr Surface_Ptr = World_Ptr->ModelByName("Surface");
 
 
         if (!Marker_Ptr) {
@@ -40,14 +26,13 @@ namespace gazebo
         
         ignition::math::Pose3d contactPose;
         contactPose.Set(0,0,0.5,0,0,0);
-
         Marker_Ptr->SetWorldPose(contactPose);
 
 
         physics::LinkPtr Beam_Ptr = Model_Ptr->GetLink("beam_link");
         physics::JointPtr joint = World_Ptr->Physics()->CreateJoint("ball",Model_Ptr);
-        joint->Attach(NULL,Beam_Ptr);
-        joint->Load(NULL, Beam_Ptr, contactPose);
+        joint->Attach(Surface_Ptr->GetLink("Surface_Link"),Beam_Ptr);
+        joint->Load(Surface_Ptr->GetLink("Surface_Link"), Beam_Ptr, contactPose);
         joint->SetAnchor(0, contactPose.Pos());
         joint->Init();
 
