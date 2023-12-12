aab9e09a094c778f9a0ca4b17413b41d1609c267
bhabas <bhabas@psu.edu>
2023-12-12 09:34:49 -0500

    Add image inversion functionality to Example_Cam_Plugin

--- sar_gazebo/plugins/include/Example_Cam_Plugin.h
+++ sar_gazebo/plugins/include/Example_Cam_Plugin.h
@@ -12,6 +12,10 @@
 #include <gazebo/common/common.hh>
 #include <gazebo/sensors/sensors.hh>
 
+#include <sensor_msgs/Image.h>
+
+// rosrun image_view image_view image:=/SAR_Internal/camera/image_raw
+
 namespace gazebo {
 
     class Example_Cam_Plugin: public ModelPlugin
@@ -20,11 +24,17 @@ namespace gazebo {
             
         protected:
             void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+            void CamData_Callback(const sensor_msgs::Image::ConstPtr &Camera_msg);
+
+            
 
         private:
 
-            ros::NodeHandle nh;
+            sensor_msgs::Image inverted_image_msg;
 
+            ros::NodeHandle nh;
+            ros::Subscriber Cam_Data_Sub = nh.subscribe<sensor_msgs::Image>("/SAR_Internal/camera/image_raw", 1, &Example_Cam_Plugin::CamData_Callback, this, ros::TransportHints().tcpNoDelay());
+            ros::Publisher Cam_Data_Pub = nh.advertise<sensor_msgs::Image>("/SAR_Internal/camera/inverted_image", 5);
     };
 
 }
