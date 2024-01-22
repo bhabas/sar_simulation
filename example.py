96a7d68f50c824bd9f0af09e224205359e9551e0
bhabas <bhabas@psu.edu>
2024-01-22 09:53:27 -0500

    Added self collision to legs and need some sort of effective stiffness calc

--- example.py
+++ example.py
@@ -1,32 +1,19 @@
-from sympy import symbols, cos, sin, Matrix, pprint, simplify
-from sympy import *
+import numpy as np
+from scipy.optimize import fsolve
 
-# Define the angle symbols
-phi, theta, beta_1, beta_2, gamma = symbols('phi theta beta_1 beta_2 gamma')
-PD, L, gamma, a = symbols('PD L gamma a')
+m = 693.0e-3  # mass in kg
+g = 9.81      # acceleration due to gravity in m/s^2
+L = 18.5e-3       # length in meters
+K = 1*2        # torsional spring constant in Nm/rad
 
+# Define the equation m*g*L*sin(theta) = K*theta
+def equation(theta):
+    return m * g * L * np.sin(theta) - 1/2*K * theta**2
 
-equation1 = Eq(sqrt(PD**2 + L**2 - 2*PD*L*cos(pi/2-gamma)),a)
-equation2 = acos((L**2 + a**2 - PD**2)/(2*L*a))
+# Initial guess for theta
+theta_guess = 1.0
 
+# Solve the equation
+theta_solution = fsolve(equation, theta_guess)
 
-solution_for_a = solve(equation1, a)[0]
-substituted_eq2 = equation2.subs(a, solution_for_a)
-
-
-pprint(factor(equation1))
-
-# # Define the first rotation matrix (for example, a rotation around the z-axis)
-# R_P_W = Matrix([
-#     [ cos(theta), sin(theta)],
-#     [-sin(theta), cos(theta)],
-# ])
-
-# R_B_W = Matrix([
-#     [ cos(phi), sin(phi)],
-#     [-sin(phi), cos(phi)],
-# ])
-
-# print("Phi_rel Rotation Matrix:")
-# pprint(simplify(R_B_W.T*R_P_W))
-# print("\n\n\n")
+print(theta_solution/2)
