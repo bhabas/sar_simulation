0ca82433ec87da9b4340b939a9d50f305be2634e
bhabas <bhabas@psu.edu>
2024-02-22 19:06:31 -0500

    Working 7-segment profile. Scaling is off however

--- sar_control/src/Traj_Funcs.c
+++ sar_control/src/Traj_Funcs.c
@@ -10,6 +10,7 @@ float s_0_t[3] = {0.0f, 0.0f, 0.0f};   // Traj Start Point [m]
 float s_f_t[3] = {0.0f, 0.0f, 0.0f};   // Traj End Point [m]
 float v_t[3] = {0.0f, 0.0f, 0.0f};     // Traj Vel [m/s]
 float a_t[3] = {0.0f, 0.0f, 0.0f};     // Traj Accel [m/s^2]
+float j_t[3] = {2.0f, 2.0f, 2.0f};        // Jerk [m/s^3]
 float T[3] = {0.0f, 0.0f, 0.0f};       // Traj completion time [s]
 float t_traj[3] = {0.0f, 0.0f, 0.0f};  // Traj time counter [s]
 
@@ -25,59 +26,113 @@ void point2point_Traj()
             float S = s_f_t[i] - s_0_t[i];
             float a_sign = (S) >= 0 ? 1.0f : -1.0f; // Determine the direction of movement
 
+            float t_jerk = a_t[i]/j_t[i]; // Time to reach max acceleration
+            float v_max = sqrtf(0.5f * a_t[i] * fabsf(S) - 4.0f * 1/6.0f * j_t[i] * a_t[i] * powf(t_jerk,3)); // Max velocity 
+            float t_acc = (v_max - 2.0f * 0.5f * j_t[i] * fsqr(t_jerk)) / a_t[i];
 
-
-            float v_max = sqrtf(0.5f * a_t[i] * fabsf(S));
-            float t_acc = v_max / a_t[i];
+            float s_jerk = 1/6.0f * j_t[i] * powf(t_jerk,3);
+            float v_jerk = 0.5f * j_t[i] * fsqr(t_jerk);
 
             float s_acc = 0.5f * a_t[i] * fsqr(t_acc);
-            float s_const = fabsf(S) - 2 * s_acc;
+            float v_acc = a_t[i] * t_acc;
+
+            float s_const = fabsf(S) - 2.0f * s_acc - 4.0f * s_jerk;
             float t_const = s_const / v_max;
-            T[i] = 2 * t_acc + t_const;
+            T[i] = t_jerk + t_acc + t_jerk + t_const + t_jerk + t_acc + t_jerk;
+
+
+            float t_1 = t_jerk;
+            float t_2 = t_jerk + t_acc;
+            float t_3 = t_jerk + t_acc + t_jerk;
+            float t_4 = t_jerk + t_acc + t_jerk + t_const;
+            float t_5 = t_jerk + t_acc + t_jerk + t_const + t_jerk;
+            float t_6 = t_jerk + t_acc + t_jerk + t_const + t_jerk + t_acc;
+            float t_7 = t_jerk + t_acc + t_jerk + t_const + t_jerk + t_acc + t_jerk;
+
+            float dds_1 = j_t[i]*t_1;
+            float ds_1 = 0.5f*j_t[i]*fsqr(t_1);
+            float s_1 = 1/6.0f*j_t[i]*pow(t_1,3);
+
+            float dds_2 = a_t[i];
+            float ds_2 = ds_1 + a_t[i]*(t_2-t_1);
+            float s_2 = s_1 + ds_1*(t_2-t_1) + 0.5f*a_t[i]*fsqr(t_2-t_1);
+
+            float dds_3 = a_t[i] - j_t[i]*(t_3-t_2);
+            float ds_3 = ds_2 + a_t[i]*(t_3-t_2) - 0.5f*j_t[i]*fsqr(t_3-t_2);
+            float s_3 = s_2 + ds_2*(t_3-t_2) + 0.5f*a_t[i]*fsqr(t_3-t_2) - 1/6.0f*j_t[i]*powf(t_3-t_2,3);
+
+            float dds_4 = 0.0f;
+            float ds_4 = ds_3;
+            float s_4 = s_3 + ds_3*(t_4-t_3);
+
+            float dds_5 = -j_t[i]*(t_5-t_4);
+            float ds_5 = ds_4 - 0.5f*j_t[i]*fsqr(t_5-t_4);
+            float s_5 = s_4 + ds_4*(t_5-t_4) - 1/6.0f*j_t[i]*powf(t_5-t_4,3);
+
+            float dds_6 = -a_t[i];
+            float ds_6 = ds_5 - a_t[i]*(t_6-t_5);
+            float s_6 = s_5 + ds_5*(t_6-t_5) - 0.5f*a_t[i]*fsqr(t_6-t_5);
+
         
             float pos_val = 0.0f;
             float vel_val = 0.0f;
             float acc_val = 0.0f;
+        
+            // printf("t_jerk: %f\n", t_jerk);
+            // printf("t_acc: %f\n", t_acc);
+            // printf("v_max: %f\n", v_max);
 
-            if (t <= t_acc)
+            if (t <= t_1)
             {
-                acc_val = a_sign * a_t[i];
-                vel_val = a_sign * a_t[i] * t;
-                pos_val = a_sign * 0.5f * a_t[i] * fsqr(t);
+                acc_val = j_t[i] * t;
+                vel_val = 0.5f * j_t[i] * fsqr(t);
+                pos_val = 1/6.0f*j_t[i]*powf(t,3);
+            }
+            else if (t_1 < t && t <= t_2)
+            {
+                acc_val = a_t[i];
+                vel_val = ds_1 + a_t[i]*(t - t_1);
+                pos_val = s_1 + ds_1*(t-t_1) + 0.5f*a_t[i]*fsqr(t-t_1);
 
             }
-            else if (t_acc < t && t <= T[i] - t_acc)
+            else if (t_2 < t && t <= t_3)
+            {
+                acc_val = a_t[i] - j_t[i]*(t - t_2);
+                vel_val = ds_2 + a_t[i]*(t-t_2) - 0.5f*j_t[i]*powf(t-t_2,2);
+                pos_val = s_2 + ds_2*(t-t_2) + 0.5f*a_t[i]*fsqr(t-t_2) - 1/6.0f*j_t[i]*powf(t-t_3,3);
+            }
+            else if (t_3 < t && t <= t_4)
             {
                 acc_val = 0.0f;
-                vel_val = v_max;
-                pos_val = s_acc + v_max * (t - t_acc);
-
-                acc_val *= a_sign;
-                vel_val *= a_sign;
-                pos_val *= a_sign;
+                vel_val = ds_3;
+                pos_val = s_3 + ds_3*(t-t_3);
             }
-            else if (T[i] - t_acc < t && t <= T[i])
+            else if (t_4 < t && t <= t_5)
+            {
+                acc_val = -j_t[i]*(t - t_4);
+                vel_val = ds_4 - 0.5f*j_t[i]*fsqr(t-t_4);
+                pos_val = s_4 + ds_4*(t-t_4) - 1/6.0f*j_t[i]*powf(t-t_4,3);
+            }
+            else if (t_5 < t && t <= t_6)
             {
                 acc_val = -a_t[i];
-                vel_val = v_max - a_t[i] * (t - (T[i] - t_acc));
-                pos_val = s_acc + s_const + (s_acc - 0.5f * a_t[i] * fsqr(T[i] - t));
-
-                acc_val *= a_sign;
-                vel_val *= a_sign;
-                pos_val *= a_sign;
+                vel_val = ds_5 - a_t[i]*(t-t_5);
+                pos_val = s_5 + ds_5*(t-t_5) - 0.5f*a_t[i]*fsqr(t-t_5);
+            }
+            else if (t_6 < t && t <= t_7)
+            {
+                acc_val = -a_t[i] + j_t[i]*(t-t_6);
+                vel_val = ds_6 - a_t[i]*(t-t_6) + 0.5f*j_t[i]*fsqr(t-t_6);
+                pos_val = s_6 + ds_6*(t-t_6) - 0.5f*a_t[i]*fsqr(t-t_6) + 1/6.0f*j_t[i]*powf(t-t_6,3);
             }
             else
             {
-                acc_val = 0.0f;
-                vel_val = 0.0f;
-                pos_val = s_acc + s_const + s_acc;
-
-                acc_val *= a_sign;
-                vel_val *= a_sign;
-                pos_val *= a_sign;
+                // acc_val = 0.0f;
+                // vel_val = 0.0f;
+                // pos_val = 0.0f;
             }
-
-            pos_val += s_0_t[i];
+            
+            // pos_val += s_0_t[i];
 
             // UPDATE DESIRED STATE VECTORS
             set_vec_element(&x_d, i, pos_val);
