fa49a5414322674858c4084937b117078e274703
bhabas <bhabas@psu.edu>
2024-02-23 07:52:57 -0500

    Revert "Working trapezoidal trajectory with new framework"
    This reverts commit 7dffc4e5679c96825bcdd7478f417db6a0c2e6dc.

--- sar_control/src/Traj_Funcs.c
+++ sar_control/src/Traj_Funcs.c
@@ -24,53 +24,17 @@ void point2point_Traj()
         {
             float t = t_traj[i];
             float S = s_f_t[i] - s_0_t[i];
-            float a_sign = (S) >= 0 ? 1.0f : -1.0f; // Determine the direction of movement
 
             float t_jerk = a_t[i]/j_t[i]; // Time to reach max acceleration
 
-            float A = 0.5f*a_t[i];
-            float B = 0.5f*j_t[i]*fsqr(t_jerk) + a_t[i]*t_jerk;
-            float C = 0.5f*a_t[i]*fsqr(t_jerk) + 0.5*j_t[i]*powf(t_jerk,3) - 0.25f*S;
-            float t_acc = (-B + sqrtf(B*B - 4*A*C))/(2.0f*A);
+            float v_max = 1.0f;
+            float a_sign = (S >= 0) ? 1.0f : -1.0f; // Determine the direction of acceleration
+            float t_acc = v_max / fabsf(a_t[i]);
 
-
-            float t_1 = t_jerk;
-            float t_2 = t_jerk + t_acc;
-            float t_3 = t_jerk + t_acc + t_jerk;
-            
-
-            float dds_1 = j_t[i]*t_1;
-            float ds_1 = 0.5f*j_t[i]*fsqr(t_1);
-            float s_1 = 1/6.0f*j_t[i]*pow(t_1,3);
-
-            float dds_2 = a_t[i];
-            float ds_2 = ds_1 + a_t[i]*(t_2-t_1);
-            float s_2 = s_1 + ds_1*(t_2-t_1) + 0.5f*a_t[i]*fsqr(t_2-t_1);
-
-            float dds_3 = a_t[i] - j_t[i]*(t_3-t_2);
-            float ds_3 = ds_2 + a_t[i]*(t_3-t_2) - 0.5f*j_t[i]*fsqr(t_3-t_2);
-            float s_3 = s_2 + ds_2*(t_3-t_2) + 0.5f*a_t[i]*fsqr(t_3-t_2) - 1/6.0f*j_t[i]*powf(t_3-t_2,3);
-
-
-            float s_const = 0.5f*S;
-            float t_const = s_const / ds_3;
-
-            float t_4 = t_jerk + t_acc + t_jerk + t_const;
-            float t_5 = t_jerk + t_acc + t_jerk + t_const + t_jerk;
-            float t_6 = t_jerk + t_acc + t_jerk + t_const + t_jerk + t_acc;
-            float t_7 = t_jerk + t_acc + t_jerk + t_const + t_jerk + t_acc + t_jerk;
-
-            float dds_4 = 0.0f;
-            float ds_4 = ds_3;
-            float s_4 = s_3 + ds_3*(t_4-t_3);
-
-            float dds_5 = -j_t[i]*(t_5-t_4);
-            float ds_5 = ds_4 - 0.5f*j_t[i]*fsqr(t_5-t_4);
-            float s_5 = s_4 + ds_4*(t_5-t_4) - 1/6.0f*j_t[i]*powf(t_5-t_4,3);
-
-            float dds_6 = -a_t[i];
-            float ds_6 = ds_5 - a_t[i]*(t_6-t_5);
-            float s_6 = s_5 + ds_5*(t_6-t_5) - 0.5f*a_t[i]*fsqr(t_6-t_5);
+            float s_acc = 0.5f * a_t[i] * fsqr(t_acc);
+            float s_const = S - 2 * s_acc;
+            float t_const = s_const / v_max;
+            T[i] = 2 * t_acc + t_const;
 
         
             float pos_val = 0.0f;
@@ -81,64 +45,43 @@ void point2point_Traj()
             // printf("t_acc: %f\n", t_acc);
             // printf("v_max: %f\n", v_max);
 
-            if (t <= t_1)
-            {
-                acc_val = j_t[i] * t;
-                vel_val = 0.5f * j_t[i] * fsqr(t);
-                pos_val = 1/6.0f*j_t[i]*powf(t,3);
-            }
-            else if (t_1 < t && t <= t_2)
-            {
-                acc_val = a_t[i];
-                vel_val = ds_1 + a_t[i]*(t - t_1);
-                pos_val = s_1 + ds_1*(t-t_1) + 0.5f*a_t[i]*fsqr(t-t_1);
+            printf("s_const: %.2f s_acc: %.2f S: %.2f\n", s_const, s_acc, S);
 
-            }
-            else if (t_2 < t && t <= t_3)
+            if (a_sign*s_const > 0) // CONSTANT VELOCITY PROFILE
             {
-                acc_val = a_t[i] - j_t[i]*(t - t_2);
-                vel_val = ds_2 + a_t[i]*(t-t_2) - 0.5f*j_t[i]*powf(t-t_2,2);
-                pos_val = s_2 + ds_2*(t-t_2) + 0.5f*a_t[i]*fsqr(t-t_2) - 1/6.0f*j_t[i]*powf(t-t_3,3);
-            }
-            else if (t_3 < t && t <= t_4)
-            {
-                acc_val = 0.0f;
-                vel_val = ds_3;
-                pos_val = s_3 + ds_3*(t-t_3);
-            }
-            else if (t_4 < t && t <= t_5)
-            {
-                acc_val = -j_t[i]*(t - t_4);
-                vel_val = ds_4 - 0.5f*j_t[i]*fsqr(t-t_4);
-                pos_val = s_4 + ds_4*(t-t_4) - 1/6.0f*j_t[i]*powf(t-t_4,3);
-            }
-            else if (t_5 < t && t <= t_6)
-            {
-                acc_val = -a_t[i];
-                vel_val = ds_5 - a_t[i]*(t-t_5);
-                pos_val = s_5 + ds_5*(t-t_5) - 0.5f*a_t[i]*fsqr(t-t_5);
-            }
-            else if (t_6 < t && t <= t_7)
-            {
-                acc_val = -a_t[i] + j_t[i]*(t-t_6);
-                vel_val = ds_6 - a_t[i]*(t-t_6) + 0.5f*j_t[i]*fsqr(t-t_6);
-                pos_val = s_6 + ds_6*(t-t_6) - 0.5f*a_t[i]*fsqr(t-t_6) + 1/6.0f*j_t[i]*powf(t-t_6,3);
-            }
-            else
-            {
-                acc_val = 0.0f;
-                vel_val = 0.0f;
-                pos_val = s_f_t[i];
-            }
-            
-            pos_val += s_0_t[i];
+                if (t <= t_acc)
+                {
+                    pos_val = s_0_t[i] + 0.5f * a_sign * a_t[i] * fsqr(t);
+                    vel_val = a_sign * a_t[i] * t;
+                    acc_val = a_sign * a_t[i];
 
-            // UPDATE DESIRED STATE VECTORS
-            set_vec_element(&x_d, i, pos_val);
-            set_vec_element(&v_d, i, vel_val);
-            set_vec_element(&a_d, i, acc_val);
-            
-            
+                }
+                else if (t_acc < t && t <= T[i] - t_acc)
+                {
+                    pos_val = s_0_t[i] + s_acc + a_sign * v_max * (t - t_acc);
+                    vel_val = a_sign * v_max;
+                    acc_val = 0.0f;
+                }
+                else if (T[i] - t_acc < t && t <= T[i])
+                {
+                    pos_val = s_f_t[i] - 0.5f * a_sign * a_t[i] * fsqr(T[i] - t);
+                    vel_val = a_sign * (v_max - a_t[i] * (t - (T[i] - t_acc)));
+                    acc_val = -a_sign * a_t[i];
+
+                }
+                else
+                {
+                    pos_val = s_f_t[i];
+                    vel_val = 0.0f;
+                    acc_val = 0.0f;
+                }
+
+                // UPDATE DESIRED STATE VECTORS
+                set_vec_element(&x_d, i, pos_val);
+                set_vec_element(&v_d, i, vel_val);
+                set_vec_element(&a_d, i, acc_val);
+               
+            }
 
             // INCREMENT TIME COUNTER FOR TRAJECTORY CALCULATIONS
             t_traj[i] += dt;
