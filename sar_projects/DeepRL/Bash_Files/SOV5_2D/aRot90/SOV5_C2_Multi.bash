55745f44104d5c641586d46af17ec4bef4f79f28
bhabas <bhabas@psu.edu>
2024-04-23 08:49:35 -0400

    Increase training time

--- sar_projects/DeepRL/Bash_Files/SOV5_2D/aRot90/SOV5_C2_Multi.bash
+++ sar_projects/DeepRL/Bash_Files/SOV5_2D/aRot90/SOV5_C2_Multi.bash
@@ -7,7 +7,7 @@ DEEP_RL_PATH=$(find ~/catkin_ws/src -name 'sar_simulation' -type d | head -n 1)/
 python $DEEP_RL_PATH/T1_Policy_Pre-Training.py \
     --GroupName SOV5_2D_aRot90/Multi \
     --TrainConfig $DEEP_RL_PATH/Config_Files/SOV5_2D_Sim/aRot90/C2/SOV5_C2_Multi_S2D.json \
-    --S3_Upload false
+    --S3_Upload true
 
 # python $DEEP_RL_PATH/T3_Policy_Data_Collection.py \
 #     --GroupName SOV5_2D_aRot90/Multi \
