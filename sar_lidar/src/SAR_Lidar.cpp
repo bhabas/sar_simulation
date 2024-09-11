2cc335ddfc22949f8122d9df14b9fcebe0501373
bhabas <bhabas@psu.edu>
2024-09-11 16:37:44 -0600

    Working lidar republisher

--- sar_lidar/src/SAR_Lidar.cpp
+++ sar_lidar/src/SAR_Lidar.cpp
@@ -1,43 +1,50 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <ros/ros.h>
 
-#include <sstream>
+// Include pcl
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-int main(int argc, char **argv)
+// Include PointCloud2 message
+#include <sensor_msgs/PointCloud2.h>
+
+static const std::string IMAGE_TOPIC = "/SAR_Internal/lidar/raw";
+static const std::string PUBLISH_TOPIC = "/pcl/points";
+
+// ROS Publisher
+ros::Publisher pub;
+
+
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
 
-  ros::init(argc, argv, "talker");
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(*cloud_msg, *cloud);
 
 
-  ros::NodeHandle n;
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    // Convert to ROS data type and publish
+    sensor_msgs::PointCloud2 output;
+    pcl::toROSMsg(*cloud, output);
+    output.header.frame_id = cloud_msg->header.frame_id;
+    output.header.stamp = ros::Time::now();
 
-  ros::Rate loop_rate(10);
+    pub.publish(output);
+
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "lidar_node");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe(IMAGE_TOPIC, 1, cloud_cb);
+
+    // ros::Subscriber sub = nh.subscribe("/SAR_Internal/lidar/raw");
+    pub = nh.advertise<sensor_msgs::PointCloud2>(PUBLISH_TOPIC, 1);
+    ros::spin();
 
 
-  int count = 0;
-  while (ros::ok())
-  {
-
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
-  }
 
 
   return 0;
