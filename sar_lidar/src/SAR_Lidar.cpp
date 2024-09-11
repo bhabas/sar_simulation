0ff76ef262ad68af90b7d55181afbe192dfe5425
bhabas <bhabas@psu.edu>
2024-09-11 16:57:16 -0600

    Lidar code for ground plane removal

--- sar_lidar/src/SAR_Lidar.cpp
+++ sar_lidar/src/SAR_Lidar.cpp
@@ -1,10 +1,13 @@
 #include <ros/ros.h>
 
 // Include pcl
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 // Include PointCloud2 message
 #include <sensor_msgs/PointCloud2.h>
@@ -22,11 +25,37 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*cloud_msg, *cloud);
 
+    // Step 2: Segment the largest plane (floor) using RANSAC
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(0.3);  // Adjust this for your floor height tolerance
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+
+    if (inliers->indices.empty()) {
+        ROS_WARN("No floor found");
+        return;
+    }
+
+    // Step 3: Extract the points that are not part of the floor (remaining points)
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud(cloud);
+    extract.setIndices(inliers);
+    extract.setNegative(true);  // true to remove the floor points
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    extract.filter(*cloud_filtered);
+
+
 
 
     // Convert to ROS data type and publish
     sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(*cloud, output);
+    pcl::toROSMsg(*cloud_filtered, output);
     output.header.frame_id = cloud_msg->header.frame_id;
     output.header.stamp = ros::Time::now();
 
